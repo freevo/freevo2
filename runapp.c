fa271b9f9fc6dffcde7f4cd45e5e44c13f35b9ec
Krister Lagerstrom <krister-freevo@kmlager.com>
2002-05-19 06:19:49 +0000

    Added logging, fixed execvp() calling error
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@116 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- runapp.c
+++ runapp.c
@@ -15,22 +15,47 @@
 #include <sys/poll.h>
 #include <signal.h>
 
+extern int errno;
+
 
 int
 main (int ac, char *av[])
 {
    sigset_t set;
+   int i;
+   FILE *fp;
+   char *newav[ac];
 
+
+   fp = fopen ("/tmp/runapp.log", "a");
+
+   fprintf (fp, "PATH = %s\n", getenv ("PATH"));
+   
+   for (i = 0; i < ac; i++) {
+      fprintf (fp, "runapp: av[%d] = '%s'\n", i, av[i]);
+   }
+
+   fflush (fp);
    
    if (ac < 2) {
       return (0);
    }
    
+   /* Copy the counted argv array to the NULL-terminated newav */
+   for (i = 1; i < ac; i++) {
+      newav[i-1] = av[i];
+   }
+
+   newav[ac-1] = NULL;
+
    /* Set up signals (turn off blocking!) */
    sigemptyset (&set);
    sigprocmask (SIG_SETMASK, &set, (sigset_t *) NULL);
 
    /* Overlay the child application */
-   execvp (av[1], &(av[1]));
+   execvp (newav[0], newav);
 
+   fprintf (fp, "runapp: failed! errno = %d\n", errno);
+   
+   fclose (fp);
 }
