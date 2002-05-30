72998ef775f8dd110a4a58d6bd507091929471d7
Krister Lagerstrom <krister-freevo@kmlager.com>
2002-05-30 06:56:04 +0000

    Initial checkin
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@161 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

---
+++ osd_server/osd_fb/ft.h
@@ -0,0 +1,9 @@
+#ifndef _FREEVO_FT_H
+#define _FREEVO_FT_H
+
+#include "portable.h"
+
+extern int ft_puts (char *pFontFilename, int ptsize, int x, int y, uint32 fgcol,
+                    unsigned char *str);
+
+#endif
