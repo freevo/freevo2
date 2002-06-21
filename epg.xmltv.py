99658433a6904341c5bb71a947452b46fe7f22f1
Aubin Paul <aubin@outlyer.org>
2002-06-21 05:28:34 +0000

    Removed all the private methods that were not used for XMLTV.
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@250 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- epg.xmltv.py
+++ epg.xmltv.py
@@ -35,55 +35,7 @@ FALSE = 0
 class Dummy:
     pass
 
-cached_guide = None
-
-
-#
-# Check if there's a recently cached guide, if not get a newer one
-#
 def get_guide():
-    global cached_guide
-
-    cached_guide = download_guide()
-
-    return cached_guide
-
-
-def cmp_channel_nums(channelnum, program):
-    if program[0] == channelnum:
-        return 1
-    else:
-        return 0
-    
-
-def filter_guide(programs):
-    all_channels = []
-    for chan in config.TV_CHANNELS:
-        f = lambda prog, c=int(chan): cmp_channel_nums(c, prog)
-        chan_progs = filter(f, programs)
-
-        if not chan_progs:
-            all_channels += [[(int(chan), '???', '???', 0)]]
-            continue
-        
-        tmp = []
-        chan_progs.reverse()
-        for p in chan_progs:
-            tmp += [p]
-            if p[3] <= time.time():
-                break
-
-        tmp.reverse()
-
-        if not tmp:
-            tmp = (chan, chan_progs[0][1], 'No schedule', 0)
-            
-        all_channels += [tmp]
-        
-    return all_channels
-
-        
-def download_guide():
 
     if os.path.isfile(config.XMLTV_FILE):
     	rawlines = programs = xmltv.read_programmes(open(config.XMLTV_FILE))
@@ -107,67 +59,6 @@ def download_guide():
 
     return guide
     
-    
-
-def get_info(str):
-    name, nr = get_channel(str)
-    start = get_starttime(str)
-    progname = get_progname(str)
-    ts = time.asctime(time.localtime(start))
-    #print '%3d: Channel: %-10s  Program: %-40s Time: %s' % (nr, name, progname, ts)
-    return (nr, name, progname, start)
-
-
-def get_channel(str):
-    start_pos = str.find('chname=')
-
-    if start_pos == -1:
-        return 'UNKNOWN'
-    else:
-        start_pos += len('chname=')
-        
-    middle_pos = str[start_pos:].find('+')
-
-    if middle_pos == -1:
-        return 'UNKNOWN'
-
-    end_pos = str[start_pos:].find('&')
-
-    if end_pos == -1:
-        return 'UNKNOWN'
-
-    name = str[start_pos:start_pos+middle_pos].replace('%26', '&')
-    nr = int(str[start_pos+middle_pos+1:start_pos+end_pos])
-    
-    return (name, nr)
-
-
-def get_starttime(str):
-    start_pos = str.find('progutn=')
-
-    if start_pos == -1:
-        return 0
-    else:
-        start_pos += len('progutn=')
-        
-    end_pos = str[start_pos:].find('&')
-
-    if end_pos == -1:
-        return 0
-
-    start_time = int(str[start_pos:start_pos+end_pos])
-    
-    return start_time
-
-
-def get_progname(str):
-
-    # Cut out the part inside the anchor tag
-    progname = str[str.rfind('>', 0, -4)+1:-4]
-    
-    return progname
-
-
 if __name__ == '__main__':
     g = get_guide()
 
@@ -175,7 +66,6 @@ if __name__ == '__main__':
     for channel in g.programs:
         for p in channel:
 		print p
-		#print p[3]
         	hh = time.localtime(p[3])[3]
         	mm = time.localtime(p[3])[4]
         	print '%3d %-10s %-30.30s %2d.%02d' % (p[0], p[1], p[2], hh, mm)
