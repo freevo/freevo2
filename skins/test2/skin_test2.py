40d48764377a36b1b65cf6127b8d1b1df73a15fb
Krister Lagerstrom <krister-freevo@kmlager.com>
2002-06-28 05:43:36 +0000

    Made skins more modular
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@267 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

---
+++ skins/test2/skin_test2.py
@@ -0,0 +1,127 @@
+#
+# skin_test2.py
+#
+# This is the Freevo test2 skin
+#
+# $Id$
+
+# Configuration file. Determines where to look for AVI/MP3 files, etc
+# Logging is initialized here, so it should be imported first
+import config
+
+import sys, socket, random, time, os
+
+# Various utilities
+import util
+
+# The mixer class, controls the volumes for playback and recording
+import mixer
+
+# The OSD class, used to communicate with the OSD daemon
+import osd
+
+# The RemoteControl class, sets up a UDP daemon that the remote control client
+# sends commands to
+import rc
+
+# Another skin that this one inherits some functions from
+sys.path += ['skins/test1']
+import skin_test1
+
+# Set to 1 for debug output
+DEBUG = 1
+
+TRUE = 1
+FALSE = 0
+
+###############################################################################
+
+# Set up the mixer
+mixer = mixer.get_singleton()
+
+# Create the remote control object
+rc = rc.get_singleton()
+
+# Create the OSD object
+osd = osd.get_singleton()
+
+
+class Skin(skin_test1.Skin):
+
+    # OSD background bitmap. Must be PNG.
+    # Format: (filename, x, y)  x=y=-1 means integer tiling
+
+    OSD_BGBITMAP = ('skins/test2/mainbg3.png', -1, -1)
+
+    bgbitmap = OSD_BGBITMAP
+    items_per_page = 13
+
+
+    # Called from the MenuWidget class to draw a menu page on the
+    # screen
+    def DrawMenu(self, menuw):
+        osd.clearscreen(osd.COL_WHITE)
+
+        menu = menuw.menustack[-1]
+
+        if not menu:
+            osd.drawstring('INTERNAL ERROR, NO MENU!', 100, osd.height/2)
+            return
+
+        if self.bgbitmap[0]:
+            apply(osd.drawbitmap, self.bgbitmap)
+        
+        # Menu heading
+        osd.drawstring(menu.heading, 160, 50,
+                       font=self.OSD_FONTNAME_HDR,
+                       ptsize=self.OSD_FONTSIZE_HDR)
+
+
+	#osd.drawbitmap('skins/test2/movies.png',80,80)
+
+        # Draw a box around the selection area
+        #osd.drawbox(40, 85, 720, 490, width=3,
+        #            color=osd.COL_BLACK)
+        
+        # Draw the menu choices for the main selection
+        x0 = 60
+        y0 = 100
+        selection_height = 390
+        if menu.packrows:
+            spacing = selection_height / menuw.items_per_page
+        else:
+            spacing = selection_height / max(len(menuw.menu_items),1)
+        for choice in menuw.menu_items:
+            if len(menuw.menustack) == 1:
+                ptscale = 2.0
+            else:
+                ptscale = 1.0
+            fontsize = self.OSD_FONTSIZE_ITEMS*ptscale
+            osd.drawstring(choice.name, (x0+115), y0,
+                           font=self.OSD_FONTNAME_ITEMS,
+                           ptsize=fontsize)
+	    if choice.icon != None: 
+	    	osd.drawbitmap(choice.icon, x0, y0)
+		#print type(choice.icon)
+		#print choice.icon
+            if menu.selected == choice:
+                osd.drawbox(x0 - 8, y0 - 3, 705, y0 + fontsize*1.5, width=-1,
+                            color=((160 << 24) | osd.COL_BLUE))
+            y0 += spacing
+
+        # Draw the menu choices for the meta selection
+        x0 = 40
+        y0 = 505
+        for item in menuw.nav_items:
+            fontsize = self.OSD_FONTSIZE_BTNS
+            osd.drawstring(item.name, x0, y0,
+                           font=self.OSD_FONTNAME_BTNS,
+                           ptsize=fontsize)
+            if menu.selected == item:
+                osd.drawbox(x0 - 4, y0 - 3, x0 + 150, y0 + fontsize*1.5,
+                            width=-1,
+                            color=((160 << 24) | osd.COL_BLUE))
+            x0 += 190
+
+        osd.update()
+        
