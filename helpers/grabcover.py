1a08414af714841ca939cd9e9f02dfb27d2a10c6
Aubin Paul <aubin@outlyer.org>
2002-07-12 04:53:27 +0000

    Added Usage, and the ability to specify an output file, since we should allow JPG cover images now. Should add that to main app too.
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@368 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- helpers/grabcover.py
+++ helpers/grabcover.py
@@ -12,6 +12,15 @@ if len(sys.argv) > 1:
 	fp = urllib2.urlopen(str(imgsrc))
 	# Make a seekable file object
 	img = cStringIO.StringIO(fp.read())
-	# Convert the image into a PNG and save to logos directory
-	output_file = 'cover.png'
+	# Convert the image
+	if len(sys.argv) < 3:
+		output_file = 'cover.png'
+	else:
+		output_file = sys.argv[2]
 	Image.open(img).save(output_file)
+else:
+	print "Usage: %s [URL] <output file>" % sys.argv[0]
+	print "Grab a cover image via HTTP, optionally convert it to JPG/PNG format"
+	print "if no <output_file> is specified, convert and write to 'cover.png'"
+	# Technically, we can convert to a lot more formats, but Freevo doesn't use them
+
