50ebd100e7b3fb12b1f21d47e0572db29e274217
Krister Lagerstrom <krister-freevo@kmlager.com>
2002-07-12 01:55:44 +0000

    Added DBG() macro. Added TRUE&FALSE defines.
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@361 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- osd_server/portable.h
+++ osd_server/portable.h
@@ -10,6 +10,21 @@ typedef float float32;
 #define OK 0
 #define ERROR -1
 
+#ifndef TRUE
+#define TRUE 1
+#endif
+
+#ifndef FALSE
+#define FALSE 0
+#endif
+
 #define ARRAY_LENGTH(a)  (sizeof(a)/sizeof(a[0]))
+
+#define DBG(str, args...) {                                                   \
+  char tmp[100000] ;                                                          \
+  sprintf (tmp, str, ## args);                                                \
+  printf ("%s:%s():%d: %s\n", __FILE__, __FUNCTION__, __LINE__, tmp);         \
+}
+
 
 #endif /* _FREEVO_PORTABLE_H_ */
