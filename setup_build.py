96492de9978d51a24dcc077ae3a68d0b8b82ff8e
Krister Lagerstrom <krister-freevo@kmlager.com>
2002-07-17 05:42:17 +0000

    Cleaned up, moved the code into different functions.
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@428 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- setup_build.py
+++ setup_build.py
@@ -7,84 +7,40 @@
 # $Id$
 #
 
-# XXX This file should probably be cleaned up to use functions instead.
-
 import sys, os
 
 
-#PYTHON_REQUIREDVERSION = '2.1'
-
-#print 'checking for Python version >= %s...' % PYTHON_REQUIREDVERSION,
-#if ((sys.version_info[0] < 2) or
-#    ((sys.version_info[0] == 2) and (sys.version_info[1] < 1))):
-#    args = (sys.version_info[0], sys.version_info[1], sys.version_info[2],
-#            PYTHON_REQUIREDVERSION)
-#    print '%s.%s.%s found, %s or later is required!' % args
-#    sys.exit()
-#else:
-#    print 'found'
-        
-
-print 'checking for Python XML...',
-try:
-    import xml
-except ImportError:
-    print 'not found, required!'
-    sys.exit()
-else:
-    print ' found'
-
-
-print 'checking for qp_xml...',
-try:
-    from xml.utils import qp_xml
-except ImportError:
-    print 'not found, required!'
-    sys.exit()
-else:
-    print 'found'
-    
-
-print 'checking for Python Imaging Library...',
-try:
-    import Image
-except ImportError:
-    print 'not found, required!'
-    sys.exit()
-else:
-    print 'found'
-
-
-# Build everything
-# XXX add more config flags, creating config files for python and C, etc.
-# XXX use getopt for interpreting the command line args
-# XXX add --help
-if len(sys.argv) > 1:
-    if sys.argv[1] == '--osd=x11':
-        buildops = 'x11'
-    elif sys.argv[1] == '--osd=sdl':
-        buildops = 'sdl'
-    elif sys.argv[1] == '--osd=fb':
-        buildops = ''
-    else:
-        print
-        print 'Usage: ./configure [--osd=fb | --osd=x11 | --osd=sdl]'
+def check_deps():
+    print 'checking for Python XML...',
+    try:
+        import xml
+    except ImportError:
+        print 'not found, required!'
         sys.exit()
-else:
-    print
-    print 'Defaulting build to "--osd=fb"'
-    buildops = ''
-    
+    else:
+        print ' found'
 
-# First clean up from the last build, if any
-print
-print 'Cleaning up from previous build...',
-os.system('make -f Makefile.in clean > /dev/null')
-print 'done'
 
-print 'Creating Makefile (buildops="%s")...' % buildops,
+    print 'checking for qp_xml...',
+    try:
+        from xml.utils import qp_xml
+    except ImportError:
+        print 'not found, required!'
+        sys.exit()
+    else:
+        print 'found'
 
-# Create a new Makefile
+
+    print 'checking for Python Imaging Library...',
+    try:
+        import Image
+    except ImportError:
+        print 'not found, required!'
+        sys.exit()
+    else:
+        print 'found'
+
+
 makefile = """
 all:
 \tmake -f Makefile.in %s
@@ -94,13 +50,59 @@ install:
 
 clean:
 \tmake -f Makefile.in clean
-""" % buildops
+"""
 
-os.system('echo "%s" > Makefile' % makefile)
+def create_makefile(buildops):
 
-print 'done'
+    # First clean up from the last build, if any
+    print
+    print 'Cleaning up from previous build...',
+    os.system('make -f Makefile.in clean > /dev/null')
+    print 'done'
 
-print
-print 'Now you can type "make" to build and "make install" as root'
-print 'to put the binaries into /usr/local/freevo or run them from here'
-print
+    print 'Creating Makefile (buildops="%s")...' % buildops,
+
+    # Create a new Makefile
+
+    os.system('echo "%s" > Makefile' % (makefile % buildops))
+
+    print 'done'
+
+    
+def print_usage():
+    print 'Usage: ./configure [--osd=fb | --osd=x11 | --osd=sdl]'
+
+    
+if __name__ == '__main__':
+
+    if len(sys.argv) >= 2 and sys.argv[1] == '--help':
+        print_usage()
+        sys.exit()
+
+    check_deps()
+    
+    # Build everything
+    # XXX add more config flags, creating config files for python and C, etc.
+    # XXX use getopt for interpreting the command line args
+    # XXX add --help
+    if len(sys.argv) > 1:
+        if sys.argv[1] == '--osd=x11':
+            buildops = 'x11'
+        elif sys.argv[1] == '--osd=sdl':
+            buildops = 'sdl'
+        elif sys.argv[1] == '--osd=fb':
+            buildops = ''
+        else:
+            print_usage()
+            sys.exit()
+    else:
+        print
+        print 'Defaulting build to "--osd=fb"'
+        buildops = ''
+
+    create_makefile(buildops)
+    
+    print
+    print 'Now you can type "make" to build and "make install" as root'
+    print 'to put the binaries into /usr/local/freevo or run them from here'
+    print
