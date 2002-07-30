288571f3db54d01c180a3f0deea3ae8debdc96d6
Dirk Meyer <dischi@freevo.org>
2002-07-30 18:28:26 +0000

    Added IMDb copyright information into the XML file and a copyright warning for images on stdout. Also new is unicode support, '&'-correction (&amp;) and a parameter to add the cd id when building the XML file.
    Problem: I wanted to use the Sherlock interface from IMDb, but
    r.read() doesn't work. I don't understand why.
    
    
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@505 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- helpers/imdb.py
+++ helpers/imdb.py
@@ -8,14 +8,38 @@ import re
 import httplib, urllib
 import sys
 import string
+import codecs
+
+
+freevo_version = '1.2.5'
+
+
+#
+# Return the CD ID for cd in the drive
+#
+
+def getCDID(drive):
+    img = open(drive)
+    img.seek(0x0000832d)
+    id = img.read(16)
+    img.close()
+    return id
+    
+#
+# make the string XML valid
+#
+
+def str2XML(line):
+    return unicode(string.replace(line, "&", "&amp;"), 'latin-1')
+    
 
 try:
     imdb_number = sys.argv[1]
     filename = sys.argv[2]
 except IndexError:
-    print "Usage: imdb.py [IMDB-NUMBER] [OUTPUT_FILE] [MOVIEFILE(S)]"
+    print "Usage: imdb.py [IMDB-NUMBER] [OUTPUT_FILE] [-id DRIVE] [MOVIEFILE(S)]"
     print "       imdb.py -s [SEARCH_STRING]"
-    print "       imdb.py -id [DRIVE] [XML_FILE]"
+    print "       imdb.py --add-id [DRIVE] [XML_FILE]"
     print
     print "Generate XML data that stores extra IMDB information for use"
     print "in the movie browser."
@@ -24,8 +48,11 @@ except IndexError:
     print "  IMDB title number for this movie"
     print
     print "OUTPUT_FILE:"
-    print "  The script will generate OUTPUT_FILE.xml fot the IMDB data"
+    print "  The script will generate OUTPUT_FILE.xml for the IMDB data"
     print "  and OUTPUT_FILE.jpg for the image (if possible)"
+    print
+    print "-id [DRIVE]"
+    print "  Stores the DVD/VCD/CD id to the XML file"
     print
     print "MOVIE_FILE(S):"
     print "  One or more files belonging to this movie"
@@ -36,24 +63,18 @@ except IndexError:
     print "-s [SEARCH_STRING]"
     print "  Search IMDB to get the IMDB_NUMBER"
     print
-    print "-id [DRIVE] [XML_FILE]"
-    print "  Stores the DVD/VCD/CD id to the XML file"
+    print "--add-id [DRIVE] [XML_FILE]"
+    print "  Adds the DVD/VCD/CD id to the XML file"
     sys.exit(1)
 
 
-# add ID tag
-if imdb_number == '-id':
-    img = open(filename)
-    img.seek(0x0000832d)
-    id = img.read(16)
-    img.seek(32808, 0)
-    label = img.read(32)
-    m = re.compile("^(.*[^ ]) *$").match(label)
-    if m:
-        label = m.group(1)
-    img.close()
-    print 'adding id=id%s' % id
-    print label
+
+#
+# add ID tag to exiting XML file
+#
+
+if imdb_number == '--add-id':
+    id = getCDID(filename)
 
     x = open(sys.argv[3])
     content = x.read()
@@ -64,12 +85,35 @@ if imdb_number == '-id':
     x.write(content)
     x.close()
     sys.exit(0)
-    
-# connect to imdb 
-conn = httplib.HTTPConnection("us.imdb.com")
 
-# let's lie about the user agent, without user-agent the return is 403
-headers = { 'User-Agent': 'Netscape 3.0' }
+
+
+#
+# Some init stuff
+#
+    
+title = year = genre = tagline = plot = rating = image = ""
+
+regexp_title   = re.compile('.*STRONG CLASS="title">(.*?)<')
+regexp_year    = re.compile('.*<A HREF="/Sections/Years/.*?([0-9]*)<')
+regexp_genre   = re.compile('.*<B CLASS="ch">Genre.*?</B>(.*)<BR>')
+regexp_tagline = re.compile('.*<B CLASS="ch">Tagline.*?</B>(.*?)<')
+regexp_plot    = re.compile('.*<B CLASS="ch">Plot Outline.*?</B>(.*?)<')
+regexp_rating  = re.compile('.*<B>([0-9\.]*)</B>/10 (.[0-9]* votes.)')
+regexp_image   = re.compile('.*(http.*?)" ALT="cover"')
+
+
+# connect to IMDb 
+conn = httplib.HTTPConnection("www.imdb.com")
+
+# set user agent
+headers = { 'User-Agent': 'freevo %s (%s)' % (freevo_version, sys.platform) }
+
+
+
+#
+# Search IMDb for a title
+#
 
 if imdb_number == '-s':
     print "searching " + filename
@@ -86,6 +130,23 @@ if imdb_number == '-s':
     sys.exit(0)
 
 
+    # the /Sherlock? way, but it doesn't work
+    conn.request("GET", "/Sherlock?"+filename, "", headers)
+    r = conn.getresponse()
+    if r.status != 200:
+        print r.status
+        print r.reason
+        sys.exit(1)
+
+    # this doesn't work, why????
+    print r.read()
+
+
+
+#
+# Get and parse the information from the site
+#
+
 conn.request("GET", "/Title?"+imdb_number, "", headers)
 r = conn.getresponse()
 if r.status != 200:
@@ -93,22 +154,10 @@ if r.status != 200:
     print r.reason
     sys.exit(1)
     
-title = year = genre = tagline = plot = rating = image = ""
-
-regexp_title   = re.compile('.*STRONG CLASS="title">(.*?)<')
-regexp_year    = re.compile('.*<A HREF="/Sections/Years/.*?([0-9]*)<')
-regexp_genre   = re.compile('.*<B CLASS="ch">Genre.*?</B>(.*)<BR>')
-regexp_tagline = re.compile('.*<B CLASS="ch">Tagline.*?</B>(.*?)<')
-regexp_plot    = re.compile('.*<B CLASS="ch">Plot Outline.*?</B>(.*?)<')
-regexp_rating  = re.compile('.*<B>([0-9\.]*)</B>/10 (.[0-9]* votes.)')
-regexp_image   = re.compile('.*(http.*?)" ALT="cover"')
-
-
-
 for line in r.read().split("\n"):
 
     m = regexp_title.match(line)
-    if m: title = m.group(1)
+    if m: title = str2XML(m.group(1))
 
     m = regexp_year.match(line)
     if m: year = m.group(1)
@@ -120,10 +169,11 @@ for line in r.read().split("\n"):
             elif g != "" and g != "(more)": genre += " / "+ g
 
     m = regexp_tagline.match(line)
-    if m: tagline = re.compile('[\t ]+').sub(" ", ' ' + m.group(1))[1:]
+    if m:
+        tagline = str2XML(re.compile('[\t ]+').sub(" ", ' ' + m.group(1))[1:])
         
     m = regexp_plot.match(line)
-    if m: plot = re.compile('[\t ]+').sub(" ", ' ' + m.group(1))[1:]
+    if m: plot = str2XML(re.compile('[\t ]+').sub(" ", ' ' + m.group(1))[1:])
         
     m = regexp_rating.match(line)
     if m: rating = m.group(1) + '/10 ' + m.group(2)
@@ -141,6 +191,10 @@ for line in r.read().split("\n"):
         conn.request("GET", "/"+url.group(2), "", headers)
         r = conn.getresponse()
         if r.status == 200:
+            print "Downloaded cover image from %s" % url.group(1)
+            print "Freevo knows nothing about the copyright of this image, please"
+            print "go to %s to check for more informations about private." % url.group(1)
+            print "use of this image"
             i = open(filename + ".jpg", 'w')
             i.write(r.read())
             i.close()
@@ -153,22 +207,48 @@ for line in r.read().split("\n"):
         conn.close()
 
 
-i = open(filename + ".xml", 'w')
+#
+# Now write the output file
+#
+
+i = codecs.open(filename + ".xml", 'w', encoding='utf-8')
 i.write( "\
 <?xml version=\"1.0\" ?>\n\
 <freevo>\n\
+  <copyright>\n\
+    The information in this file are from the Internet Movie Database (IMDb).\n\
+    Please visit http://www.imdb.com for more informations.\n\
+  </copyright>\n\
   <movie>\n\
-    <title>"+title+"</title>\n\
-    <cover>"+image+"</cover>\n\
-    <video>\n")
+    <title>"+title+"</title>\n")
 
 index = 3
 
+# insert ID?
+
 try:
-    if sys.argv[3] == "-vcd":
+    if sys.argv[3] == "-id":
+        i.write("    <id>%s</id>\n" % getCDID(sys.argv[4]))
+        index += 2
+except IndexError:
+    pass
+
+# insert image?
+
+if image:
+    i.write("    <cover source=\"%s\">%s</cover>\n" % (url.group(1), str2XML(image)))
+
+
+
+# insert filenames and video type
+
+i.write("    <video>\n")
+
+try:
+    if sys.argv[index] == "-vcd":
         i.write("      <vcd/>\n")
         index = 0
-    if sys.argv[3] == "-dvd":
+    if sys.argv[index] == "-dvd":
         i.write("      <dvd/>\n")
         index = 0
     if sys.argv[3] == "-cd":
@@ -182,12 +262,15 @@ if index > 0:
 
     try:
         while 1:
-            i.write( "        <filename>"+sys.argv[index]+"</filename>\n")
+            i.write( "        <filename>"+str2XML(sys.argv[index])+"</filename>\n")
             index += 1
     except IndexError:
         pass
 
     i.write("      </files>\n")
+
+
+# insert more IMDb info
 
 i.write("\
     </video>\n\
