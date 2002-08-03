e9b4db35817840d1c05e82a2cdc27020b2e196b0
Krister Lagerstrom <krister-freevo@kmlager.com>
2002-08-03 20:16:08 +0000

    Reindented to 2 spaces instead of 3. Added debug output for the commandline for easier manual testing.
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@552 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- runapp.c
+++ runapp.c
@@ -23,52 +23,63 @@ extern int errno;
 int
 main (int ac, char *av[])
 {
-   sigset_t set;
-   int i;
-   FILE *fp;
-   char *newav[ac];
-   struct stat statbuf;
-   char logfile[256];
-   
+  sigset_t set;
+  int i;
+  FILE *fp;
+  char *newav[ac];
+  struct stat statbuf;
+  char logfile[256];
+  char cmd_str[1000];
+  
+  
+  /* Does the logdir exist? */
+  if (!stat (RUNAPP_LOGDIR, &statbuf)) {
+    sprintf (logfile, "%s/internal-runapp.log", RUNAPP_LOGDIR);
+  } else {
+    /* No, log to the /tmp/freevo dir instead */
+    sprintf (logfile, "/tmp/freevo/internal-runapp.log");
+    mkdir ("/tmp/freevo", 0777); /* Make sure the dir exists, ignore errors */
+  }
+  
+  fp = fopen (logfile, "a");
+  
+  fprintf (fp, "PATH = %s\n", getenv ("PATH"));
+  
+  cmd_str[0] = 0;
+  
+  for (i = 0; i < ac; i++) {
+    char tmp[256];
 
-   /* Does the logdir exist? */
-   if (!stat (RUNAPP_LOGDIR, &statbuf)) {
-     sprintf (logfile, "%s/internal-runapp.log", RUNAPP_LOGDIR);
-   } else {
-     /* No, log to the /tmp/freevo dir instead */
-     sprintf (logfile, "/tmp/freevo/internal-runapp.log");
-     mkdir ("/tmp/freevo", 0777); /* Make sure the dir exists, ignore errors */
-   }
-   
-   fp = fopen (logfile, "a");
 
-   fprintf (fp, "PATH = %s\n", getenv ("PATH"));
-   
-   for (i = 0; i < ac; i++) {
-      fprintf (fp, "runapp: av[%d] = '%s'\n", i, av[i]);
-   }
+    sprintf (tmp, "%s ", av[i]);
+    strcat (cmd_str, tmp);
+    
+    fprintf (fp, "runapp: av[%d] = '%s'\n", i, av[i]);
+  }
 
-   fflush (fp);
-   
-   if (ac < 2) {
-      return (0);
-   }
-   
-   /* Copy the counted argv array to the NULL-terminated newav */
-   for (i = 1; i < ac; i++) {
-      newav[i-1] = av[i];
-   }
-
-   newav[ac-1] = NULL;
-
-   /* Set up signals (turn off blocking!) */
-   sigemptyset (&set);
-   sigprocmask (SIG_SETMASK, &set, (sigset_t *) NULL);
-
-   /* Overlay the child application */
-   execvp (newav[0], newav);
-
-   fprintf (fp, "runapp: failed! errno = %d\n", errno);
-   
-   fclose (fp);
+  fprintf (fp, "Command: '%s'\n", cmd_str);
+  
+  fflush (fp);
+  
+  if (ac < 2) {
+    return (0);
+  }
+  
+  /* Copy the counted argv array to the NULL-terminated newav */
+  for (i = 1; i < ac; i++) {
+    newav[i-1] = av[i];
+  }
+  
+  newav[ac-1] = NULL;
+  
+  /* Set up signals (turn off blocking!) */
+  sigemptyset (&set);
+  sigprocmask (SIG_SETMASK, &set, (sigset_t *) NULL);
+  
+  /* Overlay the child application */
+  execvp (newav[0], newav);
+  
+  fprintf (fp, "runapp: failed! errno = %d\n", errno);
+  
+  fclose (fp);
 }
