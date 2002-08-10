cb948a2772315030c6fe47655f850d02fc72e996
Dirk Meyer <dischi@freevo.org>
2002-08-10 17:40:18 +0000

    Changes the matroxset parameters to my current settings which IMHO looks better than the old settings. I don't know what I'm doing there, the code is taken from mplayer, I should enable cloning mode.
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@600 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- matrox_g400/mga_pal_768x576.sh
+++ matrox_g400/mga_pal_768x576.sh
@@ -1,43 +1,31 @@
 #!/bin/sh
 
 #
-# This script was posted on the mplayer-matrox mailing list by
-# Brian Hall, http://www.bigfoot.com/~brihall
+# This script is a mix from the mplayer TVout scripts and some
+# changes by me to support 768 width
 #
 # Note: fbset and matroxset are copied from mplayer! They're 
 # included here for ease of use, but check out the mplayer
 # source for updates if you have problems. There are more
 # init scripts there, setup utilities, etc.
 #
-# XXX I have not tested this with a monitor on vga-connector 1,
-# I'm only using a TV on vga-connector 2
-# /Krister
+# /Dischi
 
-# disconnect both heads
-./matroxset/matroxset -f /dev/fb0 -m 0 > /dev/null 2> /dev/null
-./matroxset/matroxset -f /dev/fb1 -m 0 > /dev/null 2> /dev/null
+# goto the matrox_g400 dir
+cd `dirname $0`
 
-# swap heads
-./matroxset/matroxset -f /dev/fb0 -m 2 > /dev/null 2> /dev/null
-./matroxset/matroxset -f /dev/fb1 -m 1 > /dev/null 2> /dev/null
-./matroxset/matroxset -f /dev/fb0 2 2 > /dev/null 2> /dev/null
+# I don't want this is doing exacly, but it should enable the
+# clone mode with IMHO looks better than the others
 
-#
-# The following is a modeline for setting up NTSC on the TV output (vga 2) on 
-# a matrox dual-head card.
+matroxset -f /dev/fb1 -m 0
+matroxset -f /dev/fb0 -m 3
 
-./fbset/fbset -fb /dev/fb0 -left 23 -right -5 -upper 39 -lower 10 -hslen 46 -vslen 4 -xres 768 -yres 576 -vxres 768 -vyres 576 -depth 32 -laced false -bcast true > /dev/null 2> /dev/null
+# switch to PAL
+matroxset 1
 
-
-# Set up a regular VGA monitor on vga connector 1. 
-#
-# Note: I don't think it is possible to use high resolutions since the 
-# hardware graphics acceleration is used for connector 2 after running 
-# this script. That means that it'll be hard to run a decent X11 
-# session.
-./fbset/fbset -db fbset.db -fb /dev/fb1 "640x480-60" > /dev/null 2> /dev/null
+# set the 768x576 output
+fbset -fb /dev/fb0 -left 23 -right -5 -upper 39 -lower 10 -hslen 46 -vslen 4 \
+	-xres 768 -yres 576 -vxres 768 -vyres 576 -depth 32 -laced false -bcast true
 
 
-# switch to pal
-
-./matroxset/matroxset 1
+setterm -blank 0
