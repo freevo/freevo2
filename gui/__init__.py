383a165506b5a931fd295560dd1257d5693c31ee
Thomas Malt <thomas@malt.no>
2002-08-18 22:15:20 +0000

    o Moved debug and test code to its own file.
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@748 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- gui/__init__.py
+++ gui/__init__.py
@@ -8,16 +8,20 @@
 # idea is that skins can use or inherit from this class and override
 # when needed.
 #
-# Todo:  - Implement "listbox" for selections with different styles
+# Todo:  o Implement "listbox" for selections with different styles
 #          for showing what is selected (glow, bg_color, etc)
 #             ListItem -> Label -> Text
-#        - Make GraphBar class.
-#        - Split classes into separate files.
-#        - Make more expetions.
-#        * Find a way to do Z-index handling.
+#        o Make GraphBar class.
+#        o Make more excpetions.
+#
+# Done:  * Find a way to do Z-index handling.
+#        * Split classes into separate files.
 #
 #-----------------------------------------------------------------------
 # $Log$
+# Revision 1.2  2002/08/18 22:15:20  tfmalt
+# o Moved debug and test code to its own file.
+#
 # Revision 1.1  2002/08/15 22:45:42  tfmalt
 # o Inital commit of Freevo GUI library. Files are put in directory 'gui'
 #   under Freevo.
@@ -77,11 +81,8 @@ import os.path
 sys.path.append(os.path.abspath('.'))
 sys.path.append(os.path.abspath('..'))
 
-
-import time 
 import gui.ZIndexRenderer
 
-from pygame.locals  import *
 from gui.Border     import *
 from gui.Color      import *
 from gui.GUIObject  import *
@@ -90,163 +91,10 @@ from gui.Label      import *
 from gui.exceptions import *
 
 DEBUG = 0
+if DEBUG:
+    from gui.debug import *
 
 osd = osd_sdl.get_singleton()
 zir = gui.ZIndexRenderer.get_singleton()
 
-
-
-# ======================================================================
-# XXX These functions are here for debug.. I'll remove them in time.
-# ======================================================================
-clock = pygame.time.Clock()
-def wait_loop():
-    while 1:
-        clock.tick(20)
-        for event in pygame.event.get():
-            if event.type == KEYDOWN:
-                return
-
-
-def save_images( pb1, pb2, pb3 ):
-    if pb1.bg_image:
-        pygame.image.save( pb1.bg_image, '/tmp/bp1.bmp' )
-    if pb2.bg_image:
-        pygame.image.save( pb2.bg_image, '/tmp/bp2.bmp' )
-    if pb3.bg_image:
-        pygame.image.save( pb3.bg_image, '/tmp/bp3.bmp' )
-
-def save_image( obj ):
-    if obj.bg_image:
-        pygame.image.save( obj.bg_image,
-                           '/tmp/'+str(obj.__class__.__name__)+'.bmp')
-    elif obj.parent.bg_image:
-        pygame.image.save( obj.parent.bg_image,
-                           '/tmp/parent'+str(obj.__class__.__name__)+'.bmp')
-        
-
-# ======================================================================
-# Main: used for testing.
-# ======================================================================
-
-if __name__ == '__main__':
-    
-    osd.clearscreen()
-    
-    osd.drawbitmap( 'skins/images/aubin_bg1.png' )
-    osd.drawstring('Hello Sailor' + str(time.time()), 10, 10,
-                   font='skins/fonts/Arial_Bold.ttf',ptsize=14)
-    
-    osd.update()
-
-    pb  = osd_sdl.SynchronizedObject( PopupBox(100, 100, 300, 150,
-                                               'Hello again sailor',
-                                               bg_color=Color((255,0,0,128)),
-                                               border=Border.BORDER_FLAT,
-                                               bd_width=1)
-                                      )
-
-    pb2 = osd_sdl.SynchronizedObject( PopupBox(200, 150, 300, 150,
-                                               'Hello twice sailor',
-                                               bg_color=Color((0,255,0,128)),
-                                               border=Border.BORDER_FLAT,
-                                               bd_width=2)
-                                      )
-                                      
-    pb3 = osd_sdl.SynchronizedObject( PopupBox(300, 200, 300, 150,
-                                               'Hello trice sailor',
-                                               bg_color=Color((0,0,192,128)),
-                                               border=Border.BORDER_FLAT,
-                                               bd_width=3) )
-    
-    pb4 = osd_sdl.SynchronizedObject( PopupBox(200, 200, 300, 150,
-                                               'Hello trice sailor',
-                                               bg_color=Color((255,128,0,128)),
-                                               border=Border.BORDER_FLAT) )
-    
-
-    print "Showing first rectangle..."
-    pb.show()
-    save_images( pb, pb2, pb3 )
-    osd.update()
-                                      
-    print "  Waiting at bottom of cycle..."
-    wait_loop()
-
-    print "Showing second rectangle..."
-    pb2.show()
-    save_images( pb, pb2, pb3 )
-    osd.update()
-    print "  Waiting at bottom of cycle..."
-    wait_loop()
-
-    print "Showing third rectangle..."
-    pb3.show()
-    save_images( pb, pb2, pb3 )
-    osd.update()
-    print "  Waiting at bottom of cycle..."
-    wait_loop()
-
-    print "Showing fourth rectangle..."
-    pb4.show()
-    save_images( pb, pb2, pb3 )
-    osd.update()
-    print "  Waiting at bottom of cycle..."
-    wait_loop()
-
-    print "Hiding second rectangle..."
-    pb2.hide()
-    save_images( pb, pb2, pb3 )
-    osd.update()
-    print "  Waiting at bottom of cycle..."
-    wait_loop()
-
-    print "Showing second rectangle..."
-    pb2.show()
-    save_images( pb, pb2, pb3 )
-    osd.update()
-    print "  Waiting at bottom of cycle..."
-    wait_loop()
-
-    print "Hiding first rectangle..."
-    pb.hide()
-    save_images( pb, pb2, pb3 )
-    osd.update()
-    print "  Waiting at bottom of cycle..."
-    wait_loop()
-
-    print "Showing first rectangle..."
-    pb.show()
-    save_images( pb, pb2, pb3 )
-    osd.update()
-    print "  Waiting at bottom of cycle..."
-    wait_loop()
-
-    print "Hiding second rectangle..."
-    pb2.hide()
-    save_images( pb, pb2, pb3 )
-    osd.update()
-    print "  Waiting at bottom of cycle..."
-    wait_loop()
-
-    print "Showing second rectangle..."
-    pb2.show()
-    save_images( pb, pb2, pb3 )
-    osd.update()
-    print "  Waiting at bottom of cycle..."
-    wait_loop()
-
-    print "Hiding first rectangle..."
-    pb.hide()
-    save_images( pb, pb2, pb3 )
-    osd.update()
-    print "  Waiting at bottom of cycle..."
-    wait_loop()
-
-    print "Hiding SECOND rectangle..."
-    pb2.hide()
-    save_images( pb, pb2, pb3 )
-    osd.update()
-    print "  Waiting at bottom of cycle..."
-    wait_loop()
 
