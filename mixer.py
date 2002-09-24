1eb68575f25eb3f530290bfb7eb1d155aab052e1
Gustavo Sverzut Barbieri <gsbarbieri@yahoo.com.br>
2002-09-24 00:39:56 +0000

    Now mixer doesn't change all the volumes to zero when a new class is intantiated. That was annoying when you use CONTROL_ALL_AUDIO = 0.
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@937 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- mixer.py
+++ mixer.py
@@ -10,6 +10,10 @@
 #
 # ----------------------------------------------------------------------
 # $Log$
+# Revision 1.9  2002/09/24 00:39:56  gsbarbieri
+# Now mixer doesn't change all the volumes to zero when a new class is intantiated.
+# That was annoying when you use CONTROL_ALL_AUDIO = 0.
+#
 # Revision 1.8  2002/08/18 04:24:17  krister
 # Redirected stdout+stderr to /dev/null when running aumix to get rid of all the annoying error messages. Please test this since I don't use Alsa.
 #
@@ -95,10 +99,11 @@ class Mixer:
         self.igainVolume  = 0 # XXX Used on SB Live
         self.ogainVolume  = 0 # XXX Ditto
         
-        self.setMainVolume(self.mainVolume)
-        self.setPcmVolume(self.pcmVolume)
-        self.setLineinVolume(self.lineinVolume)
-        self.setMicVolume(self.micVolume)
+	# Gustavo: By default, don't change the values to zero
+        # self.setMainVolume(self.mainVolume)
+        # self.setPcmVolume(self.pcmVolume)
+        # self.setLineinVolume(self.lineinVolume)
+        #  self.setMicVolume(self.micVolume)
 
         if self.mixfd:
             data = struct.pack( 'L', self.SOUND_MASK_LINE )
