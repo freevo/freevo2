0ec91d3d5d0c54c5d9370608e5bb18ef59c10599
Krister Lagerstrom <krister-freevo@kmlager.com>
2002-09-29 18:40:48 +0000

    Added DOCTYPE. You are now able to choose which channels to retrieve. Minor bugfixes.
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@960 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- contrib/PerWigren/dagenstv2xmltv.py
+++ contrib/PerWigren/dagenstv2xmltv.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# dagenstv2xmltv v0.8 by Per Wigren <wigren@open-source.nu>
+# dagenstv2xmltv v0.81 by Per Wigren <wigren@open-source.nu>
 #
 # A program to get TV programs from the Swedish site http://www.dagenstv.com
 # and save them in a XMLTV-file.
@@ -8,6 +8,23 @@
 # Yes, it's ugly, but it works!! ;)
 #
 ######################################################
+#
+# v0.7:  * Initial public release
+# v0.8:  * Swapped order of <category> and <desc>
+#        * Convert & to &amp;
+#        * Remove strange ctrl+G chars that appear in some fields
+#          for some very strange reason...
+#        * Save xml in unicode, utf-8
+# v0.81: * Indention-typo in create_xml: The lists were sorted
+#          for every program instead of once when all programs are
+#          added.. HUGE speedup!
+#        * Added this changelog ;)
+# v0.85: * Added DOCTYPE
+#        * You are now able to choose which channels to retrieve
+#        * Minor bugfixes
+#
+######################################################
+
 
 import sys,os,re,urllib,time
 
@@ -19,15 +36,21 @@ try:
 	lang=sys.argv[2]
 	file=sys.argv[3]
 except:
-	print "Usage:", sys.argv[0], "<days> <lang> <filename>"
-	print "  days      Number of days of data to retrieve"
-	print "  lang      sv for Swedish, no for Norvegian"
-	print "  filename  Filename of XMLTV .xml-file"
+	print "Usage:", sys.argv[0], "<days> <lang> <filename> [channel-ids]"
+	print "  days         Number of days of data to retrieve"
+	print "  lang         sv for Swedish, no for Norvegian"
+	print "  filename     Filename of XMLTV .xml-file"
+	print "  channel-ids  Comma-separaded list of channel-IDs"
 	sys.exit(5)
 
 if not lang in ("sv","no"):
 	print "Bad language:", lang
 	sys.exit(5)
+
+to_get=None
+if len(sys.argv)>4:
+	to_get=sys.argv[4].split(",")
+
 
 # Get a list of dates to fetch
 def get_dates(days):
@@ -38,6 +61,10 @@ def get_dates(days):
 		now += 86400
 	return temp
 dates=get_dates(days)
+
+def tomorrow(date):
+	temp = time.mktime(time.strptime(date,"%Y-%m-%d"))
+	return time.strftime("%Y-%m-%d",time.localtime(temp+86400))
 
 # Return URL to retrieve
 def url(dat="",cha="",cat=""):
@@ -112,6 +139,8 @@ def get_cha_cat():
 
 # Convert date and time to xmltv-format
 def xtime(date,stime):
+        if int(stime.replace(":","")) < 600:
+		date = tomorrow(date)
 	temp = date+stime
 	return temp.replace("-","").replace(":","")+"00 +0100"
 
@@ -213,11 +242,21 @@ if cha==None:
 	print "Error retrieving channels- and categories-information"
 	sys.exit(10)
 
+if to_get:
+	channels=[]
+	for (name,id) in cha.items():
+		if id in to_get:
+			channels.append(name)
+		
+		
+else:
+	channels=cha.keys()
+
 program={}
 
 # Retrieve and parse all pages...
 for date in dates:
-	for name in cha.keys():
+	for name in channels:
 		printinfo( "P "+date+"  "+name )
 		for prog in parse_channel_data(url(date,cha[name],""),date):
 			program[prog['name']]=prog
@@ -240,12 +279,16 @@ def create_xml(program):
 	prog_keys=[] ; cha_keys=[]
 	for key in program.keys():
 		prog_keys.append( (program[key]['start'],key) )
-		prog_keys.sort()
+	prog_keys.sort()
 	for key in cha.keys():
 		cha_keys.append( (key.lower(), key) )
-		cha_keys.sort()
+	cha_keys.sort()
 
-	x ='<tv generator-info-name="dagenstv2xmltv by Per Wigren - wigren@open-source.nu"'
+	x ='<?xml version="1.0" encoding="ISO-8859-1"?>\n'
+	x+='<!DOCTYPE tv SYSTEM "xmltv.dtd">\n'
+
+
+	x+='<tv generator-info-name="dagenstv2xmltv by Per Wigren - wigren@open-source.nu"'
 	x+=' source-info-url="http://www.dagenstv.com" source-info-name="Dagens TV"'
 	x+=' source-data-url="http://www.dagenstv.com">\n'
 	for key in cha_keys:
@@ -281,5 +324,5 @@ def create_xml(program):
 	return x
 
 # Main program ;-)
-writexml( unicode(create_xml(program),'latin-1').encode("utf-8") )
+writexml( unicode(create_xml(program),'latin-1').encode("iso-8859-1") )
 
