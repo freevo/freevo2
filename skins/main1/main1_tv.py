d31c11c3f9777168daab09b7a7cda5ce84134383
Dirk Meyer <dischi@freevo.org>
2002-10-16 19:40:19 +0000

    Now the table looks ok for any height/font combination. Also fixed a float->int bug that caused one pixel spaces here and there. Some cleanups
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@1139 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- skins/main1/main1_tv.py
+++ skins/main1/main1_tv.py
@@ -9,6 +9,10 @@
 #
 # -----------------------------------------------------------------------
 # $Log$
+# Revision 1.2  2002/10/16 19:40:19  dischi
+# Now the table looks ok for any height/font combination. Also fixed a
+# float->int bug that caused one pixel spaces here and there. Some cleanups
+#
 # Revision 1.1  2002/10/16 04:58:16  krister
 # Changed the main1 skin to use Gustavos new extended menu for TV guide, and Dischis new XML code. grey1 is now the default skin, I've tested all resolutions. I have not touched the blue skins yet, only copied from skin_dischi1 to skins/xml/type1.
 #
@@ -68,15 +72,8 @@ class Skin_TV:
 
 
     def DrawTVGuide_Clear(self, settings):
-        osd.clearscreen(osd.COL_BLACK)
 
-        if settings.background.image:
-            apply(osd.drawbitmap, (settings.background.image, -1, -1))
-
-        if settings.background.mask:
-            osd.drawbitmap(settings.background.mask,-1,-1)
-
-        DrawLogo(settings.logo)
+        InitScreen(settings)
 
         # Show title and date
         s = time.strftime('%m/%d %H:%M')
@@ -111,17 +108,17 @@ class Skin_TV:
         items = -1
         val = settings.listing 
 
-        str_w_head, str_h_head = osd.stringsize('Ajg', val.head.font, val.head.size)
+        str_w_label, str_h_label = osd.stringsize('Ajg', val.label.font, val.label.size)
         str_w_selection, str_h_selection = \
                          osd.stringsize('Ajg', val.selection.font, val.selection.size)
         str_w_normal, str_h_normal = osd.stringsize('Ajg', val.font, val.size)
 
-        str_h = max (str_h_head, str_h_selection, str_h_normal )
+        str_h = max (str_h_label, str_h_selection, str_h_normal )
 
         if self.DrawTVGuide_getExpand(settings) == 0:
-            height = val.height - str_h_head - 2 * val.spacing
+            height = val.height - str_h_label - 2 * val.spacing
         else:
-            height = val.expand.height - str_h_head - 2 * val.spacing
+            height = val.expand.height - str_h_label - 2 * val.spacing
 
         items = height / ( str_h + 2 * val.spacing )
 
@@ -143,14 +140,14 @@ class Skin_TV:
             conf_h = val.expand.height
 
 
-        str_w_head = str_h_head = 0
+        str_w_label = str_h_label = 0
         str_w_selection = str_h_selection = 0
         str_w_normal = str_h_normal = 0
         val.left_arrow_size = val.right_arrow_size = (0, 0)
         n_cols = len(to_listing[0])-1
         col_time = 30
 
-        str_w_head, str_h_head = osd.stringsize('Ajg', val.head.font, val.head.size)
+        str_w_label, str_h_label = osd.stringsize('Ajg', val.label.font, val.label.size)
         str_w_selection, str_h_selection = \
                          osd.stringsize('Ajg', val.selection.font, val.selection.size)
         str_w_normal, str_h_normal = osd.stringsize('Ajg', val.font, val.size)
@@ -159,42 +156,33 @@ class Skin_TV:
         right_arrow_size = osd.bitmapsize(val.right_arrow)
 
 
-        y_contents = conf_y + str_h_head + 2 * val.spacing
-        h_contents = conf_h - str_h_head - 2 * val.spacing
-        x_contents = conf_x + val.channel_width
-        w_contents = conf_w - val.channel_width
+        y_contents = conf_y + str_h_label + 2 * val.spacing
+        h_contents = conf_h - str_h_label - 2 * val.spacing
+        x_contents = conf_x + val.label.width
+        w_contents = conf_w - val.label.width
 
         col_size = int( w_contents / n_cols )
-        occupied_height = 0
 
 
         # Display the Time on top
         x = conf_x
         y = conf_y
         # Display the Channel on top
-        osd.drawbox(x, y, x+val.channel_width, y+str_h_head + 2 * val.spacing,
-                    width=-1, color=val.head.bgcolor)
-        osd.drawbox(x, y, x+val.channel_width, y+str_h_head + 2 * val.spacing,
-                    width=1, color=val.border_color)
+        DrawBox(x, y, x+val.label.width, y+str_h_label + 2 * val.spacing,
+                val.label.bgcolor, 1, val.border_color)
         DrawTextFramed(time.strftime("%m/%d",time.localtime(to_listing[0][1])), \
-                       val.head, x + val.spacing, y + val.spacing, \
-                       val.channel_width - 2 * val.spacing, str_h_head)
+                       val.label, x + val.spacing, y + val.spacing, \
+                       val.label.width - 2 * val.spacing, str_h_label)
 
 
-        x = x_contents
-        head2 = copy.copy(val.head)
-        head2.align='left'
         for i in range(n_cols):
-            osd.drawbox(x, y, x+col_size, y+ str_h_head + 2 * val.spacing,
-                        width=-1, color=val.head.bgcolor)   
-            osd.drawbox(x, y, x+col_size, y+ str_h_head + 2 * val.spacing,
-                        width=1, color=val.border_color)   
-            DrawTextFramed(time.strftime("%H:%M",time.localtime(to_listing[0][i+1])),
-                                head2,
-                                x + val.spacing, y + val.spacing,
-                                col_size - 2 * val.spacing, str_h_head)
-
-            x += col_size
+            x0 = int(x_contents + (float(w_contents) / n_cols) * i)
+            x1 = int(x_contents + (float(w_contents) / n_cols) * (i+1))
+            
+            DrawBox(x0, y, x1, y+ str_h_label + 2 * val.spacing,
+                    val.label.bgcolor, 1, val.border_color)   
+            DrawText(time.strftime("%H:%M",time.localtime(to_listing[0][i+1])),
+                     val.label, x0 + val.spacing, y + val.spacing, align='left')
 
         # define start and stop time
         date = time.strftime("%x", time.localtime())
@@ -202,7 +190,7 @@ class Skin_TV:
         stop_time = to_listing[0][len(to_listing[0])-1]
         stop_time += (col_time*60)
 
-        str_h = max( str_h_head, str_h_selection, str_h_normal )
+        str_h = max( str_h_label, str_h_selection, str_h_normal )
 
         # 1 sec = x pixels
         prop_1sec = float(w_contents) / float(n_cols * col_time * 60) 
@@ -210,121 +198,100 @@ class Skin_TV:
         # selected program:
         selected_prog = to_listing[1]
 
-        y = y_contents
+        items_per_page = self.DrawTVGuide_ItemsPerPage(settings)
+
         h = str_h
         for i in range(2,len(to_listing)):
-            if occupied_height <= h_contents:
-                # draw the channel name/logo/id
-                osd.drawbox(conf_x, y, conf_x+val.channel_width, y+str_h + 2 * val.spacing,
-                            width=-1, color=val.head.bgcolor)
-                osd.drawbox(conf_x, y, conf_x+val.channel_width, y+str_h + 2 * val.spacing,
-                            width=1, color=val.border_color)
-                # Logo
-                channel_logo = config.TV_LOGOS + '/' + to_listing[i].id + '.png'
-                if not os.path.isfile(channel_logo):
-                    #if DEBUG: print 'skin: Cannot find logo "%s"' % channel_logo
-                    channel_logo = None
-
-                padding = 0
-                if channel_logo:
-                    channel_logo_size = osd.bitmapsize(channel_logo)
-                    # XXX The logo was stretched vertically with the old code. /Krister
-                    padding = channel_logo_size[0]
-                    osd.drawbitmap(util.resize(channel_logo, padding, padding),
-                                   conf_x + val.spacing, y + val.spacing + \
-                                   (str_h - padding)/2)
-
-                DrawTextFramed(to_listing[i].displayname, val.head,
-                               conf_x + val.spacing + padding, y + val.spacing,
-                               val.channel_width - 2 * val.spacing - padding, str_h)
-
-
-
-
-                if to_listing[i].programs:
-                    for prg in to_listing[i].programs:
-                        val2 = copy.copy(val)
-                        val2.selection = copy.copy(val.selection)
-
-                        str = ''
-                        x = 0
-                        w = 0
-                        flag_left = 0
-                        flag_right = 0
-                        if prg.start < start_time:
-                            flag_left = 1
-                            x = x_contents
-                            t_start = start_time
-                        else:
-                            x = x_contents + int( float(prg.start - start_time) * \
-                                                  prop_1sec )
-                            t_start = prg.start
-
-                        str += prg.title
-
-                        if prg.stop > stop_time:
-                            flag_right = 1
-                            w = w_contents + x_contents - x                        
-                        else:
-                            w = int( (prg.stop - t_start) * prop_1sec )
-
-                        if prg.title == 'This channel has no data loaded':
-                            val2.align='center'
-                            val2.selection.align='center'
-
-
-                        if prg.title == selected_prog.title and \
-                           prg.channel_id == selected_prog.channel_id and \
-                           prg.start == selected_prog.start and \
-                           prg.stop == selected_prog.stop:
-
-                            osd.drawbox(x, y, x+w, y+h + 2 * val2.spacing,
-                                        width=-1, color=val2.selection.bgcolor)
-                            osd.drawbox(x, y, x+w, y+h + 2 * val2.spacing,
-                                        width=1, color=val2.border_color)
-                            DrawTextFramed(str, val2.selection,
-                                           x+val2.spacing + flag_left * \
-                                           left_arrow_size[0],
-                                           y+val2.spacing,
-                                           w - 2 * val2.spacing - flag_left * \
-                                           left_arrow_size[0] - flag_right * \
-                                           right_arrow_size[0], h)
-                        else:
-                            osd.drawbox(x, y, x+w, y+h + 2 * val2.spacing,
-                                        width=-1, color=val2.bgcolor)
-                            osd.drawbox(x, y, x+w, y+h + 2 * val2.spacing,
-                                        width=1, color=val2.border_color)
-                            DrawTextFramed(str, val2,
-                                           x+val2.spacing + flag_left * \
-                                           left_arrow_size[0], y+val2.spacing,
-                                           w - 2 * val2.spacing - flag_left * \
-                                           left_arrow_size[0] - flag_right * \
-                                           right_arrow_size[0], h)
-
-
-                        if flag_left == 1:
-                            osd.drawbitmap(val2.left_arrow, x, \
-                                           y+int((str_h - left_arrow_size[1])/2.0))
-                        if flag_right == 1:
-                            osd.drawbitmap(val2.right_arrow, \
-                                           x+w-right_arrow_size[0], \
-                                           y+int((str_h - right_arrow_size[1])/2.0))
-                else:
-                    osd.drawbox(x_contents, y, x_contents+w_contents, \
-                                y+h + 2 * val2.spacing,
-                                width=-1, color=val2.bgcolor)
-                    osd.drawbox(x_contents, y, x_contents+w_contents, \
-                                y+h + 2 * val2.spacing,
-                                width=1, color=val2.border_color)
-
-                    DrawTextFramed('-[ NO DATA ]-', val, x_contents+val2.spacing,
-                                   y+val2.spacing,
-                                   w_contents - 2 * val2.spacing, h)
-
-                occupied_height += str_h + 2 * val2.spacing
-                y += str_h + 2 * val2.spacing
-            else:
+            if i - 1 > items_per_page:
                 break
+            
+            y0 = int(y_contents + (i-2) * (float(h_contents) / items_per_page))
+            y1 = int(y_contents + (i-1) * (float(h_contents) / items_per_page))
+
+            # draw the channel name/logo/id
+            DrawBox(conf_x, y0, conf_x+val.label.width, y1,
+                    val.label.bgcolor, 1, val.border_color)
+
+            # Logo
+            channel_logo = config.TV_LOGOS + '/' + to_listing[i].id + '.png'
+            if not os.path.isfile(channel_logo):
+                channel_logo = None
+
+            padding = 0
+            if channel_logo:
+                channel_logo_size = osd.bitmapsize(channel_logo)
+                padding = channel_logo_size[0]
+                osd.drawbitmap(util.resize(channel_logo, padding, padding),
+                               conf_x + val.spacing, y0 + val.spacing + \
+                               (str_h - padding)/2)
+
+            DrawTextFramed(to_listing[i].displayname, val.label,
+                           conf_x + val.spacing + padding, y0 + val.spacing,
+                           val.label.width - 2 * val.spacing - padding, str_h)
+
+            if to_listing[i].programs:
+                for prg in to_listing[i].programs:
+                    x0 = 0
+                    x1 = 0
+
+                    w = 0
+                    flag_left = 0
+                    flag_right = 0
+
+                    if prg.start < start_time:
+                        flag_left = 1
+                        x0 = x_contents
+                        t_start = start_time
+                    else:
+                        x0 = x_contents + int(float(prg.start-start_time) * prop_1sec)
+                        t_start = prg.start
+
+                    if prg.stop > stop_time:
+                        flag_right = 1
+                        w = w_contents + x_contents - x0                        
+                        x1 = x_contents + w_contents
+                    else:
+                        w =  int( float(prg.stop - t_start) * prop_1sec )
+                        x1 = x_contents + int(float(prg.stop-start_time) * prop_1sec)
+
+                        
+                    val2 = copy.copy(val)
+                    val2.selection = copy.copy(val.selection)
+
+                    if prg.title == 'This channel has no data loaded':
+                        val2.align='center'
+                        val2.selection.align='center'
+
+                    spacing = val2.spacing + 1
+
+                    cur_val = val2
+
+                    if prg.title == selected_prog.title and \
+                       prg.channel_id == selected_prog.channel_id and \
+                       prg.start == selected_prog.start and \
+                       prg.stop == selected_prog.stop:
+
+                        cur_val = val2.selection
+                        
+                    DrawBox(x0, y0, x1, y1, cur_val.bgcolor, 1, val2.border_color)
+
+                    tx0 = min(x1, x0+(flag_left+1)*spacing+flag_left*left_arrow_size[0])
+                    tx1 = max(x0, x1-(flag_right+1)*spacing-flag_right*right_arrow_size[0])
+
+                    DrawTextFramed(prg.title, cur_val, tx0, y0+spacing, tx1-tx0, h)
+
+                    if flag_left:
+                        osd.drawbitmap(val2.left_arrow, x0 + spacing,
+                                       y0+spacing+int((str_h - left_arrow_size[1])/2))
+                    if flag_right:
+                        osd.drawbitmap(val2.right_arrow, x1-right_arrow_size[0]-spacing, \
+                                       y0+spacing+int((str_h - right_arrow_size[1])/2))
+
+            else:
+                DrawBox(x_contents, y0, x_contents+w_contents, y1,
+                        val2.bgcolor, 1, val2.border_color)
+                DrawTextFramed('-[ NO DATA ]-', val, x_contents+val.spacing,
+                               y0+val.spacing, w_contents - 2 * val2.spacing, h)
 
 
         # draw a border around the contents
