d4eb33d4be072006a98fe2f8914ac49871f860af
Krister Lagerstrom <krister-freevo@kmlager.com>
2002-11-06 03:24:31 +0000

    Added fileheader
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@1290 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- util.py
+++ util.py
@@ -1,9 +1,40 @@
-#
-# util.py
-#
-# This file contains utility functions for Freevo
-#
+#if 0 /*
+# -----------------------------------------------------------------------
+# util.py - This file contains utility functions for Freevo
+# -----------------------------------------------------------------------
 # $Id$
+#
+# Notes:
+# Todo:        
+#
+# -----------------------------------------------------------------------
+# $Log$
+# Revision 1.43  2002/11/06 03:24:31  krister
+# Added fileheader
+#
+#
+#
+# -----------------------------------------------------------------------
+# Freevo - A Home Theater PC framework
+# Copyright (C) 2002 Krister Lagerstrom, et al. 
+# Please see the file freevo/Docs/CREDITS for a complete list of authors.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MER-
+# CHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+#
+# ----------------------------------------------------------------------- */
+#endif
 
 import sys
 import socket, glob
