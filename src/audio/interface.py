06bbebd0f204dbcd590536761e315447476c273e
Dirk Meyer <dischi@freevo.org>
2003-01-12 17:10:37 +0000

    Add cache for the id tags to avoid reloading known mp3/ogg tags. This will speed up displaying a directory.
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@1505 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- src/audio/interface.py
+++ src/audio/interface.py
@@ -9,6 +9,10 @@
 #
 # -----------------------------------------------------------------------
 # $Log$
+# Revision 1.4  2003/01/12 17:10:37  dischi
+# Add cache for the id tags to avoid reloading known mp3/ogg tags. This will
+# speed up displaying a directory.
+#
 # Revision 1.3  2003/01/12 13:51:51  dischi
 # Added the feature to remove items for videos, too. For that the interface
 # was modified (update instead of remove).
@@ -39,16 +43,55 @@
 import config
 import util
 
+import cPickle as pickle
+import md5
+import os
+
+
 from audioitem import AudioItem
+
+AUDIO_CACHE_VERSION = 1
 
 def cwd(parent, files):
     """
     return a list of items based on the files
     """
     items = []
+
+    if parent and parent.type == 'dir':
+        cache_file = '%s/audio-%s-%s' % (config.FREEVO_CACHEDIR,
+                                         util.hexify(md5.new(parent.dir).digest()),
+                                         os.getuid())
+    else:
+        cache_file = None
+        
+    if cache_file and os.path.isfile(cache_file):
+        version, cache = pickle.load(open(cache_file, 'r'))
+        if version != AUDIO_CACHE_VERSION:
+            print 'cache file has a wrong version'
+            cache = {}
+    else:
+        cache = {}
+
+
+    new_cache = {}
+
     for file in util.find_matches(files, config.SUFFIX_AUDIO_FILES):
-        items += [ AudioItem(file, parent) ]
+        try:
+            data = cache[file]
+            items += [ AudioItem(file, parent, data) ]
+            new_cache[file] = data
+        except KeyError:
+            item = AudioItem(file, parent)
+            new_cache[file] = item.dump()
+            items += [ item ]
+            
         files.remove(file)
+
+
+    if cache_file:
+        pickle.dump((AUDIO_CACHE_VERSION, new_cache), open(cache_file, 'w'))
+
     return items
 
 
