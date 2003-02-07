386515d867258b8a6cd59ed76995cdee668006dd
Krister Lagerstrom <krister-freevo@kmlager.com>
2003-02-07 06:16:59 +0000

    Updated to only use the freevo_loader and preloads when necessary. Some prelim Gentoo support, please test.
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@1626 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- runapp.c
+++ runapp.c
@@ -21,12 +21,27 @@
 #include <sys/time.h>
 #include <sys/resource.h>
 #include <sched.h>
+#include <time.h>
+
 
 extern int errno;
 
-static void runapp_setprio (int newprio, FILE *fp);
+#define LOG(str, args...) {                                             \
+  static char tmp1[1000];                                               \
+  static char tmp2[1000];                                               \
+  sprintf (tmp1, str, ## args);                                         \
+  sprintf (tmp2, "runapp: %s\n", tmp1);                                 \
+  log_write (tmp2);                                                     \
+}                                                                       
+
+static FILE *log_fp = (FILE *) NULL;
+
+static void runapp_setprio (int newprio);
 static int check_runtime (void);
-static char * get_preload_str (void);
+static char * get_preload_str (char *pFilename);
+static void log_init (void);
+static void log_close (void);
+static void log_write (char *pBuf);
 
 
 int
@@ -34,33 +49,26 @@ main (int ac, char *av[])
 {
   sigset_t set;
   int i;
-  FILE *fp;
   int ac_idx = 1;               /* Inc. when args are "consumed". Ignore argv[0] */
   char *newav[1000];
   int newac = 0;
-  struct stat statbuf;
-  char logfile[256];
   char cmd_str[1000];
   char currdir[1000];
   int newprio;
   int got_runtime;
-
-
-  /* Does the logdir exist? */
-  if (!stat (RUNAPP_LOGDIR, &statbuf)) {
-    /* XXX Also check that it is writeable for this uid */
-    sprintf (logfile, "%s/internal-runapp-%d.log", RUNAPP_LOGDIR, getuid());
-  } else {
-    /* No, log to the /tmp/freevo dir instead */
-    sprintf (logfile, "/tmp/freevo/internal-runapp-%d.log", getuid());
-    mkdir ("/tmp/freevo", 0777); /* Make sure the dir exists, ignore errors */
-  }
+  int use_preloads = 0;         /* Full runtime preloads */
+  int use_preloads_gentoo = 0;  /* Tiny runtime preloads for Gentoo */
   
-  fp = fopen (logfile, "a");
+
+  /* Set the umask to be ugo+rwx XXX security implications? */
+  umask (0);
+  
+  /* Initialize the logfile stuff. Needed for the LOG() macro */
+  log_init ();
   
   /* Dump some debug info to the logfile */
-  fprintf (fp, "PATH = %s\n", getenv ("PATH"));
-  fprintf (fp, "CWD = %s\n", getcwd (currdir, sizeof (currdir)));
+  LOG ("PATH = %s", getenv ("PATH"));
+  LOG ("CWD = %s", getcwd (currdir, sizeof (currdir)));
   
   cmd_str[0] = 0;
   
@@ -71,16 +79,14 @@ main (int ac, char *av[])
     sprintf (tmp, "%s ", av[i]);
     strcat (cmd_str, tmp);
     
-    fprintf (fp, "runapp: av[%d] = '%s'\n", i, av[i]);
+    LOG ("av[%d] = '%s'", i, av[i]);
   }
 
-  fprintf (fp, "Command: '%s'\n", cmd_str);
-  
-  fflush (fp);
+  LOG ("Command: '%s'", cmd_str);
   
   /* Check for the full runtime. */
   got_runtime = check_runtime ();
-  fprintf (fp, "runapp: runtime %s\n", got_runtime ? "FOUND" : "NOT FOUND");
+  LOG ("runtime %s", got_runtime ? "FOUND" : "NOT FOUND");
   
   /* Add check for Gentoo too, different handling from the runtime */
   /* XXX */
@@ -94,49 +100,94 @@ main (int ac, char *av[])
   if (sscanf (av[ac_idx], "--prio=%d", &newprio) == 1) {
     
     /* Yes, set the process priority */
-    runapp_setprio (newprio, fp);
+    runapp_setprio (newprio);
 
     ac_idx++;                   /* Skip this parameter for the exec() */
+
+    /* Do we have any further args? */
+    if (ac_idx >= ac) {
+      LOG ("Malformed command line, exiting");
+      exit (1);
+    }
+    
   }
 
   /*
    * Build the new command string (newav)
    */
+
+  /* Make sure LD_PRELOAD is not set unless necessary */
+  unsetenv ("LD_PRELOAD");
   
-  /* Do we need to use the freevo_loader (ld.so) from the runtime? */
-  if (got_runtime) {
+  /* Is the application "python"? Substitute for the runtime python,
+   * or from the os.
+   */
+  if (strcmp (av[ac_idx], "python") == 0) {
+
+    if (got_runtime) {
+      /* Must use the runtime freevo_loader+freevo_python+preloads */
+      
+      newav[newac++] = "./runtime/dll/freevo_loader";
+      newav[newac++] = "./runtime/apps/freevo_python";
+      use_preloads = 1;
+      ac_idx++;                 /* Consume av[] == python */
+
+      LOG ("Using the runtime Python+loader");
+      
+    } else {
+      /* XXX Should have special checks for Red Hat which uses python2 etc */
+      newav[newac++] = "python";
+      ac_idx++;   /* Consume av[] == python */
+
+      /* Check if the python target is src/main.py, in which case
+       * Gentoo preloads are used if present */
+      if ((ac_idx < ac) && (strstr (av[ac_idx], "src/main.py") != (char *) NULL)) {
+        use_preloads_gentoo = 1;
+      }
+    }
+
+  } else {
+
+    /* No, it is a regular app. Check if it is in the runtime */
+    if (strstr (av[ac_idx], "runtime/apps/") != (char *) NULL) {
+
+      /* Yes, so the executable to start is the freevo_loader. */
+      newav[newac++] = "./runtime/dll/freevo_loader";
+      use_preloads = 1;
+
+      LOG ("Runtime app, must use preloads"); 
+    } else {
+      /* Just a plain old app. It will be copied to newav[] below */
+      LOG ("Regular app, no preloads"); 
+    }
+
+  }
+  
+  /* Check if LD_PRELOAD needs to be set to the full runtime preloads */
+  if (use_preloads) {
     char *pPreloads;
 
     
-    /* Yes, so the executable to start is the freevo_loader.
-     * This must be newav[0] */
-    newav[newac++] = "./runtime/dll/freevo_loader";
-    //newav[newac++] = "--library-path";
-    //newav[newac++] = "./runtime/dll";
-
-    pPreloads = get_preload_str();
-    fprintf (fp, "runapp: LD_PRELOAD = '%s'\n", pPreloads);
+    pPreloads = get_preload_str ("./runtime/preloads");
+    LOG ("Setting LD_PRELOAD = '%s'", pPreloads);
 
     setenv ("LD_PRELOAD", pPreloads, 1);
     
   }
   
-  /* Is the application "python"? Substitute for the runtime python,
-   * or from the os.
-   * The freevo_loader will be in newav[0] if present
-   */
-  if (strcmp (av[ac_idx], "python") == 0) {
+  /* Check if LD_PRELOAD needs to be set to the tiny runtime preloads (Gentoo).
+   * This will do nothing if there is no runtime stuff whatsoever. */
+  if (use_preloads_gentoo) {
+    char *pPreloads;
 
-    if (got_runtime) {
-      newav[newac++] = "./runtime/apps/freevo_python";
-      ac_idx++;
-    } else {
-      /* XXX Should have special checks for Red Hat which uses python2 etc */
-      newav[newac++] = "python";
-      ac_idx++;
-    }
+    
+    pPreloads = get_preload_str ("./runtime/preloads_gentoo");
+    LOG ("Setting Gentoo LD_PRELOAD = '%s'", pPreloads);
+
+    setenv ("LD_PRELOAD", pPreloads, 1);
+    
   }
-
+  
   /* Copy the counted argv array to the NULL-terminated newav */
   for (i = ac_idx; i < ac; i++) {
     newav[newac++] = av[ac_idx++];
@@ -146,29 +197,29 @@ main (int ac, char *av[])
 
   /* Debug */
   for (i = 0; newav[i] != NULL; i++) {
-    fprintf (fp, "runapp: newav[%d] = '%s'\n", i, newav[i]);
+    LOG ("newav[%d] = '%s'", i, newav[i]);
   }
   
   /* Set up signals (turn off blocking!) */
   sigemptyset (&set);
   sigprocmask (SIG_SETMASK, &set, (sigset_t *) NULL);
 
-  fflush (fp);
+  /* Must close all open file descriptors except std*, otherwise the exec-ed
+   * app will get them! */
+  log_close ();
   
   /* Overlay the child application */
   execvp (newav[0], newav);
   
-  fprintf (fp, "runapp: failed! errno = %d\n", errno);
+  LOG ("failed! errno = %d", errno);
   
-  fclose (fp);
-
   exit (0);
   
 }
 
 
 static void
-runapp_setprio (int newprio, FILE *fp)
+runapp_setprio (int newprio)
 {
   int res;
   int do_setprio = 0;
@@ -179,7 +230,7 @@ runapp_setprio (int newprio, FILE *fp)
     struct sched_param sp;
 
     
-    fprintf (fp, "runapp: Trying to set realtime priority\n");
+    LOG ("Trying to set realtime priority\n");
     
     /* Try to max out the priority */
     sp.sched_priority = sched_get_priority_max(SCHED_FIFO);
@@ -189,9 +240,9 @@ runapp_setprio (int newprio, FILE *fp)
       if (sched_setscheduler (0, SCHED_RR, &sp) != 0) {
         sp.sched_priority = sched_get_priority_max (SCHED_OTHER);
         if (sched_setscheduler (0, SCHED_OTHER, &sp) != 0) {
-          fprintf (fp, "runapp: Could not get any realtime priority\n");
-          fprintf (fp, "runapp: This is not a serious problem, but capture quality under high\n");
-          fprintf (fp, "runapp: load could be improved by running as root.\n");
+          LOG ("Could not get any realtime priority\n");
+          LOG ("This is not a serious problem, but capture quality under high\n");
+          LOG ("load could be improved by running as root.\n");
           do_setprio = 1;
         } else {
           fprintf (fp, "runapp: Got other realtime scheduler (%d)\n", sp.sched_priority);
@@ -215,9 +266,9 @@ runapp_setprio (int newprio, FILE *fp)
     res = setpriority (PRIO_PROCESS, 0, newprio);
     
     if (res) {
-      fprintf (fp, "runapp: setprio() failed, errno=%d\n", errno);
+      LOG ("setprio() failed, errno=%d\n", errno);
     } else {
-      fprintf (fp, "runapp: set new prio to %d\n", newprio);
+      LOG ("set new prio to %d\n", newprio);
     }
 
   }
@@ -247,14 +298,16 @@ check_runtime (void)
 
   
 static char *
-get_preload_str (void)
+get_preload_str (char *pFilename)
 {
   static char preload_str[10000];
   int fd;
   int len;
   
 
-  fd = open ("./runtime/preloads", O_RDONLY);
+  LOG ("Reading LD_PRELOAD from file '%s'", pFilename);
+  
+  fd = open (pFilename, O_RDONLY);
 
   if (fd < 0) {
     close (fd);
@@ -273,3 +326,57 @@ get_preload_str (void)
   return preload_str;
 
 }
+
+
+static void
+log_init (void)
+{
+  char logfile[256];
+  time_t t;
+
+  
+  /* Can we log to the logdir (e.g. /var/log/freevo)? */
+  sprintf (logfile, "%s/internal-runapp-%d.log", RUNAPP_LOGDIR, getuid());
+  log_fp = fopen (logfile, "a");
+
+  if (log_fp == (FILE *) NULL) {
+    /* Nope, try /tmp/freevo instead */
+    mkdir ("/tmp/freevo", 0777); /* Make sure the dir exists, ignore errors */
+    sprintf (logfile, "/tmp/freevo/internal-runapp-%d.log", getuid());
+
+    /* If this returns NULL, there is no other option. Just discard the logs... */
+    log_fp = fopen (logfile, "a"); 
+
+  }
+
+  LOG ("\n\n***********************************************************\n");
+  t = time ((time_t *) NULL);
+  LOG ("Started on %s", ctime (&t));
+}
+
+
+static void
+log_write (char *pBuf)
+{
+  if (log_fp == (FILE *) NULL) {
+    return;
+  }
+
+  fprintf (log_fp, pBuf);
+  fflush (log_fp);
+
+  /* Done */
+  return;
+    
+}
+
+
+static void
+log_close (void)
+{
+  if (log_fp != (FILE *) NULL) {
+    fclose (log_fp);
+  }
+
+}
+
