b00e57b034920d753edab680cb9acd26d64df0a6
Dirk Meyer <dischi@freevo.org>
2003-02-20 18:54:45 +0000

    some enhancements. Real /etc/lircd.conf to get the defaults
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@1834 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- helpers/freevo2lirc.pl
+++ helpers/freevo2lirc.pl
@@ -12,26 +12,45 @@
 #
 
 
+open (FILE, "/etc/lircd.conf") or die "Can't open file: /etc/lircd.conf";
+$in = 0;
+while (<FILE>) {
+  if ($in && (/^[ \t]*([^ \t]*)[ \t]*0x[0-9a-f]/)) {
+    $keys{$1} = uc($1)
+  }
+  $in = 1 if (/begin codes/i);
+}
+close FILE;
+
 $filename = $ARGV[0];
 
 open (FILE, $filename) or die "Can't open file: $filename";
 
-while (<FILE>)
-	{
-	($lircd_code,$freevo_code) = split(/:/,$_);
-	$lircd_code =~ s/\'//g;
-	$freevo_code =~ s/\'//g;
-	$freevo_code =~ s/,//;
-	$lircd_code =~ s/^\s*//;
-	$freevo_code =~ s/^\s*//;
-	
-	chomp($lircd_code);
-	chomp($freevo_code);
-	print "begin\n";
-	print "    prog = freevo\n";
-	print "    button = $lircd_code\n";
-	print "    config =  $freevo_code\n";
-	print "end\n";
-	print "\n";
-	}
+
+$in = 0;
+while (<FILE>) {
+  $in = 0 if (/}/i);
+  $in = 1 if (/RC_CMDS *=/i);
+  if ($in && (/:/)) {
+    ($lircd_code,$freevo_code) = split(/:/,$_);
+    $lircd_code =~ s/\'//g;
+    $freevo_code =~ s/\'//g;
+    $freevo_code =~ s/,//;
+    $lircd_code =~ s/^\s*//;
+    $lircd_code =~ s/ *$//;
+    $freevo_code =~ s/^\s*//;
+    
+    chomp($lircd_code);
+    chomp($freevo_code);
+    $keys{$lircd_code} = $freevo_code;
+  }
+}
+
+foreach $key (sort(keys(%keys))) {
+  print "begin\n";
+  print "    prog = freevo\n";
+  print "    button = $key\n";
+  print "    config = $keys{$key}\n";
+  print "end\n";
+}
 
