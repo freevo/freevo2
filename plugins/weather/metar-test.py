6a7c3d6c1dd1b466662ebd862f195185511ff9e4
Aubin Paul <aubin@outlyer.org>
2003-02-24 06:02:13 +0000

    Changed to use pymetar features we use elsewhere.
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@1894 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- plugins/weather/metar-test.py
+++ plugins/weather/metar-test.py
@@ -4,4 +4,5 @@ weather = pymetar.MetarReport()
 weather.fetchMetarReport('CYYZ')
 print weather.getTime()
 print weather.getWeather()
-
+print weather.getTemperatureCelsius()
+print weather.getPixmap() + '.png'
