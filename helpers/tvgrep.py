26835a6f82238ebb9b3d7e2ecf68cbedb87918a5
Aubin Paul <aubin@outlyer.org>
2003-03-10 19:32:50 +0000

    A dumb little command-line program I use to schedule stuff sometimes. It works like this:
    ./helpers/tvgrep.py -listing <show name>
    
    will print a human readable list of shows matching the pattern.
    
    ./helpers/tvgrep.py -schedule <show name>
    
    will print a bunch of lines in the freevo_record.lst format, so you could
    append them to the end of the schedule file. I don't do it automatically,
    because there is no filtering.
    
    This is a pretty messy program, but I thought someone might find it useful.
    
    I do desperately need a way to redirect the output of 'import config' somewhere
    because it prints 10 lines of debug. Disabling DEBUG_STDOUT doesn't help because
    then you can't see the listings either.
    
    I tried redirecting sys.stdout, but since config does that itself, that won't
    work either.
    
    
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@2038 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

---
+++ helpers/tvgrep.py
@@ -0,0 +1,104 @@
+import sys,os,time
+import string,re
+import cPickle as pickle
+
+# Horrible, horrible hack to work on command-line
+# Need to figure out how to make config be quiet.
+STARTDIR='/usr/local/freevo'
+os.environ['FREEVO_STARTDIR'] = STARTDIR
+sys.path.append(STARTDIR)
+sys.path.append(STARTDIR + '/src')
+sys.path.append(STARTDIR + '/src/tv')
+
+import config
+
+# Had to copy over get_tunerid, progname2filename since including the 
+# record_video code resulted in an attempt to open the graphic device. 
+
+def get_tunerid(channel_id):
+    tuner_id = None
+    for vals in config.TV_CHANNELS:
+        tv_channel_id, tv_display_name, tv_tuner_id = vals[:3]
+        if tv_channel_id == channel_id:
+            return tv_tuner_id
+    return None
+
+def progname2filename(progname):
+    '''Translate a program name to something that can be used as a filename.'''
+
+    # Letters that can be used in the filename
+    ok = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_'
+
+    s = ''
+    for letter in progname:
+        if letter in ok:
+            s += letter
+        else:
+            if s and s[-1] != '_':
+                s += '_'
+    return s
+
+
+# A simple version of the code in record_daemon/record_video for building
+# a schedule.
+
+def make_schedule (b=None):
+    if not b:
+        return
+
+    # Time stuff
+    len_secs = b.stop-b.start
+    temp = len_secs - 1
+    hour = int(temp/3600)
+    minu = int(temp/60)
+    seco = int(temp%60)
+    timecode_format = '%0.2i:%0.2i:%0.2i' % (hour,minu,seco)
+
+    # Channel
+    tunerid = get_tunerid(b.channel_id)
+
+
+    rec_name = b.title
+    if b.sub_title:
+        rec_name += '_-_' + b.sub_title
+    ts_ch = time.strftime('%m-%d_%I:%M_-', time.localtime(b.start))
+    rec_name = ts_ch + '_' + progname2filename(rec_name)
+    rec_name = os.path.join(config.DIR_RECORD, rec_name)
+
+
+
+    cl_options = { 'channel'  : tunerid,
+                   'filename' : rec_name,
+                   'seconds'  : len_secs,
+                   'timecode' : timecode_format }
+    
+    return config.VCR_CMD % cl_options
+
+# Using this is about a million times faster than using XMLTV natively.
+#
+# hardcoded to -0, some people might not process the guide as root.
+#
+picklefile = config.XMLTV_FILE + '-0.pickled'
+
+# Ugly.
+
+if len(sys.argv) > 2:
+    ARG = '.*' + sys.argv[2] + '*.'
+    REGEXP = re.compile(ARG,re.IGNORECASE)
+else:
+    print "Usage: " + str(sys.argv[0]) + " [-listing | -schedule] [TV PROGRAM]"
+    sys.exit(1)
+
+# Ugly too, but this works for now. 
+
+m = pickle.load(open(picklefile,'r'))
+for a in m.GetPrograms():
+    for b in a.programs:
+        if REGEXP.match(b.title):
+            if sys.argv[1] == '-schedule':
+                print make_schedule(b)
+            elif sys.argv[1] == '-listing':
+                if b.sub_title:
+                    print str(b) + ' - ' + b.sub_title
+                else:
+                    print b
