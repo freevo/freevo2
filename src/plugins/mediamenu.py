0b70bce57e2a5129a6a4e4ac88426ca486a2b2cb
Dirk Meyer <dischi@freevo.org>
2003-04-21 13:02:45 +0000

    Reload the mediamenu everytime we display it, some plugins may have changed
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@2317 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- src/plugins/mediamenu.py
+++ src/plugins/mediamenu.py
@@ -9,6 +9,10 @@
 #
 # -----------------------------------------------------------------------
 # $Log$
+# Revision 1.3  2003/04/21 13:02:45  dischi
+# Reload the mediamenu everytime we display it, some plugins may have
+# changed
+#
 # Revision 1.2  2003/04/20 12:43:33  dischi
 # make the rc events global in rc.py to avoid get_singleton. There is now
 # a function app() to get/set the app. Also the events should be passed to
@@ -44,6 +48,7 @@
 
 import os
 import traceback
+import copy
 
 import config
 import menu as menu_module
@@ -91,37 +96,18 @@ class MediaMenu(Item):
         Item.__init__(self)
         self.type = 'mediamenu'
 
+
     def main_menu_generate(self):
         """
         generate the items for the main menu. This is needed when first generating
         the menu and if something changes by pressing the EJECT button
         """
-        items = []
-        dirs  = []
-        
-        if self.display_type == 'video':
-            dirs += config.DIR_MOVIES
-        if self.display_type == 'audio':
-            dirs += config.DIR_AUDIO
-        if self.display_type == 'image':
-            dirs += config.DIR_IMAGES
-        if self.display_type == 'games':
-            dirs += config.DIR_GAMES
+        items = copy.copy(self.normal_items)
 
         if self.display_type:
             plugins = plugin.get('mainmenu_%s' % self.display_type)
         else:
             plugins = []
-            
-        # add default items
-        for d in dirs:
-            try:
-                (title, dir) = d
-                d = DirItem(dir, self, name = title,
-                            display_type = self.display_type)
-                items += [ d ]
-            except:
-                traceback.print_exc()
 
         for p in plugins:
             items += p.items(self)
@@ -129,27 +115,62 @@ class MediaMenu(Item):
         return items
 
 
-
     def main_menu(self, arg=None, menuw=None):
         """
         display the (IMAGE|VIDEO|AUDIO|GAMES) main menu
         """
         self.display_type = arg
+        title = 'MEDIA'
+        dirs  = []
+
+        self.menuw = menuw
+        
         if self.display_type == 'video':
             title = 'MOVIE'
-        elif self.display_type == 'audio':
+            dirs += config.DIR_MOVIES
+        if self.display_type == 'audio':
             title = 'AUDIO'
-        elif self.display_type == 'image':
+            dirs += config.DIR_AUDIO
+        if self.display_type == 'image':
             title = 'IMAGE'
-        elif self.display_type == 'games':
+            dirs += config.DIR_IMAGES
+        if self.display_type == 'games':
             title = 'GAMES'
-        else:
-            title = 'MEDIA'
+            dirs += config.DIR_GAMES
+
+        self.normal_items = []
+        # add default items
+        for d in dirs:
+            try:
+                (title, dir) = d
+                d = DirItem(dir, self, name = title,
+                            display_type = self.display_type)
+                self.normal_items += [ d ]
+            except:
+                traceback.print_exc()
+
+
         item_menu = menu_module.Menu('%s MAIN MENU' % title, self.main_menu_generate(),
-                                     item_types = self.display_type, umount_all=1)
+                                     item_types = self.display_type, umount_all=1,
+                                     reload_func = self.reload)
         self.menuw = menuw
         menuw.pushmenu(item_menu)
 
+
+    def reload(self):
+        menuw = self.menuw
+
+        menu = menuw.menustack[1]
+
+        sel = menu.choices.index(menu.selected)
+        new_choices = self.main_menu_generate()
+        if not menu.selected in new_choices:
+            if len(new_choices) <= sel:
+                menu.selected = new_choices[-1]
+            else:
+                menu.selected = new_choices[sel]
+        menu.choices = new_choices
+        return menu
 
 
     def eventhandler(self, event = None, menuw=None):
@@ -158,8 +179,8 @@ class MediaMenu(Item):
         when a disc in a rom drive changes
         """
         if plugin.isevent(event):
-            if not menuw:               # this shouldn't happen
-                menuw = menu_module.get_singleton() 
+            if not menuw:
+                menuw = self.menuw
 
             menu = menuw.menustack[1]
 
