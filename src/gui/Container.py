a46bd197bfd84ca9e4d725163d97cf355ffafa58
Rob Shortt <rob@tvcentric.com>
2003-04-22 23:54:08 +0000

    Merge parts of popupbox and button.
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@2349 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- src/gui/Container.py
+++ src/gui/Container.py
@@ -6,6 +6,9 @@
 #
 #-----------------------------------------------------------------------
 # $Log$
+# Revision 1.2  2003/04/22 23:54:08  rshortt
+# Merge parts of popupbox and button.
+#
 # Revision 1.1  2003/04/09 01:38:10  rshortt
 # Initial commit of some in-progress classes.
 #
@@ -32,11 +35,15 @@
 #
 # ----------------------------------------------------------------------
 
+import pygame
 import config
 from GUIObject      import GUIObject
+from GUIObject      import Align
 from LayoutManagers import FlowLayout
 from LayoutManagers import GridLayout
 from LayoutManagers import BorderLayout
+from Color import Color
+from Border import Border
 
 DEBUG = config.DEBUG
 
@@ -45,19 +52,20 @@ class Container(GUIObject):
     """
     """
 
-    def __init__(self, left=0, top=0, width=0, height=0, bg_color=None, 
-                 fg_color=None, border=None, bd_color=None, bd_width=None):
+    def __init__(self, type='frame', left=0, top=0, width=0, height=0, 
+                 bg_color=None, fg_color=None, selected_bg_color=None, 
+                 selected_fg_color=None, border=None, bd_color=None, 
+                 bd_width=None):
 
         GUIObject.__init__(self, left, top, width, height, bg_color, fg_color)
 
-        self.label          = None
-        self.selected_label = None
-        self.icon           = None
         self.layout         = None
-
         self.border         = border
         self.bd_color       = bd_color
         self.bd_width       = bd_width
+
+        self.internal_h_align = Align.LEFT
+        self.internal_v_align = Align.TOP
 
         if self.skin_info_spacing:
             self.h_margin = self.skin_info_spacing
@@ -66,18 +74,114 @@ class Container(GUIObject):
             self.h_margin = 10
             self.v_margin = 10
 
+        self.h_spacing = self.h_margin
+        self.v_spacing = self.v_margin
 
-    def set_layout(self, layout=FlowLayout()):
+        if type == 'frame':
+            if not self.bd_color:
+                # XXX TODO: background type 'image' is not supported here yet
+                if self.skin_info_background[0] == 'rectangle':
+                    self.bd_color = Color(self.skin_info_background[1].color)
+                else:
+                    self.bd_color = Color(self.osd.default_fg_color)
+    
+            if not self.bd_width:
+                if self.skin_info_background[0] == 'rectangle' \
+                    and self.skin_info_background[1].size:
+                    self.bd_width = self.skin_info_background[1].size
+                else:
+                    self.bd_width = 2
+
+            if not self.border:
+                self.border = Border(self, Border.BORDER_FLAT,
+                                     self.bd_color, self.bd_width)
+
+        elif type == 'widget':
+            self.selected_bg_color = selected_bg_color
+            self.selected_fg_color = selected_fg_color
+
+            if not bg_color:
+                if self.skin_info_widget.rectangle.bgcolor:
+                    self.bg_color = Color(self.skin_info_widget.rectangle.bgcolor)
+                else:
+                    self.bg_color = Color(self.osd.default_bg_color)
+    
+            if not fg_color:
+                if self.skin_info_widget.font.color:
+                    self.fg_color = Color(self.skin_info_widget.font.color)
+                else:
+                    self.fg_color = Color(self.osd.default_fg_color)
+    
+            if not self.selected_bg_color:
+                if self.skin_info_widget_selected.rectangle.bgcolor:
+                    self.selected_bg_color = Color(self.skin_info_widget_selected.rectangle.bgcolor)
+                else:
+                    self.selected_bg_color = Color((0,255,0,128))
+    
+            if not self.selected_fg_color:
+                if self.skin_info_widget_selected.font.color:
+                    self.selected_fg_color = Color(self.skin_info_widget_selected.font.color)
+                else:
+                    self.selected_fg_color = Color(self.osd.default_fg_color)
+    
+            if not self.bd_color:
+                if self.skin_info_widget.rectangle.color:
+                    self.bd_color = Color(self.skin_info_widget.rectangle.color)
+                else:
+                    self.bd_color = Color(self.osd.default_fg_color)
+    
+            if not self.bd_width:
+                if self.skin_info_widget.rectangle.size:
+                    self.bd_width = self.skin_info_widget.rectangle.size
+                else:
+                    self.bd_width = 2
+    
+            if not self.border:
+                self.border = Border(self, Border.BORDER_FLAT,
+                                     self.bd_color, self.bd_width)
+
+
+    def set_layout(self, layout=None):
+        if not layout: layout = FlowLayout(self)
         self.layout = layout
 
 
-    def _render(self, surface=None):
-        pass
+    def get_layout(self):
+        return self.layout 
 
 
-    def render(self, surface=None):
+    def _draw(self):
+        print 'Container::draw %s' % self
+
         if not self.layout:
-            self.layout = FlowLayout()
+            self.layout = FlowLayout(self)
 
-        self._render(surface)
+        self.layout.layout()
+
+        for child in self.children:
+            child.draw()
+
+        if self.icon:
+            # note that icon handling will be changed totally, ignore this
+            ix,iy = self.get_position()
+            self.osd.screen.blit(self.icon, (ix+self.h_margin,iy+self.v_margin))
+
+
+    def set_border(self, bs):
+        """
+        bs  Border style to create.
+
+        Set which style to draw border around object in. If bs is 'None'
+        no border is drawn.
+
+        Default for PopubBox is to have no border.
+        """
+        if isinstance(self.border, Border):
+            self.border.set_style(bs)
+        elif not bs:
+            self.border = None
+        else:
+            self.border = Border(self, bs)
+
+
 
