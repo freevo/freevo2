6b07ce6f1f73040b595b55732c7f7e5ee9dd59e0
Dirk Meyer <dischi@freevo.org>
2003-04-26 15:23:04 +0000

    this file now covers camera support _without_ gphoto
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@2389 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- src/image/plugins/camera.py
+++ src/image/plugins/camera.py
@@ -1,23 +1,16 @@
 #if 0 /*
 # -----------------------------------------------------------------------
-# camera.py - Special handling for digi cams through gphoto
+# camera.py - Special handling for digi cams who are storage devices
 # -----------------------------------------------------------------------
 # $Id$
 #
-# Notes: you need gphoto and the python bindings to get this working
-#        add plugin.activate('image.camera') to your local_conf.py
-#
+# Notes: 
 # Todo:        
 #
 # -----------------------------------------------------------------------
 # $Log$
-# Revision 1.1  2003/04/18 15:00:39  dischi
-# camera (gphoto support) ist now a plugin
-#
-# Revision 1.1  2003/01/14 18:54:30  dischi
-# Added gphoto support from Thomas Schüppel. You need gphoto and the
-# Python bindings to get this working. I added try-except to integrate
-# this without breaking anything.
+# Revision 1.2  2003/04/26 15:23:04  dischi
+# this file now covers camera support _without_ gphoto
 #
 #
 # -----------------------------------------------------------------------
@@ -42,144 +35,32 @@
 # ----------------------------------------------------------------------- */
 #endif
 
-
-import menu
-import util
-import config
-from item import Item
-import pygame
-import pygphoto
-import cStringIO
-import image.viewer
-
 import plugin
+import util
+
+from directory import DirItem
 
 class PluginInterface(plugin.MainMenuPlugin):
-
-    def detectCameras(self):
-        gplist = pygphoto.gp_detectcameras( ) 
-        count = pygphoto.gp_list_count( gplist )
-        list = []
-        while count > 0:
-            count = count - 1
-            list.append( [pygphoto.gp_name(gplist, count),pygphoto.gp_value(gplist,count)] )
-        return list
-
-
-    def cameraFactory( self, parent, name, port ):
-        gCamera = pygphoto.gp_camera( name, port )
-        folder = CameraFolder( parent, gCamera, "/", name )
-        return folder
-
+    """
+    Plugin for digi cams who are usb storage devices.
+    Parameter: name, usb id and mountpoint. You can get the usb id with lsusb.
+    You should also activate the usb plugin so that the menu will change
+    when you plugin in or remove the camera.
+    
+    Example for a Fujifilm Finepix:
+    plugin.activate('usb')
+    plugin.activate('image.camera', args=('Finepix', '04cb:0100', '/mnt/camera'))
+    """
+    def __init__(self, name, usb_id, mountpoint):
+        plugin.MainMenuPlugin.__init__(self)
+        self.name       = name
+        self.usb_id     = usb_id
+        self.mountpoint = mountpoint
 
     def items(self, parent):
-        items = []
-        cams = self.detectCameras( )
-        for c in cams:
-            m = self.cameraFactory( parent, c[0], c[1] )
-            m.type = 'camera'
-            m.name = c[0]
-            items.append(m)
-        return items
-    
-
-
-class CameraFile( Item ):
-    def __init__(self, parent, gcamera, path, name, duration=0):
-        Item.__init__(self, parent)
-        self.type = 'image'
-        self.url = 'gphoto://%s/%s' % (path,name)
-        self.gCamera = gcamera
-        self.path = path
-        self.name = name
-        self.filename = None
-        self.image_viewer = image.viewer.get_singleton()
-        self.duration = duration
-
-    def loadimage(self):
-        cfile = pygphoto.gp_getfile( self.gCamera, self.path, self.name )
-        string = pygphoto.gp_readfile( cfile )
-        file = cStringIO.StringIO( string )
-        tmp = pygame.image.load(file)  # XXX Cannot load everything
-        return tmp.convert_alpha()  # XXX Cannot load everything
-
-    def copy(self, obj):
-        """
-        Special copy value DirItem
-        """
-        Item.copy(self,obj)
-        self.gCamera = obj.gCamera
-        self.path = obj.path
-        self.name = obj.name
-        self.image_viewer = obj.image_viewer
-
-    def actions(self):
-        """
-        Retrieve and Show the Image
-        """
-        items = [ ( self.view, 'View Image' ) ]
-	return items
-
-    def cache(self):
-        """
-        caches (loads) the next image
-        """
-        self.image_viewer.cache(self)
-
-    def view(self, arg=None, menuw=None):
-        """
-        view the image
-        """
-        self.parent.current_item = self
-        self.image_viewer.view(self)
-
-
-
-class CameraFolder( Item ):
-    def __init__(self, parent, gcamera, path, name ):
-        Item.__init__(self, parent)
-        self.gCamera = gcamera
-        self.path = path
-        self.name = name
-	self.type = 'folder'
-
-    def copy(self, obj):
-        """
-        Special copy value DirItem
-        """
-        Item.copy(self,obj)
-        self.gCamera = obj.gCamera
-        self.path = obj.path
-        self.name = obj.name
-
-    def actions(self):
-        items = [ ( self.cwd, 'Browse directory' ) ]
-        return items
-
-    def cwd(self, arg=None, menuw=None):
-        """
-        make a menu item for each file in the directory
-        """
-        items = []
-        parentPath = self.path
-        if len(parentPath) == 1:
-            parentPath = ""
-        print "cwd for" + parentPath
-        # Append Folders
-        folders = pygphoto.gp_getsubfolders( self.gCamera, self.path )
-        number = pygphoto.gp_list_count( folders )
-        while number > 0:
-            number = number - 1
-            name = pygphoto.gp_name( folders, number )
-            subFolder = CameraFolder( self, self.gCamera, parentPath + "/" + name, name )
-            items.append( subFolder )
-        files = pygphoto.gp_getfiles( self.gCamera, self.path )
-        number = pygphoto.gp_list_count( files )
-        while number > 0:
-            number = number - 1
-            name = pygphoto.gp_name( files, number )
-            subFile = CameraFile( self, self.gCamera, parentPath, name )
-            items.append( subFile )
-        item_menu = menu.Menu(self.name, items)
-        menuw.pushmenu(item_menu)        
-        return items
+        if self.usb_id in util.list_usb_devices():
+            d = DirItem(self.mountpoint, parent, self.name, display_type='image')
+            d.mountpoint = self.mountpoint
+            return [ d ]
+        return []
+        
