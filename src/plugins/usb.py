c883511b62eb4d258a28cb1662020f6ed4a8ef61
Dirk Meyer <dischi@freevo.org>
2003-04-26 15:12:26 +0000

    usb daemon plugin to watch for bus changes
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@2387 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

---
+++ src/plugins/usb.py
@@ -0,0 +1,65 @@
+#if 0 /*
+# -----------------------------------------------------------------------
+# usb.py - the Freevo usb plugin
+# -----------------------------------------------------------------------
+# $Id$
+#
+# Notes: This plugin sends an event if an usb device is added
+#        or removed
+# Todo:        
+#
+# -----------------------------------------------------------------------
+# $Log$
+# Revision 1.1  2003/04/26 15:12:26  dischi
+# usb daemon plugin to watch for bus changes
+#
+#
+# ----------------------------------------------------------------------- */
+#endif
+
+import plugin
+import util
+import rc
+
+TRUE  = 1
+FALSE = 0
+
+class PluginInterface(plugin.DaemonPlugin):
+    """
+    This Plugin to scan for usb devices. You should activate this
+    plugin if you use mainmenu plugins for special usb devices
+    like camera.py.
+    """
+    def __init__(self):
+        plugin.DaemonPlugin.__init__(self)
+        self.devices = util.list_usb_devices()
+        self.interval  = 10
+        self.idlecount = 0
+
+    def poll(self, menuw=None, arg=None):
+        """
+        poll to check for devices
+        """
+        self.idlecount += 1
+        if self.idlecount != self.interval:
+            return
+        
+        self.idlecount = 0
+        changes = FALSE
+
+        current_devices = util.list_usb_devices()
+        for d in current_devices:
+            try:
+                self.devices.remove(d)
+            except ValueError:
+                changes = TRUE
+                print 'usb.py: new device %s' %d
+
+        for d in self.devices:
+            changes = TRUE
+            print 'usb.py: removed device %s' % d
+
+        if changes:
+            rc.post_event(plugin.event('USB'))
+            
+        self.devices = current_devices
