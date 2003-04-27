aadc345cd2acba1354ca0bd7d64ffc6982e3af8d
Rob Shortt <rob@tvcentric.com>
2003-04-27 17:28:59 +0000

    Added the proper fileheader and some notes.
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@2402 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- src/plugins/joy.py
+++ src/plugins/joy.py
@@ -1,10 +1,43 @@
+#if 0 /*
+# -----------------------------------------------------------------------
+# joy.py - A joystick plugin for Freevo.
+# -----------------------------------------------------------------------
+# $Id$
 #
-# joy.py - Freevo joystick control handling.
+# Notes:
+# This plugin sends UDP commands to the freevo main app based on 
+# joypad/joystick input.  Currently it uses raw access to the Linux kernel 
+# joystick device (API v2.0).  To use this you must have set
+# ENABLE_NETWORK_REMOTE = 1 and configured your joystick in  local_conf.py.
 #
-# This is a standalone application that sends UDP commands to the freevo
-# main app based on joypad/joystick input. Currently uses raw access to
-# the Linux kernel joystick device (API v2.0).
+# -----------------------------------------------------------------------
+# $Log$
+# Revision 1.2  2003/04/27 17:28:59  rshortt
+# Added the proper fileheader and some notes.
 #
+#
+#
+# -----------------------------------------------------------------------
+# Freevo - A Home Theater PC framework
+# Copyright (C) 2003 Krister Lagerstrom, et al. 
+# Please see the file freevo/Docs/CREDITS for a complete list of authors.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MER-
+# CHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+#
+# ----------------------------------------------------------------------- */
+#endif
 
 import sys
 import os
