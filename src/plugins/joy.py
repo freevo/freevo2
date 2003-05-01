b6e75f319aa1032d4812f61f5098dc7b211a370a
Rob Shortt <rob@tvcentric.com>
2003-05-01 22:50:43 +0000

    This is now a real plugin that no longer needs ENABLE_NETWORK_REMOTE to work.
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@2421 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- src/plugins/joy.py
+++ src/plugins/joy.py
@@ -1,17 +1,19 @@
 #if 0 /*
 # -----------------------------------------------------------------------
-# joy.py - A joystick plugin for Freevo.
+# joy.py - A joystick control plugin for Freevo.
 # -----------------------------------------------------------------------
 # $Id$
 #
 # Notes:
-# This plugin sends UDP commands to the freevo main app based on 
-# joypad/joystick input.  Currently it uses raw access to the Linux kernel 
-# joystick device (API v2.0).  To use this you must have set
-# ENABLE_NETWORK_REMOTE = 1 and configured your joystick in  local_conf.py.
+# To use this plugin make sure that your joystick is already working 
+# properly and then configure JOY_DEV and JOY_CMDS in your local_conf.py.
+# You will also need to have plugin.activate('joy') in your config as well.
 #
 # -----------------------------------------------------------------------
 # $Log$
+# Revision 1.3  2003/05/01 22:50:43  rshortt
+# This is now a real plugin that no longer needs ENABLE_NETWORK_REMOTE to work.
+#
 # Revision 1.2  2003/04/27 17:28:59  rshortt
 # Added the proper fileheader and some notes.
 #
@@ -43,112 +45,87 @@ import sys
 import os
 import select
 import struct
-import socket
 import traceback
-import threading
 from time import sleep
 
-# We need to use stuff from the main directory
-sys.path += [ '..', '.' ]
-
-# Configuration file. Determines where to look for AVI/MP3 files, etc
 import config
 import plugin
+import rc
+
+DEBUG = 0
+
+TRUE = 1
+FALSE = 0
 
 
 class PluginInterface(plugin.DaemonPlugin):
 
     def __init__(self):
         plugin.DaemonPlugin.__init__(self)
+
         self.plugin_name = 'JOY'
-
-        self.thread = joy_thread()
-        self.thread.setDaemon(1)
-        self.thread.start()
-
-
-class joy_thread(threading.Thread):
-
-    def __init__(self):
-        threading.Thread.__init__(self)
-        
-        # self.mode_flag = threading.Event()
-
-        self.host = config.REMOTE_CONTROL_HOST
-        self.port = config.REMOTE_CONTROL_PORT
+        self.device_name = ''
      
         if config.JOY_DEV == 0:
             print 'Joystick input module disabled, exiting'
             return
 
-        if not config.ENABLE_NETWORK_REMOTE:
-            print 'You must have ENABLE_NETWORK_REMOTE = 1 in your local_conf.py!!'
-            return
-
-
-    def sendcmd(self, cmd):
-    
-        # Set up the UDP/IP connection to the Freevo main application
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.device_name = '/dev/input/js'+str((config.JOY_DEV - 1))
 
         try:
-            sock.sendto(cmd, (config.REMOTE_CONTROL_HOST, config.REMOTE_CONTROL_PORT))
-        except:
-            print 'Freevo is down, discarding the command!\r'
-        # lazy man's debounce...
-        sleep(0.3)
-    
-    
-    def run(self):
-    
-        # For starters, check if we are even wanted...
-        if config.JOY_DEV == 0:
-            print 'Joystick input module disabled, exiting'
-            return
-    
-        joy_device = '/dev/input/js'+str((config.JOY_DEV - 1))
-        try:
-            j = os.open(joy_device,os.O_RDONLY|os.O_NONBLOCK)
+            self.joyfd = os.open(self.device_name, 
+                                      os.O_RDONLY|os.O_NONBLOCK)
         except OSError:
-            print 'Unable to open %s, trying /dev/js%s...' % (joy_device,joy_device)
-            joy_device = '/dev/js'+str((config.JOY_DEV - 1))
+            print 'Unable to open %s, trying /dev/js%s...' % \
+                  (self.device_name, self.device_name)
+            self.device_name = '/dev/js'+str((config.JOY_DEV - 1))
+
             try:
-                j = os.open(joy_device,os.O_RDONLY|os.O_NONBLOCK)
+                self.joyfd = os.open(self.device_name, 
+                                          os.O_RDONLY|os.O_NONBLOCK)
             except OSError:
-                print 'Unable to open %s, check modules and/or permissions' % joy_device
+                print 'Unable to open %s, check modules and/or permissions' % \
+                      self.device_name
                 print 'exiting...'
-                return()
+                return
     
-        print 'using joystick',config.JOY_DEV
-        command = ''
+        print 'using joystick', config.JOY_DEV
     
-        while 1:
-            r,w,e=select.select([j],[],[],0)
-            if r:
-                c = os.read(j,8)
-            data = struct.unpack('IhBB',c)
-            if data[2] == 1 & data[1] == 1:
-                button = 'button '+str((data[3] + 1))
-                command = config.JOY_CMDS.get(button, '')
-                sleep(0.3) # the direction pad can use lower debounce time
-            if data[2] == 2:
-                if ((data[3] == 1) & (data[1] < 0)):
-                    button = 'up'
-                    command = config.JOY_CMDS['up']
-                if ((data[3] == 1) & (data[1] > 0)):
-                    button = 'down'
-                    command = config.JOY_CMDS['down']
-                if ((data[3] == 0) & (data[1] < 0)):
-                    button = 'left'
-                    command = config.JOY_CMDS['left']
-                if ((data[3] == 0) & (data[1] > 0)):
-                    button = 'right'
-                    command = config.JOY_CMDS['right']
-            if command != '':
-                print 'Translation: "%s" -> "%s"' % (button, command)
-                self.sendcmd(command)
-                command = ''
-            sleep(0.1)
-    
+        self.poll_intervall = 3
+        self.poll_menu_only = FALSE
+
+
+    def poll(self):
+        command = ''    
+        if DEBUG: print 'self.joyfd = %s' % self.joyfd
+        (r, w, e) = select.select([self.joyfd], [], [], 0)
+        if DEBUG: print 'r,w,e = %s,%s,%s' % (r,w,e)
+        
+        if r:
+            c = os.read(self.joyfd, 8)
+        else: 
+            return
+
+        data = struct.unpack('IhBB', c)
+        if data[2] == 1 & data[1] == 1:
+            button = 'button '+str((data[3] + 1))
+            command = config.JOY_CMDS.get(button, '')
+            sleep(0.3) # the direction pad can use lower debounce time
+        if data[2] == 2:
+            if ((data[3] == 1) & (data[1] < 0)):
+                button = 'up'
+                command = config.JOY_CMDS['up']
+            if ((data[3] == 1) & (data[1] > 0)):
+                button = 'down'
+                command = config.JOY_CMDS['down']
+            if ((data[3] == 0) & (data[1] < 0)):
+                button = 'left'
+                command = config.JOY_CMDS['left']
+            if ((data[3] == 0) & (data[1] > 0)):
+                button = 'right'
+                command = config.JOY_CMDS['right']
+        if command != '':
+            if DEBUG: print 'Translation: "%s" -> "%s"' % (button, command)
+            rc.post_event(command)
     
 
