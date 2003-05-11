37981b1b2679a6b07a959afdbb8290cbe350ad21
Rob Shortt <rob@tvcentric.com>
2003-05-11 22:44:02 +0000

    A new webserver based on twisted web.  This webserver will support user authentication.
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@2475 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- src/www/webserver.py
+++ src/www/webserver.py
@@ -1,6 +1,8 @@
+#!/usr/bin/env python
+
 #if 0 /*
 # -----------------------------------------------------------------------
-# webserver.py - Simple httpd daemon
+# webserver.py - A small webserver based on twisted.web
 # -----------------------------------------------------------------------
 # $Id$
 #
@@ -9,14 +11,9 @@
 #
 # -----------------------------------------------------------------------
 # $Log$
-# Revision 1.3  2003/05/01 13:43:14  dischi
-# Thanks to Michael Ruelle the webserver is now a plugin
+# Revision 1.4  2003/05/11 22:44:02  rshortt
+# A new webserver based on twisted web.  This webserver will support user authentication.
 #
-# Revision 1.2  2003/03/04 05:43:39  krister
-# Added port settings for the builtin webserver. This is still work in progress!
-#
-# Revision 1.1  2003/02/28 17:52:41  krister
-# First version of an internal python webserver. Work in progress.
 #
 #
 # -----------------------------------------------------------------------
@@ -41,175 +38,35 @@
 # ----------------------------------------------------------------------- */
 #endif
 
-import os
 import sys
-import urllib
-import time
-import traceback
 
-from BaseHTTPServer import HTTPServer
-from CGIHTTPServer import CGIHTTPRequestHandler
+import config
 
-import config # Freevo config
+from twisted.internet import app
+from twisted.web import static, server, vhost, twcgi, script, trp
 
-# CGI Script functions
-import www.htdocs2.guide as guide
-import www.htdocs2.edit_favorite as edit_favorite
-import www.htdocs2.favorites as favorites
-import www.htdocs2.library as library
-import www.htdocs2.manualrecord as manualrecord
-import www.htdocs2.record as record
-import www.htdocs2.search as search 
+from twisted.python import log
 
+DEBUG = 1
 
-def cgi_test():
-    print 'Content-type: text/html'
-    print ''
-    print 'Hello from the test CGI app'
-    
+TRUE = 1
+FALSE = 0
 
-# Translate CGI script names into python module+function
-# The modules are *not* reloaded, the webserver must be restarted
-# if any changes are made.
-cgi_apps = { 'guide.cgi' : guide.run_cgi,
-             'edit_favorite.cgi' : edit_favorite.run_cgi,
-             'favorites.cgi' : favorites.run_cgi,
-             'library.cgi' : library.run_cgi,
-             'manualrecord.cgi' : manualrecord.run_cgi,
-             'record.cgi' : record.run_cgi,
-             'search.cgi' : search.run_cgi,
-             'tst.cgi' : cgi_test }
+logfile = '/tmp/freevoweb.log'
+log.startLogging(open(logfile, 'a'))
 
+docRoot = './htdocs'
 
-class FreevoHTTPServer(HTTPServer):
+root = static.File(docRoot)
+root.processors = {
+            '.cgi': twcgi.CGIScript,
+            '.epy': script.PythonScript,
+            '.rpy': script.ResourceScript,
+}
 
-    def verify_request(self, request, client_address):
-        ip = client_address[0]
-        if not config.WWW_IP_ALLOW or ip in config.WWW_IP_ALLOW:
-            return 1
-        else:
-            try:
-                request.send('Content-type: text/plain\n\n')
-                request.send('Error!\nClient IP %s not authorized!\n' % ip)
-            except:
-                pass
-            return 0
+root.putChild('vhost', vhost.VHostMonsterResource())
+site = server.Site(root)
+application = app.Application('web')
+application.listenTCP(config.WWW_PORT, site)
 
-    
-class FreevoCGIHTTPRequestHandler(CGIHTTPRequestHandler):
-    """This is a special CGI handler. It never executes external
-    CGI apps, instead it knows about some builtin scripts that can
-    be run.
-    """
-
-    def translate_path(self, path):
-        p = CGIHTTPRequestHandler.translate_path(self, path)
-        p = '%s/src/www/htdocs%s' % ( os.getcwd(), p[len(os.getcwd()):])
-        return p
-
-
-    def is_cgi(self):
-        if '?' in self.path:
-            if self.path.split('?')[0].endswith('.cgi'):
-                return 1
-            else:
-                return 0
-        else:
-            if self.path.endswith('.cgi'):
-                return 1
-            else:
-                return 0
-
-
-    def run_cgi(self):
-        """Execute a CGI script."""
-
-        if '?' in self.path:
-            cgi, query = os.path.basename(self.path).split('?')
-        else:
-            cgi = os.path.basename(self.path)
-            query = ''
-
-        self.log_message('CGI: "%s" "%s"', `cgi`, `query`)
-        scriptname = cgi
-        if cgi in cgi_apps:
-            cgi_app = cgi_apps[cgi]
-        else:
-            self.send_error(404, "No such CGI script (%s)" % `scriptname`)
-            return
-
-        # Reference: http://hoohoo.ncsa.uiuc.edu/cgi/env.html
-        # XXX Much of the following could be prepared ahead of time!
-        rest = os.path.basename(self.path)
-        env = {}
-        env['SERVER_SOFTWARE'] = self.version_string()
-        env['SERVER_NAME'] = self.server.server_name
-        env['HTTP_HOST'] = self.server.server_name
-        env['GATEWAY_INTERFACE'] = 'CGI/1.1'
-        env['SERVER_PROTOCOL'] = self.protocol_version
-        env['SERVER_PORT'] = str(self.server.server_port)
-        env['REQUEST_METHOD'] = self.command
-        uqrest = urllib.unquote(rest)
-        env['PATH_INFO'] = uqrest
-        env['PATH_TRANSLATED'] = self.translate_path(uqrest)
-        env['SCRIPT_NAME'] = scriptname
-        if query:
-            env['QUERY_STRING'] = query
-        host = self.address_string()
-        if host != self.client_address[0]:
-            env['REMOTE_HOST'] = host
-        env['REMOTE_ADDR'] = self.client_address[0]
-        # XXX AUTH_TYPE
-        # XXX REMOTE_USER
-        # XXX REMOTE_IDENT
-        if self.headers.typeheader is None:
-            env['CONTENT_TYPE'] = self.headers.type
-        else:
-            env['CONTENT_TYPE'] = self.headers.typeheader
-        length = self.headers.getheader('content-length')
-        if length:
-            env['CONTENT_LENGTH'] = length
-        accept = []
-        for line in self.headers.getallmatchingheaders('accept'):
-            if line[:1] in "\t\n\r ":
-                accept.append(line.strip())
-            else:
-                accept = accept + line[7:].split(',')
-        env['HTTP_ACCEPT'] = ','.join(accept)
-        ua = self.headers.getheader('user-agent')
-        if ua:
-            env['HTTP_USER_AGENT'] = ua
-        co = filter(None, self.headers.getheaders('cookie'))
-        if co:
-            env['HTTP_COOKIE'] = ', '.join(co)
-        # XXX Other HTTP_* headers
-        os.environ.update(env)
-
-        sys.stdout = self.wfile
-        self.send_response(200, "Script output follows")
-
-        sys.__stdout__.write('cgi_app starting...\n')
-        t0 = time.time()
-        cgi_app()
-        self.log_message('CGI script done (%1.1f seconds)', (time.time() - t0))
-        
-
-def run():
-    srvaddr = ('', config.WWW_PORT)
-
-    while 1:
-        try:
-            srvobj = FreevoHTTPServer(srvaddr, FreevoCGIHTTPRequestHandler)
-            srvobj.serve_forever()
-        except KeyboardInterrupt:
-            sys.exit()
-        except:
-            traceback.print_exc()
-            pass
-        sys.__stdout__.write('Ooops, server went down, restarting...\n')
-        time.sleep(3)
-
-
-if __name__ == '__main__':
-    os.chdir(os.environ['FREEVO_STARTDIR'])
-    run()
+application.run(save=0)
