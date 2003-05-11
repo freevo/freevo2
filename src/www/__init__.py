c609ecdaa684f66285b55b7ecef243d2959404c3
Rob Shortt <rob@tvcentric.com>
2003-05-11 22:46:33 +0000

    Changes for the new webserver.
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@2476 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- src/www/__init__.py
+++ src/www/__init__.py
@@ -7,7 +7,6 @@ import traceback
 sys.path += ['./src/www','./src/tv']
 
 import plugin
-import www.webserver as webserver
 
 TRUE  = 1
 FALSE = 0
@@ -16,21 +15,12 @@ FALSE = 0
 class PluginInterface(plugin.DaemonPlugin):
     def __init__(self):
         plugin.DaemonPlugin.__init__(self)
-        self.thread = web_thread()
+        self.pid = None
+
         try:
-            # self.run()
-            self.thread.start()
+            self.pid = os.spawnlp(os.P_NOWAIT, './freevo', './freevo', 'execute', 'src/www/webserver.py')
         except:
             print 'Crash!'
             traceback.print_exc()
             sleep(1)
                                                                                 
-class web_thread(threading.Thread):
-
-    def __init__(self):
-        threading.Thread.__init__(self)
-        self.mode_flag = threading.Event()
-
-    def run(self):
-        webserver.run()
-
