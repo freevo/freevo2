6e3a4e92ca956756c3ec0a00c3861d771d5bd85e
Rob Shortt <rob@tvcentric.com>
2003-05-14 12:24:31 +0000

    Comment out VCR_CMD, add some other comments.
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@2511 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- record_config.py
+++ record_config.py
@@ -1,12 +1,18 @@
 # This is the config file used by record_server.py.
 
 import plugin
+
+# We need to remove all of the plugins activated by the main config.
 plugin.all_plugins = []
 plugin.ptl = {}
 
 DEBUG = 0
 
-VCR_CMD = ('/var/media/bin/tvrecord %(channel)s %(seconds)s "%(filename)s"')    
+# XXX Note:  This VCR_CMD is not used yet, please modify the one in 
+# local_conf.py.  Beware that the recording plugin expects a different format,
+# 3 options instead of 4: %(channel)s, %(seconds)s, and %(filename)s.
+#
+# VCR_CMD = ('/var/media/bin/tvrecord %(channel)s %(seconds)s "%(filename)s"')    
 
 RECORD_SERVER_IP = 'localhost'
 RECORD_SERVER_PORT = 18001
