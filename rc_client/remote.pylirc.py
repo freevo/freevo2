d96fd033ef4b2bf52fec7b6b4b6d2f0d9178f466
Dirk Meyer <dischi@freevo.org>
2003-06-07 11:34:01 +0000

    cleanup
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@2640 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- rc_client/remote.pylirc.py
+++ rc_client/remote.pylirc.py
@@ -1,17 +1,43 @@
-#
+#if 0 /*
+# -----------------------------------------------------------------------
 # remote.py  -  Freevo remote control handling.
-#
-# This is a standalone application that reads commands from either the
-# lirc daemon or the keyboard, and sends UDP commands to the freevo main app.
-#
+# -----------------------------------------------------------------------
 # $Id$
+#
+# Notes:
+# Todo:        
+#
+# -----------------------------------------------------------------------
+# $Log$
+# Revision 1.2  2003/06/07 11:34:01  dischi
+# cleanup
+#
+#
+# -----------------------------------------------------------------------
+# Freevo - A Home Theater PC framework
+# Copyright (C) 2002 Krister Lagerstrom, et al. 
+# Please see the file freevo/Docs/CREDITS for a complete list of authors.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MER-
+# CHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+#
+# ----------------------------------------------------------------------- */
+#endif
 
 import sys
 import socket
-import random
-import termios, tty, time, os
-import string, popen2, fcntl, select
-import traceback
+import tty, time, os
 
 import pylirc
 
@@ -21,12 +47,9 @@ sys.path += [ '..', '.','src/']
 # Configuration file. Determines where to look for AVI/MP3 files, etc
 import config
 
-# Various utilities
-import util
 
 # Set to 1 for debug output
 DEBUG = 1
-
 
 cmds_kbd = {
     'S'           : 'SLEEP',
@@ -187,6 +210,7 @@ def main():
 
     
 if __name__ == "__main__":
+    import traceback
     try:
         main()
     except:
