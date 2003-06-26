d1905d3bbf40fc62596847a4227da2b9da322d88
Aubin Paul <aubin@outlyer.org>
2003-06-26 14:08:34 +0000

    Added a on screen IMDB info plugin from Bjorn Svensson, changes I made:
    o Converted to "AlertBox" so it prompts the user to say "Ok"
    o Added a line to use the imdb image  as an icon. It doesn't work yet, but
       it's something in the popup box, not the plugin.
    
    
    Activate with
    
    plugin.activate('video.imdb_info')
    
    and you can add a "hotkey" to do it as an event:
    
    EVENTS['menu']['2'] = Event(MENU_CALL_ITEM_ACTION, arg='video.imdb_info')
    (not tested)
    
    
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@2821 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

---
+++ src/video/plugins/imdb_info.py
@@ -0,0 +1,52 @@
+# -----------------------------------------------------------------------
+# info.py - Plugin for displaying movieinfo
+# -----------------------------------------------------------------------
+#
+# Notes: Info plugin.
+#        You can show IMDB informations for video items with this plugin.
+#        Activate with: plugin.activate('video.info')
+#        You can also bind it to a key (in this case key 2):
+#        EVENTS['menu']['2'] = Event(MENU_CALL_ITEM_ACTION, arg='info_show')
+#
+# Todo:  - Scaling and nice graphics
+#
+# -----------------------------------------------------------------------
+
+import os
+
+import menu
+import config
+import plugin
+import re
+import time
+
+from video import xml_parser
+from gui.AlertBox import AlertBox
+
+
+class PluginInterface(plugin.ItemPlugin):        
+
+    def actions(self, item):
+        self.item = item
+        if item.type == 'video' and item.info:
+            if item.mode == 'file':
+                return [ ( self.info_showdata, 'Show info for this file',
+                           'info_show') ]
+        return []
+
+
+            
+    def info_showdata(self, arg=None, menuw=None):
+        """
+        show info for this item
+        """
+
+        file = self.item.xml_file
+        infolist = xml_parser.save_parseMovieFile(file, os.path.dirname(file),[])
+        for info in infolist:
+            print info.image
+            box = AlertBox(icon=info.image, width=550, height=400, text=' %s\n \n %s\n \n Year: %s\n Genre: %s\n Rating: %s\n Runtime: %s' % (info.name,info.info['plot'],info.info['year'],info.info['genre'],info.info['rating'],info.info['runtime']))
+            box.show()
+#            time.sleep(3)
+#            box.destroy()
+
