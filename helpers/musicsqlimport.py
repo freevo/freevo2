b24a83eadce3fddd72e7a2c37b98dc205dcbfb36
Aubin Paul <aubin@outlyer.org>
2003-06-30 04:38:47 +0000

    Changed type of 'last_play' field to float so we can use python's time.time() instead of timestamps.
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@2868 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- helpers/musicsqlimport.py
+++ helpers/musicsqlimport.py
@@ -51,7 +51,7 @@ def create_db():
     print DATABASE
     db = sqlite.connect(DATABASE)
     cursor = db.cursor()
-    cursor.execute("CREATE TABLE music (id INTEGER PRIMARY KEY, dirtitle VARCHAR(255), md5 VARCHAR(33), path VARCHAR(255), filename VARCHAR(255), type VARCHAR(3), artist VARCHAR(255), title VARCHAR(255), album VARCHAR(255), year VARCHAR(255), track NUMERIC(3), track_total NUMERIC(3), bpm NUMERIC(3), last_play timestamp, play_count NUMERIC, start_time VARCHAR, end_time VARCHAR, rating NUMERIC, eq  VARCHAR)")
+    cursor.execute("CREATE TABLE music (id INTEGER PRIMARY KEY, dirtitle VARCHAR(255), md5 VARCHAR(33), path VARCHAR(255), filename VARCHAR(255), type VARCHAR(3), artist VARCHAR(255), title VARCHAR(255), album VARCHAR(255), year VARCHAR(255), track NUMERIC(3), track_total NUMERIC(3), bpm NUMERIC(3), last_play float, play_count NUMERIC, start_time VARCHAR, end_time VARCHAR, rating NUMERIC, eq  VARCHAR)")
     db.commit()
     db.close()
 
