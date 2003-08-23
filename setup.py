b38594cea9203e16402c11542f0acc4c18676e80
Dirk Meyer <dischi@freevo.org>
2003-08-23 09:31:06 +0000

    respect the changes made
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@3393 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- setup.py
+++ setup.py
@@ -6,12 +6,6 @@
 # This is the first version of a real python setup script to install
 # Freevo directly into the system. This script can't be used with
 # the runtime, because the runtime contains it's own python.
-#
-# TODO:
-# o add more needed libs to 'libs'
-# o fix fbcon stuff in freevo_config.py. It looks for contrib/fbcon but
-#   installed the scripts are somewhere else
-# o test it on more platforms than mine
 
 
 __revision__ = "$Id$"
@@ -22,7 +16,10 @@ import os
 import sys
 
 libs = ( ('mmpython', 'http://www.sf.net/projects/mmpython' ),
-         ('pygame', 'http://www.pygame.org') )
+         ('pygame', 'http://www.pygame.org'),
+         ('Image', 'http://www.pythonware.com/products/pil/'),
+         ('twisted', 'http://www.twistedmatrix.com/') )
+
 
 def package_finder(result, dirname, names):
     for name in names:
@@ -52,8 +49,8 @@ for p in package_dir:
 
 data_files = []
 os.path.walk('./share', data_finder, data_files)
-os.path.walk('./contrib', data_finder, data_files)
-os.path.walk('./helpers', data_finder, data_files)
+os.path.walk('./contrib/fbcon', data_finder, data_files)
+os.path.walk('./contrib/xmltv', data_finder, data_files)
 
 # copy freevo_config.py to share/freevo. It's the best place to put it
 # for now, but the location should be changed
@@ -76,22 +73,12 @@ if len(sys.argv) > 1 and sys.argv[1].lower() == 'install':
             print 'please download it from %s and install it' % url
             sys.exit(1)
             
-#     # compile runapp
-#     if os.environ.has_key('LOGDIR') and os.environ['LOGDIR']:
-#         logdir='"%s"' % os.environ['LOGDIR']
-#     else:
-#         logdir='"/var/log/freevo"'
-#     print 'building runapp'
-#     os.system('gcc -O2 -Wall -static -o runapp runapp.c '
-#               '-DRUNAPP_LOGDIR=\\"%s\\" -DDEBUG' % logdir)
-#     # bad hack, this is no script, but it is compiled and
-#     # ready to install now
-#     scripts.append('runapp')
 
+version = open('VERSION').read()[:-1]
 
 # now start the python magic
 setup (name = "freevo",
-       version = "1.3.4cvs",
+       version = version,
        description = "Freevo",
        author = "Krister Lagerstrom, et al.",
        author_email = "",
@@ -102,3 +89,4 @@ setup (name = "freevo",
        packages = packages,
        data_files = data_files
        )
+
