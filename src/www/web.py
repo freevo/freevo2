d23e0e06eecc96f99d745fd0b02ed635d943af5f
Rob Shortt <rob@tvcentric.com>
2003-09-05 02:26:28 +0000

    Moving these things into freevo_config.py.
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@3576 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- src/www/web.py
+++
@@ -1,9 +0,0 @@
-
-# The tv guide is divided into 30 minute intervals
-INTERVAL=60*30
-
-# Lets show 6 intervals at once
-n_cols = 6
-
-STYLESHEET = 'styles/main.css'
-JAVASCRIPT = 'scripts/display_prog-head.js'
