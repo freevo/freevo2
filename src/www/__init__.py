a1d056883f2494affba30178530e016e69a24ab1
Dirk Meyer <dischi@freevo.org>
2003-09-22 20:21:01 +0000

    autodisable the plugin for display=dxr3
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@3809 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- src/www/__init__.py
+++ src/www/__init__.py
@@ -1,17 +1,17 @@
 import os
 import config
 import sys
-import threading
-import traceback
-
 import plugin
-
-TRUE  = 1
-FALSE = 0
-
 
 class PluginInterface(plugin.DaemonPlugin):
     def __init__(self):
+        if config.CONF.display == 'dxr3':
+            print 'For some strange reason, the starting of the webserver inside'
+            print 'Freevo messes up with the DXR3 output. The webserver plugin'
+            print 'will be disabled. Start it from outside Freevo with'
+            print 'freevo webserver [start|stop]'
+            self.reason = 'dxr3 output'
+            return
         plugin.DaemonPlugin.__init__(self)
         self.pid = None
         os.system('%s webserver start' % os.environ['FREEVO_SCRIPT'])
