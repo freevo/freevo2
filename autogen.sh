667d71f1edd7a453d379f2c501735e87bf756437
Dirk Meyer <dischi@freevo.org>
2003-09-23 18:26:41 +0000

    get some wiki pages
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@3825 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- autogen.sh
+++ autogen.sh
@@ -16,6 +16,24 @@ echo generating freevo_howto html files
 
 cd Docs
 docbook2html -o howto freevo_howto.sgml 2>&1 | grep -v jade
+cd ..
 
+
+wiki=src/www/htdocs/help/wiki
+
+echo
+echo getting some wiki pages
+rm -rf $wiki
+mkdir $wiki
+
+for file in FxdFiles SkinInfo; do
+    wget http://freevo.sourceforge.net/cgi-bin/moin.cgi/DocumentationPage_2f$file \
+	-O $wiki/$file.html
+done
+
+wget http://freevo.sourceforge.net/cgi-bin/moin.cgi/FrequentlyAskedQuestions \
+    -O $wiki/faq.html
+
+exit 0
 
 # end of autogen.sh 
