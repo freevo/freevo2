0b6e2d951e46adc7a476dd458ed984ecff28bc7f
Dirk Meyer <dischi@freevo.org>
2003-10-03 15:38:07 +0000

    auto update
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@3894 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

---
+++ i18n/update.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+print 'update pot file'
+
+f = open('freevo.pot')
+
+fxd_strings = []
+
+add = 0
+for line in f.readlines():
+    if line.find('Manualy added from fxd skin files') > 0:
+        add = 1
+    if add:
+        fxd_strings.append(line)
+        
+f.close()
+
+os.system('(cd ../src ; find . -name \*.py | xargs xgettext -o ../i18n/freevo.pot)')
+
+f = open('freevo.pot', 'a')
+    
+for line in fxd_strings:
+    f.write(line)
+
+f.close()
+
+print '',
+for file in ([ os.path.join('.', fname) for fname in os.listdir('.') ]):
+    if os.path.isdir(file) and file.find('CVS') == -1:
+        print 'update %s...' % file,
+        sys.stdout.flush()
+        file = os.path.join(file, 'LC_MESSAGES/freevo.po')
+        mo = os.path.splitext(file)[0] + '.mo'
+        os.system('msgmerge --update --backup=off %s ./freevo.pot' % file)
+        os.system('msgfmt -o %s %s' % (mo, file))
+        
