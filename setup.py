90f1d856c1542f556100bb4536f5ac493114ed28
Dirk Meyer <dischi@freevo.org>
2003-10-18 13:05:16 +0000

    use new freevo distutils
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@4042 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- setup.py
+++ setup.py
@@ -17,36 +17,13 @@ import sys
 
 sys.path.append('./src')
 import version
+import util.distutils
 
-libs = ( ('mmpython', 'http://www.sf.net/projects/mmpython' ),
-         ('pygame', 'http://www.pygame.org'),
-         ('Image', 'http://www.pythonware.com/products/pil/'),
-         ('twisted', 'http://www.twistedmatrix.com/') )
+util.distutils.check_libs((('mmpython', 'http://www.sf.net/projects/mmpython' ),
+                           ('pygame', 'http://www.pygame.org'),
+                           ('Image', 'http://www.pythonware.com/products/pil/'),
+                           ('twisted', 'http://www.twistedmatrix.com/')))
 
-
-def package_finder(result, dirname, names):
-    for name in names:
-        if os.path.splitext(name)[1] == '.py':
-            import_name = dirname.replace('/','.').replace('..src', 'freevo')
-            result[import_name] = dirname
-            return result
-    return result
-
-def data_finder(result, dirname, names):
-    files = []
-    for name in names:
-        if os.path.isfile(os.path.join(dirname, name)):
-            if name != 'freevo.pot':
-                files.append(os.path.join(dirname, name))
-            
-    if files and dirname.find('/CVS') == -1:
-        result.append((dirname.replace('./share', 'share/freevo').
-                       replace('./src/www', 'share/freevo').\
-                       replace('./i18n', 'share/locale').\
-                       replace('./contrib', 'share/freevo/contrib').\
-                       replace('./Docs', 'share/doc/freevo-%s' % version.__version__).\
-                       replace('./helpers', 'share/freevo/helpers'), files))
-    return result
 
 # check if everything is in place
 if not os.path.isdir('./Docs/howto'):
@@ -54,21 +31,7 @@ if not os.path.isdir('./Docs/howto'):
     print 'of Freevo. Please run ./autogen.sh first'
     sys.exit(0)
 
-# create list of source files
-package_dir = {}
-os.path.walk('./src', package_finder, package_dir)
-packages = []
-for p in package_dir:
-    packages.append(p)
-
-# create list of data files (share)
-data_files = []
-os.path.walk('./share', data_finder, data_files)
-os.path.walk('./contrib/fbcon', data_finder, data_files)
-os.path.walk('./contrib/xmltv', data_finder, data_files)
-os.path.walk('./src/www/htdocs', data_finder, data_files)
-os.path.walk('./i18n', data_finder, data_files)
-os.path.walk('./Docs/howto', data_finder, data_files)
+data_files = util.distutils.data_files
 
 for f in ('BUGS', 'COPYING', 'ChangeLog', 'INSTALL', 'README'):
     data_files.append(('share/doc/freevo-%s' % version.__version__, ['%s' % f ]))
@@ -80,22 +43,6 @@ data_files.append(('share/freevo', [ 'freevo_config.py' ]))
 
 scripts = ['freevo']
 
-
-# ok, this can't be done by setup it seems, so we have to do it
-# manually
-if len(sys.argv) > 1 and sys.argv[1].lower() == 'install':
-    # check for needed libs
-    for module, url in libs:
-        print 'checking for %-13s' % (module+'...'),
-        try:
-            exec('import %s' % module)
-            print 'found'
-        except:
-            print 'not found'
-            print 'please download it from %s and install it' % url
-            sys.exit(1)
-            
-
 # now start the python magic
 setup (name = "freevo",
        version = version.__version__,
@@ -105,8 +52,8 @@ setup (name = "freevo",
        url = "http://freevo.sf.net",
 
        scripts=scripts,
-       package_dir = package_dir,
-       packages = packages,
+       package_dir = util.distutils.package_dir,
+       packages = util.distutils.packages,
        data_files = data_files
        )
 
