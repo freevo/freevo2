408b9c50b5e0b558f84c545ad0c18e377e04c56b
Aubin Paul <aubin@outlyer.org>
2003-10-22 19:21:21 +0000

    A little flexibility in the autogen.sh script; if you don't want to fetch documentation or use docbook, you can call it with one of the options. Type "./autogen.sh help" to see them. For the old behaviour, just call autogen.sh as before (no options)
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@4114 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- autogen.sh
+++ autogen.sh
@@ -5,35 +5,64 @@
 # Dirk Meyer  <dmeyer@tzi.de>
 # $Id$
 
-for file in `find i18n -name freevo.po`; do
-    out=`echo $file | sed 's/\.po$/.mo/'`
-    echo generating $out
-    msgfmt -o $out $file 2> /dev/null
-done
+gen_i18n() {
+    for file in `find i18n -name freevo.po`; do
+     out=`echo $file | sed 's/\.po$/.mo/'`
+     echo generating $out
+     msgfmt -o $out $file 2> /dev/null
+    done
+}
 
-echo
-echo generating freevo_howto html files
+howto() {
+    echo
+    echo generating freevo_howto html files
 
-cd Docs
-docbook2html -o howto freevo_howto.sgml 2>&1 | grep -v jade
-cd ..
+    cd Docs
+    docbook2html -o howto freevo_howto.sgml 2>&1 | grep -v jade
+    cd ..
+}
 
+wiki() {
+    wiki=src/www/htdocs/help/wiki
 
-wiki=src/www/htdocs/help/wiki
+    echo
+    echo getting some wiki pages
+    rm -rf $wiki
+    mkdir $wiki
 
-echo
-echo getting some wiki pages
-rm -rf $wiki
-mkdir $wiki
-
-for file in FxdFiles SkinInfo; do
+    for file in FxdFiles SkinInfo; do
     wget http://freevo.sourceforge.net/cgi-bin/moin.cgi/DocumentationPage_2f$file \
 	-O $wiki/$file.html
-done
+    done
 
-wget http://freevo.sourceforge.net/cgi-bin/moin.cgi/FrequentlyAskedQuestions \
+    wget http://freevo.sourceforge.net/cgi-bin/moin.cgi/FrequentlyAskedQuestions \
     -O $wiki/faq.html
 
-exit 0
+    exit 0
+}
+
+# Main
+case "$1" in
+    nodocs)
+        gen_i18n
+        ;;
+    nodocbook)
+        gen_i18n
+        wiki
+        ;;
+    help)
+        echo -n "Usage:   "
+        echo $0
+        echo "          nodocs     -  Just generate translations"
+        echo "          nodocbook  -  Fetch Wiki but do not make howto"
+        echo "          <default>  -  Generate translations, fetch wiki and generate Howto"
+        ;;
+    *)
+        gen_i18n
+        howto
+        wiki
+        ;;
+esac
+
 
 # end of autogen.sh 
