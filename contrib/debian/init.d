06389c71ae8f07c187cbf074b579e1a6135a7a30
Aubin Paul <aubin@outlyer.org>
2003-10-25 14:44:02 +0000

    Added an init script, and a debconf question to ask the user if they want to have freevo start up on boot. By default, it puts Freevo at the tail of the bootup sequence so it will wait for the other scripts to load. This is obviously important if you need to initialize your TV, or start X or something before your load Freevo.
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@4142 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

---
+++ contrib/debian/init.d
@@ -0,0 +1,44 @@
+#! /bin/sh
+#
+
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+DAEMON=/usr/bin/freevo
+NAME=freevo
+DESC=freevo
+
+test -x $DAEMON || exit 0
+
+# Include freevo defaults if available
+if [ -f /etc/freevo/boot_config ] ; then
+	. /etc/freevo/boot_config
+fi
+
+set -e
+
+case "$1" in
+  start)
+	echo -n "Starting $DESC: "
+        $DAEMON start
+	echo "$NAME."
+	;;
+  stop)
+	echo -n "Stopping $DESC: "
+        $DAEMON stop
+	echo "$NAME."
+	;;
+  restart)
+	echo -n "Restarting $DESC: "
+        $DAEMON stop
+	sleep 1
+        $DAEMON start
+	echo "$NAME."
+	;;
+  *)
+	N=/etc/init.d/$NAME
+	# echo "Usage: $N {start|stop|restart|reload|force-reload}" >&2
+	echo "Usage: $N {start|stop|restart}" >&2
+	exit 1
+	;;
+esac
+
+exit 0
