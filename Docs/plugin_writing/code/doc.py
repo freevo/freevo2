312fda7d653805d47facb6459cb4a158b5f76799
Dirk Meyer <dischi@freevo.org>
2003-10-31 20:00:24 +0000

    more doc
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@4188 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

---
+++ Docs/plugin_writing/code/doc.py
@@ -0,0 +1,14 @@
+import plugin
+
+class MyPlugin(plugin.Plugin):
+    """
+    This is a short description
+
+    And here should be a long description how this plugin works,
+    how to activate it and what settings the user needs to add
+    to the local_conf.py
+    """
+
+    def __init__(self):
+        pass
+    
