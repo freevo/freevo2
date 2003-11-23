d750530807577697e77303db3042878bd2079b47
Rob Shortt <rob@tvcentric.com>
2003-11-23 20:32:05 +0000

    This is a plugin for sending IR commands to an external tuner.  You can specify your irsend command when you plugin.activate.
    This is totally untested as irsend doesn't work with my receiver.  If you
    would like to use this give it a try and notify of any problems or modify
    it to work correctly.  It is now merely a proof of concept.
    
    
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@4407 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

---
+++ src/tv/plugins/irsend_generic.py
@@ -0,0 +1,91 @@
+
+#if 0 /*
+# -----------------------------------------------------------------------
+# irsend_generic.py - Send commands to an external tv receiver using a
+#                     shell command like irsend from Lirc.  
+# -----------------------------------------------------------------------
+# $Id$
+#
+# Notes:  Plugin wrapper for irsend or any other remote controll command.
+#
+# Todo:        
+#
+# -----------------------------------------------------------------------
+# $Log$
+# Revision 1.1  2003/11/23 20:32:05  rshortt
+# This is a plugin for sending IR commands to an external tuner.  You can
+# specify your irsend command when you plugin.activate.
+#
+# This is totally untested as irsend doesn't work with my receiver.  If you
+# would like to use this give it a try and notify of any problems or modify
+# it to work correctly.  It is now merely a proof of concept.
+#
+#
+#
+# -----------------------------------------------------------------------
+# Freevo - A Home Theater PC framework
+# Copyright (C) 2003 Krister Lagerstrom, et al. 
+# Please see the file freevo/Docs/CREDITS for a complete list of authors.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MER-
+# CHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+#
+# ----------------------------------------------------------------------- */
+#endif
+
+import os, sys, time, string
+import plugin
+
+
+class PluginInterface(plugin.Plugin):
+    """
+    Use this plugin if you need to use Lirc's irsend (or similar) command
+    to tell an external tuner to change the channel.
+    Example usage (local_conf.py):
+
+    plugin_external_tuner = plugin.activate('tv.irsend_echostar', 
+                            args=('/usr/bin/irsend SEND_ONCE <remote_name>', ))
+
+    Where <remote_name> is the name of the remote you are using to send codes
+    with in lircd.conf.
+    """
+
+    def __init__(self, command):
+        plugin.Plugin.__init__(self)
+
+        self.command = command
+
+        plugin.register(self, 'EXTERNAL_TUNER')
+
+
+    def setChannel(self, chan):
+        chan = str(chan)
+        digits = len(chan)
+        chan_args = ''
+
+        for i in range(digits):
+            chan_args += chan[i] + ' '
+
+        self.transmitSignal(chan_args)
+
+
+    def transmitButton(self, button):
+        print 'sending button: %s\n' % button
+        self.transmitSignal(code)
+    
+
+    def transmitSignal(self, code):
+        sendcmd = '%s %s' % (self.command, code)
+        os.system(sendcmd)
+    
