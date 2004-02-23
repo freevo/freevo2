2140b102c4ec9d47f3f9951c253de57579c31bd4
Rob Shortt <rob@tvcentric.com>
2004-02-23 03:43:23 +0000

    Ditch the popup-gui style in favour of a faster and more freevo-like menu.
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@5383 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- src/tv/program_display.py
+++ src/tv/program_display.py
@@ -9,75 +9,9 @@
 #
 #-----------------------------------------------------------------------
 # $Log$
-# Revision 1.26  2004/02/19 04:57:57  gsbarbieri
-# Support Web Interface i18n.
-# To use this, I need to get the gettext() translations in unicode, so some changes are required to files that use "print _('string')", need to make them "print String(_('string'))".
+# Revision 1.27  2004/02/23 03:43:23  rshortt
+# Ditch the popup-gui style in favour of a faster and more freevo-like menu.
 #
-# Revision 1.25  2004/02/18 21:55:11  dischi
-# update to new gui code
-#
-# Revision 1.24  2004/01/09 02:10:00  rshortt
-# Patch from Matthieu Weber to revive add/edit favorites support from the
-# TV interface.
-#
-# Revision 1.23  2003/11/28 20:08:58  dischi
-# renamed some config variables
-#
-# Revision 1.22  2003/11/15 17:24:31  dischi
-# i18n patches from Davin Sagnol
-#
-# Revision 1.21  2003/10/22 02:45:02  rshortt
-# Fix a crash when no recordserver is available.
-#
-# Revision 1.20  2003/10/21 15:11:52  outlyer
-# Remove a stray print... this was probably for debugging.
-#
-# Revision 1.19  2003/10/20 01:41:55  rshortt
-# Moving tv_util from src/tv/ to src/util/.
-#
-# Revision 1.18  2003/10/18 09:33:34  dischi
-# fix some parent handling and remove the PopupBox
-#
-# Revision 1.17  2003/10/14 17:06:02  dischi
-# add datetimeformat patch from patch from Eirik Meland
-#
-# Revision 1.16  2003/10/14 02:30:12  rshortt
-# Changes to make the list update when you remove a scheduled recording.
-#
-# Also made the buttons in the program display on top of one another instead
-# of side by side.  I will be adding more buttons (add to favorites, search
-# for more) and they will look and fit better this way.  I think they would
-# all be better together in a ListBox.
-#
-# Revision 1.15  2003/09/13 21:00:50  outlyer
-# OSDFont has 'name' as an attribute, while Font has 'filename' It was swapped
-# here, and was crashing.
-#
-# Revision 1.14  2003/09/11 14:10:20  outlyer
-# Fix a crash if you press up/down in certain 'record' screens.
-# Also, disable debug information by default; allow the global variable
-# to decide.
-#
-# Revision 1.13  2003/09/07 11:18:27  dischi
-# many optical improvements
-#
-# Revision 1.12  2003/09/06 19:54:04  rshortt
-# Don't crash if there's no description.
-#
-# Revision 1.11  2003/09/06 17:31:17  rshortt
-# Removed 'Description: ' and added a close button.
-#
-# Revision 1.10  2003/09/05 02:48:12  rshortt
-# Removing src/tv and src/www from PYTHONPATH in the freevo script.
-# Therefore any module that was imported from src/tv/ or src/www that
-# didn't have a leading 'tv.' or 'www.' needed it added.  Also moved
-# tv/tv.py to tv/tvmenu.py to avoid namespace conflicts.
-#
-# Revision 1.9  2003/09/01 19:46:02  dischi
-# add menuw to eventhandler, it may be needed
-#
-# Revision 1.8  2003/08/23 12:51:43  dischi
-# removed some old CVS log messages
 #
 #
 #-----------------------------------------------------------------------
@@ -102,334 +36,145 @@
 #
 # ----------------------------------------------------------------------
 
-import time
+import time, traceback
 
-import config, tv.edit_favorite
+import plugin, config, menu
+
 import util.tv_util as tv_util
 import tv.record_client as record_client
-import tv.view_favorites, tv.program_search
 import event as em
 
-from gui.GUIObject import *
-from gui.Border    import *
-from gui.Label     import *
-from gui.ListBox   import *
+from item import Item
+from gui.PopupBox  import *
 from gui.AlertBox  import *
 
 DEBUG = config.DEBUG
 
 
-class ProgramDisplay(PopupBox):
-    """
-    prog      the program to record
-    context   guide or recording
-    left      x coordinate. Integer
-    top       y coordinate. Integer
-    width     Integer
-    height    Integer
-    """
-    
-    def __init__(self, parent=None, prog=None, context=None, left=None, 
-                 top=None, width=600, height=None, vertical_expansion=1):
-
-        self.left = left
-        self.top = top
+class ProgramItem(Item):
+    def __init__(self, parent, prog, context='guide'):
+        Item.__init__(self, parent, skin_type='video')
         self.prog = prog
-        self.font = None
+        self.context = context
 
-        if context:
-            self.context = context
+        self.name = self.title = prog.title
+        self.sub_title = prog.sub_title
+        self.desc = prog.desc
+        self.channel = tv_util.get_chan_displayname(prog.channel_id)
+
+        if hasattr(prog, 'scheduled'):
+            self.scheduled = prog.scheduled
         else:
-            self.context = 'guide'
+            self.scheduled = False
 
-        PopupBox.__init__(self, parent=parent, text=self.prog.title, x=left,
-                          y=top, width=width, height=height,
-                          vertical_expansion=vertical_expansion)
+        self.start = time.strftime(config.TV_DATETIMEFORMAT,
+                                   time.localtime(prog.start))
+        self.stop = time.strftime(config.TV_DATETIMEFORMAT,
+                                       time.localtime(prog.stop))
 
-        self.v_spacing = 15
-        self.h_margin = 20
-        self.v_margin = 20
-
-        self.internal_h_align = Align.LEFT
-
-        used_height = 0
-        
-        if self.prog.sub_title:
-            subtitle_txt = 'Subtitle:  %s' % self.prog.sub_title
-            subtitle = Label(subtitle_txt, self, Align.LEFT)
-            used_height += subtitle.font.height + self.v_spacing
-
-        desc = None
-        if self.prog.desc:
-            desc = Label(self.prog.desc, self, Align.LEFT)
-            desc.set_font(font=self.font.name, size=self.font.size -2,
-                          color=self.font.color)
-
-        chan = Label(_('Channel:  %s') % \
-                      tv_util.get_chan_displayname(self.prog.channel_id), 
-                                                   self, Align.LEFT)
-        chan.set_font(font=self.font.name, size=self.font.size -2,
-                      color=self.font.color)
-
-        start = Label(_('Start:  %s') % time.strftime(config.TV_DATETIMEFORMAT, 
-                                      time.localtime(self.prog.start)),
-                                      self, Align.LEFT)
-        start.set_font(font=self.font.name, size=self.font.size -2,
-                       color=self.font.color)
-
-        stop = Label(_('Stop:  %s') % time.strftime(config.TV_DATETIMEFORMAT, 
-                                     time.localtime(self.prog.stop)), 
-                                     self, Align.LEFT)
-        stop.set_font(font=self.font.name, size=self.font.size -2,
-                      color=self.font.color)
-
-        used_height += chan.font.height + start.font.height + stop.font.height + \
-                       (4 *self.v_spacing)
-        
-        self.buttons = []
-        all_buttons = { 'record': 'Record',
-                        'search': 'Search for more of this program',
-                        'favorites': 'Add to favorites',
-                        'remove': 'Remove',
-                        'cancel': 'CANCEL',
-                      }
-        self.button_config = []
-        if self.context == 'guide':
-            self.button_config = ['record', 'favorites', 'cancel']
-        else:
-            self.button_config = ['remove', 'cancel']
-            
-        for bname in self.button_config:
-            b = Button(_(all_buttons[bname]), width=(width-60)/2)
-            b.set_h_align(Align.CENTER)
-            self.add_child(b)
-            self.buttons.append(b)
-
-        self.buttons[0].toggle_selected()
-        self.current_button = 0
-        
-        if desc:
-            desc.width  = self.width - 30
-            desc.height = self.height - used_height - 100
-            desc.get_rendered_size()
-
-
-        # correct height and top 
-        self.content.layout()
-        needed_height = self.content.layout_manager.needed_space + 2 * self.v_margin
-
-        if needed_height > self.height:
-            self.set_size(self.width, needed_height)
-
-
-    def eventhandler(self, event, menuw=None):
-        if DEBUG: print String(_('ProgramDisplay: event = %s')) % event
-
-        trapped = em.MENU_EVENTS.values()
-
-        #if event in trapped:
-        #    return
-
-
-        #if event in (em.INPUT_UP, em.INPUT_DOWN):
-        if event == em.INPUT_UP:
-            if self.current_button > 0:
-                self.buttons[self.current_button].toggle_selected()
-                self.current_button -= 1
-                self.buttons[self.current_button].toggle_selected()
-            self.draw()
-            self.osd.update(self.get_rect())
-            return
-
-        elif event == em.INPUT_DOWN:
-            if self.current_button < len(self.buttons) - 1:
-                self.buttons[self.current_button].toggle_selected()
-                self.current_button += 1
-                self.buttons[self.current_button].toggle_selected()
-            self.draw()
-            self.osd.update(self.get_rect())
-            return
-        
-
-        # The following block of code was for when the options were in a list
-        # box.  Trying a different way (buttons) now.
-        #if event in (em.INPUT_UP, em.INPUT_DOWN, em.INPUT_LEFT, em.INPUT_RIGHT):
-        #    if DEBUG: print 'ProgramDisplay: should scroll'
-        #    if hasattr(self,'options'):
-        #        return self.options.eventhandler(event)
-        #    else:
-        #        return
-
-        elif event == em.INPUT_ENTER:
-            if self.button_config[self.current_button] == 'record' and self.context == 'guide':
-                (result, msg) = record_client.scheduleRecording(self.prog)
-                if result:
-                    AlertBox(parent=self, 
-                             text=_('"%s" has been scheduled for recording') % \
-                              self.prog.title, handler=self.destroy).show()
-                else:
-                    AlertBox(parent=self, 
-                             text=_('Scheduling Failed: %s') % msg).show()
-
-            # XXX: search for other programs like this
-            elif 0:
-                tv.program_search.ProgramSearch(parent=self, 
-                                             search=self.prog.title).show()
-
-            # XXX: add to favorites - should go to an edit favorites screen
-            elif self.button_config[self.current_button] == 'favorites' and self.context == 'guide':
-                self.destroy()
-                tv.edit_favorite.EditFavorite(parent=self, 
-                                           subject=self.prog).show()
-
-            # next will only happen if we are not viewing from the guide
-            elif self.button_config[self.current_button] == 'remove':
-                (result, msg) = record_client.removeScheduledRecording(self.prog)
-                if result:
-                    # This is confusing, I wanted to click but it's no
-                    # Alertbox
-                    #
-                    # pop = PopupBox(parent=self, 
-                    #                text='"%s" has been removed' % \
-                    #                self.prog.title)
-                    # pop.show()
-                    # time.sleep(2)
-                    # pop.destroy()
-
-                    searcher = None
- 
-                    if self.context == 'recording' and self.parent:
-                        for child in self.parent.children:
-                            if isinstance(child, ScheduledRecordings):
-                                searcher = child
-                                break
-
-                    if searcher:
-                        searcher.refreshList()
-                    self.destroy()
-                    if searcher:
-                        searcher.draw()
-                        self.osd.update()
-
-                else:
-                    AlertBox(parent=self, 
-                             text=_('Remove Failed: %s') % msg).show()
-
-            elif self.button_config[self.current_button] == 'cancel':
-                self.destroy()
-
-        elif event == em.INPUT_EXIT:
-            self.destroy()
-        else:
-            return self.parent.eventhandler(event)
-
-
-
-class ScheduledRecordings(PopupBox):
-    """
-    left      x coordinate. Integer
-    top       y coordinate. Integer
-    width     Integer
-    height    Integer
-    text      String to print.
-    handler   Function to call after pressing ENTER.
-    bg_color  Background color (Color)
-    fg_color  Foreground color (Color)
-    icon      icon
-    border    Border
-    bd_color  Border color (Color)
-    bd_width  Border width Integer
-    """
-
-        
-    def __init__(self, parent='osd', text=None, handler=None, 
-                 left=None, top=None, width=600, height=300, bg_color=None, 
-                 fg_color=None, icon=None, border=None, bd_color=None, 
-                 bd_width=None, vertical_expansion=1):
-
-        if not text:
-            text = _('Scheduled Recordings')
-        
-        PopupBox.__init__(self, text, handler, left, top, width, height,
-                          icon, vertical_expansion, parent=parent)
-
-        self.result = False
-
-        (self.server_available, msg) = record_client.connectionTest()
-        if not self.server_available:
-            errormsg = Label(_('Record server unavailable: %s') % msg,
-                             self, Align.CENTER)
-            return
-
-        self.internal_h_align = Align.CENTER
-
-        items_height = Button('foo').height + 2
-        self.num_shown_items = 8
-        self.results = ListBox(width=(self.width-2*self.h_margin),
-                               height=self.num_shown_items*items_height,
-                               show_v_scrollbar=0)
-        self.results.y_scroll_interval = self.results.items_height = items_height
-        max_results = 10
-
-        self.results.set_h_align(Align.CENTER)
-        self.add_child(self.results)
-
-        self.refreshList()
-
-
-    def refreshList(self):
-        (self.result, recordings) = record_client.getScheduledRecordings()
-        if self.result:
-            progs = recordings.getProgramList()
-        else:
-            errormsg = Label(_('Get recordings failed: %s') % recordings, 
-                             self, Align.CENTER)
-            return 
-
-
-        if self.result:
-            i = 0
-            self.results.items = []
-
-            if len(progs) > self.num_shown_items:
-                self.results.show_v_scrollbar = 1
+        self.categories = ''
+        for cat in prog.categories:
+            if prog.categories.index(cat) > 0:
+                self.categories += ', %s' % cat
             else:
-                self.results.show_v_scrollbar = 0
+                self.categories += '%s' % cat
+
+        self.ratings = ''
+        for rat in prog.ratings.keys():
+            self.ratings += '%s ' % rat
+
+
+    def actions(self):
+        return [( self.display_program , 'Display program' )]
+
+
+    def display_program(self, arg=None, menuw=None):
+        items = []
+        if self.context == 'schedule':
+	    items.append(menu.MenuItem(_('Remove from schedule'), 
+                                       action=self.remove_program))
+        elif self.context == 'guide':
+	    items.append(menu.MenuItem(_('Schedule for recording'), 
+                                       action=self.schedule_program))
+
+        if self.context != 'search':
+            items.append(menu.MenuItem(_('Search for more of this program'), 
+                                       action=self.find_more))
+
+        items.append(menu.MenuItem(_('Add to favorites'), 
+                                   action=self.add_favorite))
+
+        program_menu = menu.Menu(_('Program Menu'), items, 
+                                 item_types = 'tv program menu')
+        rc.app(None)
+        program_menu.infoitem = self
+        menuw.pushmenu(program_menu)
+        menuw.refresh()
+
+
+    def add_favorite(self, arg=None, menuw=None):
+        pass
+
+
+    def find_more(self, arg=None, menuw=None):
+        # XXX: The searching part of this function could probably be moved
+	#      into a util module or record_client itself.
+        _debug_('searching for: %s' % self.prog.title)
+
+        pop = PopupBox(text=_('Searching, please wait...'))
+        pop.show()
+
+        items = []
+        (result, matches) = record_client.findMatches(self.prog.title)
+                             
+        pop.destroy()
+        if result:
+            _debug_('search found %s matches' % len(matches))
 
             f = lambda a, b: cmp(a.start, b.start)
-            progs = progs.values()
-            progs.sort(f)
-            for prog in progs:
-                i += 1
-                self.results.add_item(text='%s %s: %s' % \
-                                      (time.strftime(config.TV_DATETIMEFORMAT,
-                                                     time.localtime(prog.start)),
-                                       tv_util.get_chan_displayname(prog.channel_id),
-                                       prog.title),
-                                      value=prog)
-
-            space_left = self.num_shown_items - i
-            if space_left > 0:
-                for i in range(space_left):
-                    self.results.add_item(text=' ', value=0)
-
-            self.results.toggle_selected_index(0)
-
-
-    def eventhandler(self, event, menuw=None):
-        if not self.result or not self.server_available:
-            self.destroy()
-            return
-
-        if event in (em.INPUT_UP, em.INPUT_DOWN, em.INPUT_LEFT, em.INPUT_RIGHT):
-            return self.results.eventhandler(event)
-        elif event == em.INPUT_ENTER:
-            ProgramDisplay(prog=self.results.get_selected_item().value,
-                           context='recording').show()
-            return
-        elif event == em.INPUT_EXIT:
-            self.destroy()
+            matches.sort(f)
+            for prog in matches:
+               items.append(ProgramItem(self, prog, context='search'))
         else:
-            return self.parent.eventhandler(event)
+	    if matches == 'no matches':
+                AlertBox(text=_('No matches found for %s') % self.prog.title).show()
+                return
+            AlertBox(text=_('findMatches failed: %s') % matches).show()
+            return
+
+        search_menu = menu.Menu(_( 'Search Results' ), items,
+                                item_types = 'tv program menu')
+        rc.app(None)
+        menuw.pushmenu(search_menu)
+        menuw.refresh()
+
+
+    def schedule_program(self, arg=None, menuw=None):
+        (result, msg) = record_client.scheduleRecording(self.prog)
+        if result:
+            AlertBox(text=_('"%s" has been scheduled for recording') % \
+                     self.prog.title).show()
+            if menuw:  
+                menuw.back_one_menu(arg='reload')
+        else:
+            AlertBox(text=_('Scheduling Failed: %s') % msg).show()
+        # then menu back one or refresh the menu with remove option
+	# instead of schedule
+
+
+    def remove_program(self, arg=None, menuw=None):
+        (result, msg) = record_client.removeScheduledRecording(self.prog)
+        if result:
+            # then menu back one which should show an updated list if we
+            # were viewing scheduled recordings or back to the guide and
+            # update the colour of the program we selected.
+	    # or refresh the menu with remove option instead of schedule
+            if menuw:  
+                menuw.back_one_menu(arg='reload')
+
+        else:
+            AlertBox(text=_('Remove Failed: %s') % msg).show()
+
+
 
 
