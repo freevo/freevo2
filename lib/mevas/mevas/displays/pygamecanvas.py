c42f347843cee71b2c872ec7298a046d70136bce
Dirk Meyer <dischi@freevo.org>
2004-08-22 20:01:09 +0000

    use new to_sdl function in pyimlib2
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@6118 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- lib/mevas/mevas/displays/pygamecanvas.py
+++ lib/mevas/mevas/displays/pygamecanvas.py
@@ -1,5 +1,6 @@
-import time
 import mevas
+import mevas.rect as rect
+
 import pygame
 
 from bitmapcanvas import *
@@ -9,29 +10,37 @@ class PygameCanvas(BitmapCanvas):
 
 	def __init__(self, size):
 		super(PygameCanvas, self).__init__(size, preserve_alpha = False)
-		pygame.init()
-		self._screen = pygame.display.set_mode(size)
+
+		# Initialize the PyGame modules.
+		if not pygame.display.get_init():
+			pygame.display.init()
+			pygame.font.init()
+
+		self._screen  = pygame.display.set_mode(size, 0, 32)
+		self._surface = pygame.Surface(size, 0, 32)
+		self._rect    = []
+
+
+	def _update_end(self, object = None):
+		if not self._rect:
+			return
+		self._rect = rect.optimize_for_rendering(self._rect)
+		self._backing_store._image.to_sdl_surface(self._surface)
+		for pos, size in self._rect:
+			self._screen.blit(self._surface, pos, pos + size)
+		pygame.display.update(self._rect)
+		self._rect = []
 
 	def _blit(self, img, r):
-		pos, size = r
-		t0=time.time()
-
 		if isinstance(img, mevas.imagelib.get_backend("pygame").Image):
 			# FIXME: add code for native pygame images here.
 			pass
+		if isinstance(img, mevas.imagelib.get_backend("imlib2").Image):
+			pass
 		else:
-			t1=time.time()
-			if img.size != size:
-				img = imagelib.crop(img, pos, size)
-
-			data = img.get_raw_data("RGB")
-			# pygame.image.fromstring refuses to read from a buffer :(
-			img = pygame.image.fromstring( str(data), size, "RGB")
-			print "Conversion took", time.time()-t1
-			self._screen.blit(img, pos)
-
-		pygame.display.update(r)
-		print "Blit", r, "took", time.time()-t0
+			# FIXME: add code for not imlib2 images here
+			pass
+		self._rect.append(r)
 
 
 # vim: ts=4
