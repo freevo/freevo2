34051692fcb2e6b8bb894607eae0a99a1d7d4ee0
Dirk Meyer <dischi@freevo.org>
2004-09-10 19:01:27 +0000

    use screen directly if the depth matches
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@6277 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- lib/mevas/mevas/displays/pygamecanvas.py
+++ lib/mevas/mevas/displays/pygamecanvas.py
@@ -2,6 +2,7 @@ import mevas
 import mevas.rect as rect
 
 import pygame
+import time
 
 from bitmapcanvas import *
 from pygame.locals import *
@@ -17,30 +18,37 @@ class PygameCanvas(BitmapCanvas):
 			pygame.font.init()
 
 		self._screen  = pygame.display.set_mode(size, 0, 32)
-		self._surface = pygame.Surface(size, 0, 32)
-		self._rect    = []
+		if self._screen.get_bitsize() != 32:
+			# if the bitsize is not 32 as requested, we need
+			# a tmp surface to convert from imlib2 to pygame
+			# because imlib2 uses 32 bit and with a different
+			# value memcpy will do nasty things
+			self._surface = pygame.Surface(size, 0, 32)
+		self._rect = []
 
 
 	def _update_end(self, object = None):
 		if not self._rect:
 			return
-		self._rect = rect.optimize_for_rendering(self._rect)
-		self._backing_store._image.to_sdl_surface(self._surface)
-		for pos, size in self._rect:
-			self._screen.blit(self._surface, pos, pos + size)
+		if hasattr(self, '_surface'):
+			self._backing_store._image.to_sdl_surface(self._surface)
+			for pos, size in self._rect:
+				self._screen.blit(self._surface, pos, pos + size)
+		else:
+			self._backing_store._image.to_sdl_surface(self._screen)
 		pygame.display.update(self._rect)
 		self._rect = []
 
+
 	def _blit(self, img, r):
-		if isinstance(img, mevas.imagelib.get_backend("pygame").Image):
-			# FIXME: add code for native pygame images here.
-			pass
 		if isinstance(img, mevas.imagelib.get_backend("imlib2").Image):
+			pass
+		elif isinstance(img, mevas.imagelib.get_backend("pygame").Image):
+			# FIXME: add code for native pygame images here.
 			pass
 		else:
 			# FIXME: add code for not imlib2 images here
 			pass
 		self._rect.append(r)
-
 
 # vim: ts=4
