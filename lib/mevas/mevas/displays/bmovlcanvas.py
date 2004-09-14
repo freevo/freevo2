c9b6921c899988fa4e14dd593b1970769c1d808a
Dirk Meyer <dischi@freevo.org>
2004-09-14 20:03:53 +0000

    hide/show improvements
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@6302 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- lib/mevas/mevas/displays/bmovlcanvas.py
+++ lib/mevas/mevas/displays/bmovlcanvas.py
@@ -7,10 +7,21 @@ class BmovlCanvas(BitmapCanvas):
 	def __init__(self, size):
 		super(BmovlCanvas, self).__init__(size, preserve_alpha = True)
 		self._update_rect = None
-		self.fifo = os.open('/tmp/bmovl', os.O_WRONLY)
-		self.bmovl_visible = False
+		self.fifo = os.open('/tmp/bmovl-%s' % os.getpid(), os.O_WRONLY)
+		self.bmovl_visible = True
+		try:
+			os.write(self.fifo, 'SHOW\n')
+		except (IOError, OSError):
+			print 'IOError on bmovl.fifo'
 
 
+	def has_visible_child(self):
+		for c in self.children:
+			if c.visible and c.get_alpha():
+				return True
+		return False
+
+	
 	def _update_end(self, object = None):
 		if not self._update_rect:
 			return
@@ -18,8 +29,8 @@ class BmovlCanvas(BitmapCanvas):
 			print 'IOError: no bmovl fifo'
 			return
 
-		if not self.children and self.bmovl_visible:
-			_debug_('hide')
+		if self.bmovl_visible and not self.has_visible_child():
+			print 'bmovl hide'
 			try:
 				os.write(self.fifo, 'HIDE\n')
 				os.write(self.fifo, 'CLEAR %s %s 0 0\n' % \
@@ -27,10 +38,12 @@ class BmovlCanvas(BitmapCanvas):
 			except (IOError, OSError):
 				print 'IOError on bmovl.fifo'
 			self.bmovl_visible = False
-
+			return
+		
 		pos, size = self._update_rect
 		img = imagelib.crop(self._backing_store, pos, size)
 		try:
+			print 'bmovl update', pos, size
 			os.write(self.fifo, 'RGBA32 %d %d %d %d %d %d\n' % \
 				 (size[0], size[1], pos[0], pos[1], 0, 0))
 
@@ -39,8 +52,8 @@ class BmovlCanvas(BitmapCanvas):
 			print 'IOError on bmovl.fifo'
 		self._update_rect = None
 
-		if self.children and not self.bmovl_visible:
-			_debug_('show')
+		if not self.bmovl_visible and self.has_visible_child():
+			print 'bmovl show'
 			try:
 				os.write(self.fifo, 'SHOW\n')
 			except (IOError, OSError):
