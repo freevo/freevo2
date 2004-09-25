07cc6f588eeae874c851698fdc05eb1e2e9d6b04
Rob Shortt <rob@tvcentric.com>
2004-09-25 04:47:05 +0000

    Move input classes into input plugins.
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@6341 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- src/rc.py
+++ src/rc.py
@@ -9,6 +9,9 @@
 #
 # -----------------------------------------------------------------------
 # $Log$
+# Revision 1.41  2004/09/25 04:47:05  rshortt
+# Move input classes into input plugins.
+#
 # Revision 1.40  2004/08/22 20:15:56  dischi
 # remove keyboard auto append
 #
@@ -49,14 +52,10 @@ import time
 import thread
 import types
 
-import config
-
-from event import Event, BUTTON
 import eventhandler
 
 SHUTDOWN = -1
 
-PYLIRC     = False
 _singleton = None
 
 
@@ -98,171 +97,6 @@ def shutdown():
 
 # --------------------------------------------------------------------------------
 
-# --------------------------------------------------------------------------------
-# internal classes of this module
-# --------------------------------------------------------------------------------
-
-class Lirc:
-    """
-    Class to handle lirc events
-    """
-    def __init__(self):
-        try:
-            import pylirc
-        except ImportError:
-            print 'WARNING: PyLirc not found, lirc remote control disabled!'
-            raise Exception
-        try:
-            if os.path.isfile(config.LIRCRC):
-                pylirc.init('freevo', config.LIRCRC)
-                pylirc.blocking(0)
-            else:
-                raise IOError
-        except RuntimeError:
-            print 'WARNING: Could not initialize PyLirc!'
-            raise Exception
-        except IOError:
-            print 'WARNING: %s not found!' % config.LIRCRC
-            raise Exception
-
-        self.nextcode = pylirc.nextcode
-
-        self.previous_returned_code   = None
-        self.previous_code            = None;
-        self.repeat_count             = 0
-        self.firstkeystroke           = 0.0
-        self.lastkeystroke            = 0.0
-        self.lastkeycode              = ''
-        self.default_keystroke_delay1 = 0.25  # Config
-        self.default_keystroke_delay2 = 0.25  # Config
-
-        global PYLIRC
-        PYLIRC = True
-
-        
-    def get_last_code(self):
-        """
-        read the lirc interface
-        """
-        result = None
-
-        if self.previous_code != None:
-            # Let's empty the buffer and return the most recent code
-            while 1:
-                list = self.nextcode();
-                if list != []:
-                    break
-        else:
-            list = self.nextcode()
-
-        if list == []:
-            # It's a repeat, the flag is 0
-            list   = self.previous_returned_code
-            result = list
-
-        elif list != None:
-            # It's a new code (i.e. IR key was released), the flag is 1
-            self.previous_returned_code = list
-            result = list
-
-        self.previous_code = result
-        return result
-
-
-
-    def poll(self):
-        """
-        return next event
-        """
-        list = self.get_last_code()
-
-        if list == None:
-            nowtime = 0.0
-            nowtime = time.time()
-            if (self.lastkeystroke + self.default_keystroke_delay2 < nowtime) and \
-                   (self.firstkeystroke != 0.0):
-                self.firstkeystroke = 0.0
-                self.lastkeystroke = 0.0
-                self.repeat_count = 0
-
-        if list != None:
-            nowtime = time.time()
-
-            if list:
-                for code in list:
-                    if ( self.lastkeycode != code ):
-                        self.lastkeycode = code
-                        self.lastkeystroke = nowtime
-                        self.firstkeystoke = nowtime
-
-            if self.firstkeystroke == 0.0 :
-                self.firstkeystroke = time.time()
-            else:
-                if (self.firstkeystroke + self.default_keystroke_delay1 > nowtime):
-                    list = []
-                else:
-                    if (self.lastkeystroke + self.default_keystroke_delay2 < nowtime):
-                        self.firstkeystroke = nowtime
-
-            self.lastkeystroke = nowtime
-            self.repeat_count += 1
-
-            for code in list:
-                eventhandler.post_key(code)
-
-        
-# --------------------------------------------------------------------------------
-
-class Keyboard:
-    """
-    Class to handle keyboard input
-    """
-    def __init__(self, callback):
-        """
-        init the keyboard event handler
-        """
-        self.callback = callback
-
-
-    def poll(self):
-        """
-        return next event
-        """
-        e = self.callback()
-        if e:
-            eventhandler.post_key(e)
-            
-
-# --------------------------------------------------------------------------------
-
-class Network:
-    """
-    Class to handle network control
-    """
-    def __init__(self):
-        """
-        init the network event handler
-        """
-        import socket
-        self.port = config.REMOTE_CONTROL_PORT
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.sock.setblocking(0)
-        self.sock.bind(('', self.port))
-        
-
-    def poll(self):
-        """
-        return next event
-        """
-        try:
-            return self.sock.recv(100)
-        except:
-            # No data available
-            return None
-
-
-# --------------------------------------------------------------------------------
     
 class MainLoop:
     """
@@ -272,16 +106,6 @@ class MainLoop:
     def __init__(self, use_pylirc=1, use_netremote=1):
 
         self.inputs = []
-        if use_pylirc:
-            try:
-                self.inputs.append(Lirc())
-            except:
-                pass
-
-        if use_netremote and config.ENABLE_NETWORK_REMOTE and \
-               config.REMOTE_CONTROL_PORT:
-            self.inputs.append(Network())
-
         self.callbacks          = []
         self.shutdown_callbacks = []
 
