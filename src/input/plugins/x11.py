59ce27e02052a74e9af5aea98b0e645a3942b81b
Dirk Meyer <dischi@freevo.org>
2004-10-06 18:45:44 +0000

    add x11 keyboard handler for imlib2 display
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@6398 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

---
+++ src/input/plugins/x11.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+#
+# x11.py
+# 
+# Author:   <crunchy@tzi.de>
+# 
+# x11
+# 
+# $Id$
+# 
+# Copyright (C) 2004 Andreas Büsching <crunchy@tzi.de>
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+import notifier
+
+import gui
+import plugin
+import config
+
+from input import linux_input
+
+class PluginInterface(plugin.InputPlugin):
+    """
+    Plugin for pygame input events
+    """
+    def __init__(self):
+        plugin.InputPlugin.__init__(self)
+
+        self.keymap = {}
+        for key in config.KEYMAP:
+            if hasattr(linux_input, 'KEY_%s' % key):
+                code = getattr(linux_input, 'KEY_%s' % key)
+                self.keymap[code] = config.KEYMAP[key]
+            elif key == 'ESCAPE':
+                self.keymap[ linux_input.KEY_ESC ] = config.KEYMAP[ key ]
+            elif key == 'RETURN':
+                self.keymap[ linux_input.KEY_ENTER ] = config.KEYMAP[ key ]
+            elif key == 'PERIOD':
+                self.keymap[ linux_input.KEY_DOT ] = config.KEYMAP[ key ]
+            elif key == 'KP_MINUS':
+                self.keymap[ linux_input.KEY_KPMINUS ] = config.KEYMAP[ key ]
+            elif key == 'KP_PLUS':
+                self.keymap[ linux_input.KEY_KPPLUS ] = config.KEYMAP[ key ]
+            else:
+                _debug_('Error: unable to find key code for %s' % key, 0)
+        gui.get_display()._display.callback = self.handle
+        notifier.addSocket( gui.get_display()._display.socket,
+                            gui.get_display()._display.update )
+ 
+    def handle( self, keycode ):
+        """
+        Callback to handle the pygame events.
+        """
+        # DO NOT ASK!!!!
+        if keycode > 96: keycode += 5
+        else: keycode -= 8
+        if keycode > 105: keycode -= 1
+        
+        if keycode in self.keymap:
+            self.post_key( self.keymap[ keycode ] )
