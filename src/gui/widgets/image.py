4bffed0195f94d83787f35529c986f18c22563ef
Dirk Meyer <dischi@freevo.org>
2004-10-09 16:21:51 +0000

    add shadow support for images
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@6434 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- src/gui/widgets/image.py
+++ src/gui/widgets/image.py
@@ -1,31 +1,20 @@
 # -*- coding: iso-8859-1 -*-
-# -----------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # image.py - basic image widget
-# -----------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # $Id$
 #
-# -----------------------------------------------------------------------
-# $Log$
-# Revision 1.4  2004/09/07 18:48:57  dischi
-# internal colors are now lists, not int
+# This file defines an image widget to use on a container or display. Besides
+# the normal CanvasImage this class can also auto-resize the image and draw
+# a shadow behind the image.
 #
-# Revision 1.3  2004/08/22 20:06:21  dischi
-# Switch to mevas as backend for all drawing operations. The mevas
-# package can be found in lib/mevas. This is the first version using
-# mevas, there are some problems left, some popup boxes and the tv
-# listing isn't working yet.
-#
-# Revision 1.2  2004/07/27 18:52:31  dischi
-# support more layer (see README.txt in backends for details
-#
-# Revision 1.1  2004/07/25 18:14:05  dischi
-# make some widgets and boxes work with the new gui interface
-#
-# -----------------------------------------------------------------------
-#
+# -----------------------------------------------------------------------------
 # Freevo - A Home Theater PC framework
+# Copyright (C) 2002-2004 Krister Lagerstrom, Dirk Meyer, et al.
 #
-# Copyright (C) 2002 Krister Lagerstrom, et al.
+# Maintainer:    Dirk Meyer <dmeyer@tzi.de>
+#
+# Please see the file freevo/Docs/CREDITS for a complete list of authors.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -41,21 +30,49 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 #
-# ----------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 
 from mevas.image import CanvasImage
 
 class Image(CanvasImage):
     """
-    An image object that can be drawn onto a layer
+    An image that can be added to a container or display. It can be created
+    with an imagelib image or a filename and can optional position or resize
+    the image or create a shadow behind it.
     """
-    def __init__(self, image, (x, y), size=None):
+    def __init__(self, image, pos=None, size=None, shadow=None):
+        """
+        Create the image.
+        Parameter description:
+        image    an imagelib image or string
+        pos      postion of the image on the parent
+        size     if set, the image will be scalled the given size. If width or
+                 height is None or -1, the image will be scalled to keep the
+                 aspect ratio
+        shadow   a tripple x,y,color defining the attributes of the shadow
+                 that will be added behind the image
+        """
         if not image:
+            # invalid image, return 1x1 empty space
             CanvasImage.__init__(self, (1,1))
             return
-        
-        CanvasImage.__init__(self, image)
+
+        if shadow:
+            # shadow used, drop a shadow at x,y with the given color
+            sx, sy, scolor = shadow
+            CanvasImage.__init__(self, (image.width + sx,
+                                        image.height + sy))
+            # draw shadow
+            self.draw_rectangle((sx, sy), (image.width, image.height),
+                                scolor, 1)
+            # draw image
+            self.draw_image(image)
+        else:
+            # CanvasImage is only the image itself
+            CanvasImage.__init__(self, image)
+
         if size:
+            # resize the image
             width, height = size
             # check width and height for scaling
             if width == None or width == -1:
@@ -67,4 +84,6 @@ class Image(CanvasImage):
             if width != self.image.width or height != self.image.height:
                 self.image.scale((width, height))
 
-        self.set_pos((x, y))
+        if pos:
+            # set position on parent
+            self.set_pos(pos)
