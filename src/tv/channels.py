755580f54932cdf066d562d1b46ed7b62322424d
Rob Shortt <rob@tvcentric.com>
2004-10-18 01:15:20 +0000

    Changes to use new pyepg: -Add ProgramItem and ChannelItem classes which take code from the old tv  and channel classes. -change freq vars to uri, we already have a freq (KHz, tv/freq.py) -preserve tuner_id, which is mostly useful for people in North America as  most prefer to see it in the display.
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@6458 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- src/tv/channels.py
+++ src/tv/channels.py
@@ -9,6 +9,14 @@
 #
 # -----------------------------------------------------------------------
 # $Log$
+# Revision 1.27  2004/10/18 01:15:20  rshortt
+# Changes to use new pyepg:
+# -Add ProgramItem and ChannelItem classes which take code from the old tv
+#  and channel classes.
+# -change freq vars to uri, we already have a freq (KHz, tv/freq.py)
+# -preserve tuner_id, which is mostly useful for people in North America as
+#  most prefer to see it in the display.
+#
 # Revision 1.26  2004/10/09 09:11:56  dischi
 # wrap epg scanning in a thread
 #
@@ -77,78 +85,101 @@
 # ----------------------------------------------------------------------- */
 
 import os
-import time
-import stat
 import string
+import time
 import traceback
 
 import config
-import tv.freq, tv.v4l2
-import util
+import tv.freq
 import plugin
-import util.fthread as fthread
+# import util.fthread as fthread
+from item import Item
 
 # The Electronic Program Guide
 import pyepg
 
+EPGDB = os.path.join(config.FREEVO_CACHEDIR, 'epgdb')
 
 DEBUG = config.DEBUG
 
-_channels_cache = None
+NO_DATA = _('no data')
+_channels = None
+_epg = None
+
+
+def get_epg():
+    """
+    Return an existing instance of the EPG database.
+    """
+
+    global _epg
+
+    if not _epg:
+        _epg = pyepg.get_epg(EPGDB)
+        # _epg = fthread.call(pyepg.get_epg, EPGDB)
+
+    return _epg
+
+
+def get_new_epg():
+    """
+    Return a fresh instance of the EPG database.
+    """
+
+    global _epg
+    _epg = pyepg.get_epg(EPGDB)
+
+    return _epg
+
 
 def get_channels():
-    global _channels_cache
-    reload = False
-    pickle = os.path.join(config.FREEVO_CACHEDIR, 'epg')
+    """
+    Return an already created ChannelList, this may save a bit of time.
+    """
+    
+    global _channels
 
-    if not _channels_cache:
-        _debug_('no epg in memory, caching')
-        reload = True
+    if not _channels:
+        _debug_('no channels in memory, loading')
+        _channels = ChannelList()
 
-    elif not os.path.isfile(pickle):
-        _debug_('no epg "%s", will try to rebuild' % pickle)
-        reload = True
-
-    elif os.stat(pickle)[stat.ST_MTIME] > _channels_cache.load_time:
-        _debug_('epg newer than memory cache, reloading')
-        reload = True
-
-    if reload:
-        if DEBUG:
-            stime = time.time()
-        _channels_cache = ChannelList()
-        if DEBUG:
-            _debug_('epg load took %s seconds' % (time.time()-stime))
-
-    return _channels_cache
+    return _channels
 
 
 def get_settings_by_id(chan_id):
+    """
+    """
+    
     settings = []
 
     for c in get_channels().get_all():
         if c.id == chan_id:
-            for f in c.freq:
-                which = string.split(f, ':')[0]
+            for u in c.uri:
+                which = string.split(u, ':')[0]
                 settings.append(which)
             return settings
 
 
 def get_settings_by_name(chan_name):
+    """
+    """
+    
     settings = []
 
     for c in get_channels().get_all():
-        if c.name == chan_id:
-            for f in c.freq:
-                which = string.split(f, ':')[0]
+        if c.name == chan_name:
+            for u in c.uri:
+                which = string.split(u, ':')[0]
                 settings.append(which)
             return settings
 
 
 def get_lockfile(which):
-    #  In this function we must figure out which device these settings use
-    #  because we are allowed to have multiple settings for any particular
-    #  device, usually for different inputs (tuner, composite, svideo).
+    """
+    In this function we must figure out which device these settings use
+    because we are allowed to have multiple settings for any particular
+    device, usually for different inputs (tuner, composite, svideo).
+    """
 
     dev_lock = False
     settings = config.TV_SETTINGS.get(which)
@@ -178,6 +209,9 @@ def get_lockfile(which):
 
 
 def lock_device(which):
+    """
+    """
+    
     lockfile = get_lockfile(which)
 
     if os.path.exists(lockfile):
@@ -194,6 +228,9 @@ def lock_device(which):
 
 
 def unlock_device(which):
+    """
+    """
+    
     lockfile = get_lockfile(which)
 
     if os.path.exists(lockfile):
@@ -210,6 +247,9 @@ def unlock_device(which):
 
 
 def is_locked(which):
+    """
+    """
+    
     lockfile = get_lockfile(which)
 
     if os.path.exists(lockfile):
@@ -220,59 +260,172 @@ def is_locked(which):
 
 
 def get_actual_channel(channel_id, which):
+    """
+    """
     
     for chan in get_channels().get_all():
         if chan.id == channel_id:
-            for f in chan.freq:
-                if string.split(f, ':')[0] == which:
-                    return string.lstrip(f, '%s:' % which)
+            for u in chan.uri:
+                if string.split(u, ':')[0] == which:
+                    return string.lstrip(u, '%s:' % which)
 
 
-class Channel:
+class ProgramItem(Item):
     """
-    information about one specific channel, also containing
-    epg informations
+    Information about one TV program.
     """
-    def __init__(self, name, freq, epg):
-        self.name = name
-        self.freq = []
-        self.epg  = epg
-        if isinstance(freq, list) or isinstance(freq, tuple):
-            for f in freq:
-                self.__add_freq__(f)
+
+    def __init__(self, title, start, stop, subtitle='', description='', 
+                 id=None, channel_id=None, parent=None):
+        if parent:
+            Item.__init__(self, parent)
         else:
-            self.__add_freq__(freq)
+            Item.__init__(self)
 
-        # variables from the epg
-        self.id   = self.epg.id
+        self.title = self.info['title'] = title
+        # self.info['title'] = title
+        self.start = self.info['start'] = start
+        self.stop = self.info['stop'] = stop
+        self.channel_id = channel_id
+        self.info['subtitle'] = subtitle
+        self.info['description'] = description
+        self.info['id'] = id
+        self.name = '%d\t%s' % (self.info['start'], self.title)
 
-        # functions from the epg
-        self.next = self.epg.next
-        self.prev = self.epg.prev
-        self.get  = self.epg.get
+        self.valid = 1
+# XXX: getting acsii error with if title == NO_DATA:
+#        if title == NO_DATA:
+#            self.valid = 0
+#        else:
+#            self.valid = 1
+        
+        self.scheduled  = 0
 
-
-    def __add_freq__(self, freq):
+    def __unicode__(self):
         """
-        add a frequence to the internal list where to find that channel
+        return as unicode for debug
         """
-        if freq.find(':') == -1:
-            freq = '%s:%s' % (config.TV_DEFAULT_SETTINGS, freq)
-        self.freq.append(freq)
+        bt = time.localtime(self.start)   # Beginning time tuple
+        et = time.localtime(self.stop)    # End time tuple
+        begins = '%s-%02d-%02d %02d:%02d' % (bt[0], bt[1], bt[2], bt[3], bt[4])
+        ends   = '%s-%02d-%02d %02d:%02d' % (et[0], et[1], et[2], et[3], et[4])
+        return u'%s to %s  %3s ' % (begins, ends, self.channel_id) + \
+                   self.title + u' (%s)' % self.pos
+
+
+    def __str__(self):
+        """
+        return as string for debug
+        """
+        return String(self.__unicode__())
+
+    
+    def __cmp__(self, other):
+        """
+        compare function, return 0 if the objects are identical, 1 otherwise
+        """
+        if not other:
+            return 1
+        return self.title != other.title or \
+               self.start != other.start or \
+               self.stop  != other.stop or \
+               self.channel_id != other.channel_id
+
+
+    def getattr(self, attr):
+        """
+        return the specific attribute as string or an empty string
+        """
+        if attr == 'start':
+            return Unicode(time.strftime(config.TV_TIMEFORMAT, time.localtime(self.start)))
+        if attr == 'stop':
+            return Unicode(time.strftime(config.TV_TIMEFORMAT, time.localtime(self.stop)))
+        if attr == 'date':
+            return Unicode(time.strftime(config.TV_DATEFORMAT, time.localtime(self.start)))
+        if attr == 'time':
+            return self.getattr('start') + u' - ' + self.getattr('stop')
+        if hasattr(self, attr):
+            return getattr(self,attr)
+        return ''
+
+
+
+class ChannelItem(Item):
+    """
+    Information about one specific channel, also containing
+    epg informations.
+    """
+    def __init__(self, id, call_sign, uri, add_programs=True):
+        Item.__init__(self)
+
+        self.info['id'] = id
+        self.info['call_sign'] = call_sign
+        self.info['tuner_id'] = ''
+        self.uri = []
+        self.programs = []
+        self.logo = ''
+        self.index       = {}
+        self.index_start = 0
+        self.index_end   = 0
+
+        if isinstance(uri, list) or isinstance(uri, tuple):
+            for u in uri:
+                self.__add_uri__(u)
+        else:
+            self.__add_uri__(uri)
+
+        # XXX: change this to config.TV_CHANNEL_DISPLAY_FORMAT or something as
+        #      many people won't want to see the tuner_id.
+        # XXX: What should we use, name or title, or both?
+        self.name = self.title = self.info['display_name'] = '%s %s' % \
+                     (self.info['tuner_id'], self.info['call_sign'])
+
+        # print 'C: %s' % self.info['id']
+        # print 'C: %s' % self.id()
+        if add_programs:
+            # Create a list of ProgramItems containing EPG data.
+            for p in get_epg().get_programs(self.info['id']):
+                # print 'P: %s' % p
+                self.programs.append(ProgramItem(p.title, p.start, p.stop,
+                                                 subtitle=p.subtitle,
+                                                 description=p.description,
+                                                 id=p.id,
+                                                 channel_id=self.info['id']))
+
+                # TODO: add information about program being recorded which
+                #       comes from another DB table - same with categories,
+                #       ratings and advisories.
+
+        self.create_index()
+
+
+    def __add_uri__(self, uri):
+        """
+        Add a URI to the internal list where to find that channel.
+        Also save the tuner_id because many people, mostly North Americans,
+        like to use it (usually in the display).
+        """
+        if uri.find(':') == -1:
+            self.info['tuner_id'] = uri
+            uri = '%s:%s' % (config.TV_DEFAULT_SETTINGS, uri)
+        else:
+            self.info['tuner_id'] = uri.split(':')[1]
+
+        self.uri.append(uri)
 
 
     def player(self):
         """
         return player object for playing this channel
         """
-        for f in self.freq:
-            device, freq = f.split(':')
-            print device, freq
-            # try all internal frequencies
+        for u in self.uri:
+            device, uri = u.split(':')
+            print device, uri
+            # try all internal URIs
             for p in plugin.getbyname(plugin.TV, True):
                 # FIXME: better handling for rate == 1 or 2
-                if p.rate(self, device, freq):
-                    return p, device, freq
+                if p.rate(self, device, uri):
+                    return p, device, uri
         return None
 
 
@@ -282,42 +435,189 @@ class Channel:
         """
         settings = {}
 
-        for f in self.freq:
-            type = string.split(f, ':')[0]
+        for u in self.uri:
+            type = string.split(u, ':')[0]
             settings[type] = config.TV_SETTINGS.get(type)
 
         return settings
+
+
+    def create_index(self):
+        """
+        create index for faster access
+        """
+        last     = 0
+        index    = 0
+        last_key = 0
+
+        self.programs[0].index = 0
+        for p in self.programs[1:]:
+            index += 1
+            p.index = index
+            key = int(p.start) / (60 * 60 * 24)
+            if not self.index_start:
+                self.index_start = key
+            self.index_end = key
+            if not self.index.has_key(key):
+                if last_key:
+                    while len(self.index[last_key]) < 48:
+                        self.index[last_key].append(last)
+                self.index[key] = []
+            pos = (int(p.start) / (60 * 30)) % (48)
+            p.pos = pos
+            
+            if len(self.index[key]) >= pos + 1:
+                last = index
+                continue
+            while len(self.index[key]) < pos:
+                self.index[key].append(last)
+            self.index[key].append(index)
+            last = index
+            last_key = key
+        while len(self.index[key]) < 48:
+            self.index[key].append(index)
+
+
+    def __get_pos__(self, start, stop):
+        """
+        get internal positions for programs between start and stop
+        """
+        start -= 60 * 30
+        key = int(start) / (60 * 60 * 24)
+        if key < self.index_start:
+            key = self.index_start
+            pos = 0
+        else:
+            pos = (int(start) / (60 * 30)) % (48)
+
+        start = max(self.index[key][pos], 0)
+        
+        key = int(stop) / (60 * 60 * 24)
+        if key > self.index_end:
+            key = self.index_end
+            pos = 47
+        else:
+            pos = (int(stop) / (60 * 30)) % (48) + 1
+
+        if pos >= 48:
+            # next day
+            key += 1
+            if key > self.index_end:
+                key = self.index_end
+                pos = 47
+            else:
+                pos = 0
+
+        stop = self.index[key][pos] + 1
+        return start, stop
+
+
+    def get(self, start, stop=0):
+        """
+        get programs between start and stop time or if stop=0, get
+        the program running at 'start'
+        """
+        print 'RLS: get(%d, %d)' % (start, stop)
+        if not stop:
+            stop = start
+        start_p, stop_p = self.__get_pos__(start, stop)
+        f = lambda p, a=start, b=stop: not (p.start > b or p.stop < a)
+        try:
+            progs = filter(f, self.programs[start_p:stop_p])
+            if not len(progs):  return [ None, ]
+            return progs
+        except Exception, e:
+            print 'RLS: return filter failed'
+            traceback.print_exc()
+            return [ None, ]
+                
+
+    def next(self, prog):
+        """
+        return next program after 'prog'
+        """
+        pos  = min(len(self.programs)-1, prog.index + 1)
+        prog = self.programs[pos]
+        if pos < len(self.programs) and not prog.valid:
+            return self.next(prog)
+        return prog
+
+    
+    def prev(self, prog):
+        """
+        return previous program before 'prog'
+        """
+        pos = max(0, prog.index - 1)
+        prog = self.programs[pos]
+        if pos > 0 and not prog.valid:
+            return self.prev(prog)
+        return prog
                     
 
 
 class ChannelList:
+    """
+    This is the channel list internal to Freevo.  Here we get channel
+    information from pyepg (using EPGDB) and append it with anything
+    specific to Freevo like which device to use, or override other
+    things such as the display name.
+    """
 
     def __init__(self):
         self.selected = 0
-        self.channels = []
+        self.channel_list = []
+        self.channel_dict = {}
 
-        source = config.XMLTV_FILE
-        pickle = os.path.join(config.FREEVO_CACHEDIR, 'epg')
+        if not os.path.isfile(EPGDB):
+            print 'No EPGDB found! (%s)' % EPGDB
+            return
 
-        epg = fthread.call(pyepg.load, source, pickle)
-        self.load_time = time.time()
+        try:
+            self.epg = get_epg()
+        except:
+            print 'Failed to load EPGDB! (%s)' % EPGDB
+            traceback.print_exc()
+            return
 
-        for c in config.TV_CHANNELS:
-            self.channels.append(Channel(c[1], c[2], epg.get(c[0])))
+        for c in self.epg.get_channels():
+            override = False
+            for cc in config.TV_CHANNELS:
+                if c.id == cc[0]:
+                    # Override with config data.
+                    self.add_channel(ChannelItem(id=cc[0], call_sign=cc[1], 
+                                                 uri=cc[2]))
+                    override = True
+                    break
+                
+            if not override:
+                self.add_channel(ChannelItem(id=c.id, call_sign=c.call_sign, 
+                                             uri=c.tuner_id))
         
+
+    def add_channel(self, channel):
+        """
+        add a channel to the list
+        """
+        if not self.channel_dict.has_key(channel.id):
+            # Add the channel to both the dictionary and the list. This works
+            # well in Python since they will both point to the same object!
+            self.channel_dict[channel.id] = channel
+            self.channel_list.append(channel)
+
 
     def down(self):
         """
         go one channel up
         """
-        self.selected = (self.selected + 1) % len(self.channels)
+        self.selected = (self.selected + 1) % len(self.channel_list)
 
 
     def up(self):
         """
         go one channel down
         """
-        self.selected = (self.selected + len(self.channels) - 1) % len(self.channels)
+        self.selected = (self.selected + len(self.channel_list) - 1) % \
+                         len(self.channel_list)
 
 
     def set(self, pos):
@@ -327,16 +627,21 @@ class ChannelList:
         self.selected = pos
         
 
-    def get(self):
+    def get(self, id=None):
         """
-        return selected channel information
+        return channel with id or whichever is selected
         """
-        return self.channels[self.selected]
+
+        if id:
+            chan = self.channel_dict.get(id)
+            if chan: return chan
+
+        return self.channel_list[self.selected]
 
 
     def get_all(self):
         """
         return all channels
         """
-        return self.channels
+        return self.channel_list
     
