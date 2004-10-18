f830fd75232007520e1636b5a6f1f6b973cd2b11
Rob Shortt <rob@tvcentric.com>
2004-10-18 01:06:38 +0000

    The schema for the EPG database.  To load this run: sqlite /path/to/freevo/cache/epgdb < epg_schema.sql and ignore the errors about dropping tables because there may not be any to drop. TODO: have the EPGDB object automaticly install the schema if a DB doesn't exist and have some way to automaticly upgrade the DB between revisions.
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@6457 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

---
+++ lib/pyepg/epg_schema.sql
@@ -0,0 +1,96 @@
+
+drop table admin;
+create table admin (
+    version text primary key
+);
+insert into admin (version) values ("0.1.0");
+
+drop table channels;
+create table channels (
+    id text primary key,
+    call_sign text not null,
+    tuner_id text not null
+);
+
+drop table channel_types;
+create table channel_types (
+    id integer primary key,
+    name text not null
+);
+insert into channel_types (id, name) values (0, "undefined");
+insert into channel_types (id, name) values (1, "tv");
+insert into channel_types (id, name) values (2, "camera");
+insert into channel_types (id, name) values (3, "radio");
+
+drop table programs;
+create table programs (
+    id integer primary key,
+    channel_id text not null,
+    title text not null,
+    subtitle text,
+    description text,
+    episode text,
+    start int not null,
+    stop int not null,
+    rating int,
+    original_airdate int,
+    stars int
+);
+
+drop table categories;
+create table categories (
+    id integer primary key,
+    name text not null
+);
+insert into categories (id, name) values (0, "undefined");
+insert into categories (id, name) values (1, "series");
+insert into categories (id, name) values (2, "news");
+insert into categories (id, name) values (3, "movie");
+insert into categories (id, name) values (4, "special");
+insert into categories (id, name) values (5, "audio");
+insert into categories (id, name) values (6, "feed");
+insert into categories (id, name) values (7, "drama");
+
+drop table program_category;
+create table program_category (
+    program_id integer not null,
+    category_id integer not null
+);
+
+drop table advisories;
+create table advisories (
+    id integer primary key,
+    name text not null
+);
+insert into advisories (id, name) values (0, "undefined");
+
+drop table program_advisory;
+create table program_advisory (
+    program_id integer not null,
+    advisory_id integer not null
+);
+
+drop table ratings;
+create table ratings (
+    id integer primary key,
+    name text not null
+);
+insert into ratings (id, name) values (0, "undefined");
+insert into ratings (id, name) values (1, "NR");
+insert into ratings (id, name) values (2, "G");
+insert into ratings (id, name) values (3, "PG");
+insert into ratings (id, name) values (4, "PG-13");
+insert into ratings (id, name) values (5, "PG-14");
+insert into ratings (id, name) values (6, "A");
+insert into ratings (id, name) values (7, "R");
+insert into ratings (id, name) values (8, "X");
+
+drop table record_programs;
+create table record_programs (
+    program_id integer primary key
+);
+
+drop table recorded_programs;
+create table recorded_programs (
+    program_id integer primary key
+);
