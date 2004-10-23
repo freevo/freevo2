8ba47b76baee4e85461ffcc14cba41176ce6f9b9
Rob Shortt <rob@tvcentric.com>
2004-10-23 14:38:13 +0000

    Combine ProgramItem with what I had in channels.py, remove import of Favorite because it will be combined with FavoriteItem, added comments.
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@6497 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- src/tv/program_display.py
+++ src/tv/program_display.py
@@ -9,6 +9,10 @@
 #
 # -----------------------------------------------------------------------
 # $Log$
+# Revision 1.43  2004/10/23 14:38:13  rshortt
+# Combine ProgramItem with what I had in channels.py, remove import of
+# Favorite because it will be combined with FavoriteItem, added comments.
+#
 # Revision 1.42  2004/08/23 12:40:55  dischi
 # remove osd.py dep
 #
@@ -60,56 +64,128 @@ from time import gmtime, strftime
 import plugin, config, menu
 import gui
 
-import util.tv_util as tv_util
+# import util.tv_util as tv_util
 import tv.record_client as record_client
 import event as em
 
 from item import Item
 from gui import AlertBox
 from gui import InputBox
-from tv.record_types import Favorite
+# from tv.record_types import Favorite
 
 DEBUG = config.DEBUG
 
 
 class ProgramItem(Item):
-    def __init__(self, parent, prog, context='guide'):
-        Item.__init__(self, parent, skin_type='video')
-        self.prog = prog
+    def __init__(self, title, start, stop, subtitle='', description='',
+                 id=None, channel_id=None, context='guide', skin_type='video',
+                 parent=None):
+        if parent:
+            Item.__init__(self, parent, skin_type=skin_type)
+        else:
+            Item.__init__(self, skin_type=skin_type)
+
+        # TODO: deal with context 'guide' or 'scheduled' (find a different way)
         self.context = context
 
-        self.name = self.title = prog.title
-        if hasattr(prog,'sub_title'): self.sub_title = prog.sub_title
-        if hasattr(prog,'desc'): self.desc = prog.desc
-        self.channel = tv_util.get_chan_displayname(prog.channel_id)
-
-        if hasattr(prog, 'scheduled'):
-            self.scheduled = prog.scheduled
-        else:
-            self.scheduled = False
-        self.favorite = False
-
-        self.start = time.strftime(config.TV_DATETIMEFORMAT,
-                                   time.localtime(prog.start))
-        self.stop = time.strftime(config.TV_DATETIMEFORMAT,
-                                       time.localtime(prog.stop))
-
-        self.categories = ''
-        try:
-            for cat in prog.categories:
-                if prog.categories.index(cat) > 0:
-                    self.categories += ', %s' % cat
-                else:
-                    self.categories += '%s' % cat
-        except AttributeError:
-            pass
+        self.title = self.info['title'] = title
+        # self.info['title'] = title
+        self.start = self.info['start'] = start
+        self.stop = self.info['stop'] = stop
         
+        # FIXME: after self.info['channel'] is fixed further below remove it
+        #        from here.
+        self.info['channel'] = self.channel_id = channel_id
+        self.info['subtitle'] = subtitle
+        self.info['description'] = description
+        self.info['id'] = id
+        self.name = '%d\t%s' % (self.info['start'], self.title)
+
+        self.valid = 1
+# we probably don't need the valid attribute anymore
+# XXX: getting acsii error with if title == NO_DATA:
+#        if title == NO_DATA:
+#            self.valid = 0
+#        else:
+#            self.valid = 1
+
+        # FIXME: add a hook here or another file to see if we're
+        #        scheduled for recording
+        self.scheduled  = False
+
+        # FIXME: fix tv_util.get_chan_displayname() or do it another way
+        # self.info['channel'] = tv_util.get_chan_displayname(channel_id)
+
+        self.start_str = time.strftime(config.TV_DATETIMEFORMAT,
+                                       time.localtime(self.start))
+        self.stop_str = time.strftime(config.TV_DATETIMEFORMAT,
+                                      time.localtime(self.stop))
+
+        # TODO: add category support (from epgdb)
+        self.categories = ''
+        #try:
+        #    for cat in prog.categories:
+        #        if prog.categories.index(cat) > 0:
+        #            self.categories += ', %s' % cat
+        #        else:
+        #            self.categories += '%s' % cat
+        #except AttributeError:
+        #    pass
+        
+        # TODO: add ratings support (from epgdb)
         self.ratings = ''
-        try:
-            for rat in prog.ratings.keys():
-                self.ratings += '%s ' % rat
-        except:
-            pass
+        #try:
+        #    for rat in prog.ratings.keys():
+        #        self.ratings += '%s ' % rat
+        #except:
+        #    pass
+
+
+    def __unicode__(self):
+        """
+        return as unicode for debug
+        """
+        bt = time.localtime(self.start)   # Beginning time tuple
+        et = time.localtime(self.stop)    # End time tuple
+        begins = '%s-%02d-%02d %02d:%02d' % (bt[0], bt[1], bt[2], bt[3], bt[4])
+        ends   = '%s-%02d-%02d %02d:%02d' % (et[0], et[1], et[2], et[3], et[4])
+        return u'%s to %s  %3s ' % (begins, ends, self.channel_id) + self.title
+
+
+    def __str__(self):
+        """
+        return as string for debug
+        """
+        return String(self.__unicode__())
+
+
+    def __cmp__(self, other):
+        """
+        compare function, return 0 if the objects are identical, 1 otherwise
+        """
+        if not other:
+            return 1
+        return self.title != other.title or \
+               self.start != other.start or \
+               self.stop  != other.stop or \
+               self.channel_id != other.channel_id
+
+
+    def getattr(self, attr):
+        """
+        return the specific attribute as string or an empty string
+        """
+        if attr == 'start':
+            return Unicode(time.strftime(config.TV_TIMEFORMAT, time.localtime(self.start)))
+        if attr == 'stop':
+            return Unicode(time.strftime(config.TV_TIMEFORMAT, time.localtime(self.stop)))
+        if attr == 'date':
+            return Unicode(time.strftime(config.TV_DATEFORMAT, time.localtime(self.start)))
+        if attr == 'time':
+            return self.getattr('start') + u' - ' + self.getattr('stop')
+        if hasattr(self, attr):
+            return getattr(self,attr)
+        return ''
 
 
     def actions(self):
@@ -158,9 +234,12 @@ class ProgramItem(Item):
 
 
     def add_favorite(self, arg=None, menuw=None):
-        fav = Favorite(self.prog.title, self.prog, True, True, True, -1)
-        fav_item = FavoriteItem(self, fav, fav_action='add')
-        fav_item.display_favorite(menuw=menuw)
+        pass
+
+        # FIXME: combine Favorite and FavoriteItem
+        # fav = Favorite(self.prog.title, self.prog, True, True, True, -1)
+        # fav_item = FavoriteItem(self, fav, fav_action='add')
+        # fav_item.display_favorite(menuw=menuw)
 
 
     def rem_favorite(self, arg=None, menuw=None):
