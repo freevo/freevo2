a205e5c1ae4fb54f3cedbbd594a83804d401b00e
Rob Shortt <rob@tvcentric.com>
2004-11-19 16:55:58 +0000

    New ivtv record plugin.
    This plugin requires the "encoder" utility that comes with the CK series of
    ivtv drivers found at http://67.18.1.101/~ckennedy/ivtv/.  You neeed at least
    version 0.2.0-rc2s from the stable branch or 0.3.1o from the unstable branch
    because we rely on a paramerter for passing the channel frequency that was
    introduced in those versions.
    
    To use:
    
    plugin.activate('record.ivtv') # it defaults to ivtv0
    or:
    plugin.activate('record.ivtv', args='ivtv1')
    
    Right now it only supports one card but I will be sending a patch for encoder
    to support setting the device.
    
    
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@6646 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

---
+++ src/record/plugins/ivtv.py
@@ -0,0 +1,104 @@
+# -*- coding: iso-8859-1 -*-
+# -----------------------------------------------------------------------------
+# ivtv.py - A plugin for recording from an ivtv card supported by the drivers
+#           found at http://ivtv.sf.net.
+#
+# This plugin requires the "encoder" utility that comes with the CK series of
+# ivtv drivers found at http://67.18.1.101/~ckennedy/ivtv/.  You neeed at least
+# version 0.2.0-rc2s from the stable branch or 0.3.1o from the unstable branch
+# because we rely on a paramerter for passing the channel frequency that was
+# introduced in those versions.
+#
+# -----------------------------------------------------------------------------
+# $Id$
+#
+#
+# -----------------------------------------------------------------------------
+# Freevo - A Home Theater PC framework
+# Copyright (C) 2002-2004 Krister Lagerstrom, Dirk Meyer, et al.
+#
+# First Edition: Rob Shortt <rshortt@users.sf.net>
+# Maintainer:    Rob Shortt <rshortt@users.sf.net>
+#
+# Please see the file freevo/Docs/CREDITS for a complete list of authors.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MER-
+# CHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+#
+# -----------------------------------------------------------------------------
+
+import os
+import time
+import config
+import generic
+from tv.freq import get_frequency
+from system.tvcards import IVTVCard
+
+
+class PluginInterface(generic.PluginInterface):
+
+    def __init__(self, device='ivtv0'):
+        self.name = device
+        self.device = config.TV_SETTINGS[device]
+        generic.PluginInterface.__init__(self)
+
+        if not isinstance(self.device, IVTVCard):
+            self.reason = 'Device %s is not of class IVTVCard'
+            return
+
+        self.suffix = '.mpeg'
+
+        rating = 1
+        channels = []
+        for c in self.device.channels:
+            channels.append([c])
+        self.channels = [ device, rating, channels ]
+
+
+    def config(self):
+        return [ ( 'IVTV_ENCODER', '/usr/local/bin/encoder', 
+                   'Location of the encoder utility.' ), ]
+
+        
+    def get_cmd(self, rec):
+        channel = self.device.channels[rec.channel]
+        frequency = get_frequency(channel, self.device.chanlist)
+
+        if rec.url.startswith('file:'):
+            filename = rec.url[5:]
+        else:
+            filename = rec.url
+
+        duration = int(rec.stop - time.time())
+
+        return [ config.IVTV_ENCODER, 
+                 '-bmode',    String(self.device.bitrate_mode),
+                 '-brate',    String(self.device.bitrate),
+                 '-bpeak',    String(self.device.bitrate_peak),
+                 '-audio',    String(self.device.audio_bitmask),
+                 '-dnrmode',  String(self.device.dnr_mode),
+                 '-dnrtype',  String(self.device.dnr_type),
+                 '-dnrspat',  String(self.device.dnr_spatial),
+                 '-dnrtemp',  String(self.device.dnr_temporal),
+                 '-stream',   String(self.device.stream_type),
+                 '-pulldown', String(self.device.pulldown),
+                 '-input',    String(self.device.input),
+                 '-f',        String(frequency), 
+                 String(duration), 
+                 filename ]
+
+    
+    def get_channel_list(self):
+        return [ self.channels ]
+
