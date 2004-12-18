0a9189ac4717ea2dad0ba930940f5d82e75803a8
Dirk Meyer <dischi@freevo.org>
2004-12-18 13:27:54 +0000

    move basic process handling from childapp to util.popen
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@6765 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- src/childapp.py
+++ src/childapp.py
@@ -1,77 +1,23 @@
 # -*- coding: iso-8859-1 -*-
-# -----------------------------------------------------------------------
-# childapp.py - Runs an application in a child process
-# -----------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+# childapp.py - runs an application in a child process
+# -----------------------------------------------------------------------------
 # $Id$
 #
-# Notes:
-# Todo:        
+# Run a child application inside Freevo. The class is based util.popen and
+# send events on start/stop. It is also possible to shut down the gui when
+# the child is starting.
 #
-# -----------------------------------------------------------------------
-# $Log$
-# Revision 1.71  2004/12/12 18:02:39  dischi
-# wait longer on exit
+# TODO: o remove doeslogging, debugname
+#       o better handling of stop_osd and is_video
 #
-# Revision 1.70  2004/12/05 13:01:10  dischi
-# delete old tv variables, rename some and fix detection
-#
-# Revision 1.69  2004/11/20 18:22:58  dischi
-# use python logger module for debug
-#
-# Revision 1.68  2004/11/07 16:39:21  dischi
-# create extra finish function
-#
-# Revision 1.67  2004/11/04 17:40:17  dischi
-# change to new notifier interface
-#
-# Revision 1.66  2004/11/01 20:14:04  dischi
-# fix debug
-#
-# Revision 1.65  2004/10/30 18:47:14  dischi
-# fix missing import
-#
-# Revision 1.64  2004/10/29 18:16:41  dischi
-# moved killall to this file
-#
-# Revision 1.63  2004/10/06 19:24:00  dischi
-# switch from rc.py to pyNotifier
-#
-# Revision 1.62  2004/10/06 18:44:24  dischi
-# rewrite to use pyNotifier and no threads
-#
-# Revision 1.61  2004/09/15 20:47:42  dischi
-# avoid duplicate events
-#
-# Revision 1.60  2004/08/23 20:38:56  dischi
-# fix osd stop/restart
-#
-# Revision 1.59  2004/08/23 12:39:59  dischi
-# remove osd.py dep
-#
-# Revision 1.58  2004/07/26 18:10:16  dischi
-# move global event handling to eventhandler.py
-#
-# Revision 1.57  2004/07/10 12:33:36  dischi
-# header cleanup
-#
-# Revision 1.56  2004/06/06 06:51:55  dischi
-# fix prio handling
-#
-# Revision 1.55  2004/05/31 10:40:57  dischi
-# update to new callback handling in rc
-#
-# Revision 1.54  2004/05/30 18:27:53  dischi
-# More event / main loop cleanup. rc.py has a changed interface now
-#
-# Revision 1.53  2004/05/29 19:06:46  dischi
-# register poll function to rc
-#
-# Revision 1.52  2004/05/09 14:16:16  dischi
-# let the child stdout handled by main
-#
-# -----------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # Freevo - A Home Theater PC framework
-# Copyright (C) 2002 Krister Lagerstrom, et al. 
+# Copyright (C) 2002-2004 Krister Lagerstrom, Dirk Meyer, et al.
+#
+# First edition: Krister Lagerstrom <krister-freevo@kmlager.com>
+# Maintainer:    Dirk Meyer <dmeyer@tzi.de>
+#
 # Please see the file freevo/Docs/CREDITS for a complete list of authors.
 #
 # This program is free software; you can redistribute it and/or modify
@@ -88,74 +34,27 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 #
-# ----------------------------------------------------------------------- */
-import sys
-import time
+# -----------------------------------------------------------------------------
+
+
+__all__ = [ 'Instance' ]
+
+# Python imports
 import os
-import fcntl
-import threading, thread
-import signal
-import copy
-import popen2
-import glob
-import re
 
-import notifier
-
+# Freevo imports
+import sysconfig
 import config
 import eventhandler
-import cleanup
-import util
+import util.popen
+import gui
 
 from event import *
 
-import logging
-log = logging.getLogger('childapp')
-
-watcher = None
-
-
-def killall(appname, sig=9):
-    """
-    kills all applications with the string <appname> in their commandline.
-
-    The <sig> parameter indicates the signal to use.
-    This implementation uses the /proc filesystem, it might be Linux-dependent.
-    """
-
-    unify_name = re.compile('[^A-Za-z0-9]').sub
-    appname = unify_name('', appname)
-
-    cmdline_filenames = glob.glob('/proc/[0-9]*/cmdline')
-
-    for cmdline_filename in cmdline_filenames:
-        try:
-            fd = vfs.open(cmdline_filename)
-            cmdline = fd.read()
-            fd.close()
-        except IOError:
-            continue
-        if unify_name('', cmdline).find(appname) != -1:
-            # Found one, kill it
-            pid = int(cmdline_filename.split('/')[2])
-            try:
-                os.kill(pid, sig)
-            except:
-                pass
-    return
-
-
-class Instance:
-    """
-    Base class for started child processes
-    """
-    ready = False
-
+class Instance(util.popen.Process):
     def __init__( self, app, debugname = None, doeslogging = 0, prio = 0,
                   stop_osd = 2 ):
-        cleanup.register( self.stop )
-
-        self.is_video = 0                       # Be more explicit
+        self.is_video = 0
         if stop_osd == 2: 
             self.is_video = 1
             eventhandler.post( Event( VIDEO_START ) )
@@ -163,106 +62,33 @@ class Instance:
 
         self.stop_osd = stop_osd
         if self.stop_osd:
-            import gui
             gui.display.hide()
         
         if hasattr(self, 'item'):
             eventhandler.post( Event( PLAY_START, arg = self.item ) )
 
-        # return status of the child
-        self.status = 0
-
         if isinstance( app, unicode ):
-            app = app.encode( config.LOCALE, 'ignore' )
+            app = String(app)
             
-        if isinstance(app, str):
-            # app is a string to execute. It will be executed by 'sh -c '
-            # inside the popen code
-            self.binary = app.lstrip()
-                
-            start_str = app
-            debug_name = app[ : app.find( ' ' ) ]
-        else:
-            # app is a list
-            if app[ 0 ].startswith( '--prio' ): app = app[ 1 : ]
-            while '' in app:
-                app.remove( '' )
+        if not debugname:
+            if isinstance(app, str):
+                debugname = app[ : app.find( ' ' ) ]
+            else:
+                debugname = app[ 0 ]
 
-            self.binary = str( ' ' ).join( app )
-            start_str = app
-            
-            debug_name = app[ 0 ]
-            log.info('running %s' % self.binary)
-        
-        if debug_name.rfind('/') > 0:
-            debug_name = debug_name[ debug_name.rfind( '/' ) + 1 : ]
-        else:
-            debug_name = debug_name
+            if debugname.rfind('/') > 0:
+                debugname = debugname[ debugname.rfind( '/' ) + 1 : ]
 
         if debugname:
-	    debug_name = debugname
+	    debugname = sysconfig.logfile(debugname)
 	
-	if doeslogging or config.CHILDAPP_DEBUG:
-	    doeslogging = 1
-	
-        self.__kill_timer = None
-        self.stopping = False
-        self.child   = popen2.Popen3( start_str, True, 100 )
-        self.outfile = self.child.fromchild 
-        self.errfile = self.child.childerr
-        self.infile  = self.child.tochild
+        util.popen.Process.__init__(self, app, debugname)
 
-        self.__io_out = IO_Handler( 'stdout', self.outfile, self.stdout_cb,
-                                   debug_name, doeslogging )
-        self.__io_err = IO_Handler( 'stderr', self.errfile, self.stderr_cb,
-                                   debug_name, doeslogging )
-        watcher.add( self.child, self.__child_died )
-        
         if prio and config.CONF.renice:
             os.system('%s %s -p %s 2>/dev/null >/dev/null' % \
                       (config.CONF.renice, prio, self.child.pid))
             
-        self.ready = True
-        self.dead = False
         
-
-    # Write a string to the app. 
-    def write( self, line ):
-        try:
-            self.infile.write(line)
-            self.infile.flush()
-        except (IOError, ValueError):
-            pass
-        
-
-    def isAlive( self ):
-        if not self.ready: # return true if constructor has not finished yet
-            return True
-        return not self.outfile.closed or not self.errfile.closed
-        
-
-    def stop( self, cmd = '' ):
-        """
-        stop the child
-        """
-        if self.stopping:
-            return
-
-        self.stopping = True
-        cleanup.unregister( self.stop )
-
-        if self.isAlive() and not self.__kill_timer:
-            if cmd:
-                log.info('sending exit command to app')
-                self.write(cmd)
-                cb = notifier.Callback( self._kill, 15 )
-                self.__kill_timer = notifier.addTimer( 3000, cb )
-            else:
-                cb = notifier.Callback( self._kill, 15 )
-                self.__kill_timer = notifier.addTimer( 0, cb )
-
-            while not self.dead:
-                notifier.step( False, False )
 
     def stop_event(self):
         """
@@ -271,58 +97,9 @@ class Instance:
         return PLAY_END
 
 
-    def _kill( self, signal ):
-        if not self.isAlive():
-            self.dead = True
-            return False
-        # child needs some assistance with dying ...
-        try:
-            os.kill( self.child.pid, signal )
-        except OSError:
-            pass
-
-        if signal == 15:
-            cb = notifier.Callback( self._kill, 9 )
-        else:
-            cb = notifier.Callback( self._killall, 15 )
-            
-        self.__kill_timer = notifier.addTimer( 3000, cb )
-        
-        return False
-
-    def _killall( self, signal ):
-        if not self.isAlive():
-            self.dead = True
-            return False
-        # child needs some assistance with dying ...
-        try:
-            killall( self.binary, signal )
-        except OSError:
-            pass
-
-        log.info('kill -%d %s' % ( signal, self.binary ))
-        if signal == 15:
-            cb = notifier.Callback( self._killall, 9 )
-            self.__kill_timer = notifier.addTimer( 2000, cb )
-        else:
-            log.critical('PANIC %s' % self.binary)
-            
-        return False
-
-    def __child_died( self, proc ):
-        self.dead = True
-        # cleanup IO handler and kill timer
-        self.__io_out.cleanup()
-        self.__io_err.cleanup()
-        if self.__kill_timer:
-            notifier.removeTimer( self.__kill_timer )
-        self.finished()
-
-        
     def finished(self):
         # Ok, we can use the OSD again.
         if self.stop_osd:
-            import gui
             gui.display.show()
 
         if not self.stopping:
@@ -331,122 +108,3 @@ class Instance:
             eventhandler.post( self.stop_event() )
 
         
-    # Override this method to receive stdout from the child app
-    # The function receives complete lines
-    def stdout_cb( self, line ):
-        pass
-
-
-    # Override this method to receive stderr from the child app
-    # The function receives complete lines
-    def stderr_cb( self, line ):
-        pass
-
-
-class IO_Handler:
-    """
-    reading data from socket 
-    """
-    def __init__( self, name, fp, callback, logger = None, doeslogging = 0 ):
-        self.name = name
-        self.fp = fp
-        fcntl.fcntl( self.fp.fileno(), fcntl.F_SETFL, os.O_NONBLOCK )
-        self.callback = callback
-        self.logger = None
-        self.saved = ''
-        notifier.addSocket( fp, self._handle_input )
-        if logger and doeslogging:
-            logger = os.path.join( config.LOGDIR,
-                                        '%s-%s.log' % ( logger, name ) )
-            try:
-                try:
-                    os.unlink(logger)
-                except:
-                    pass
-                self.logger = open(logger, 'w')
-                log.info('logging child to "%s"' % logger)
-            except IOError:
-                log.warning('Error: Cannot open "%s" for logging' % logger)
-            
-    def cleanup( self ):
-        notifier.removeSocket( self.fp )
-
-    def _handle_input( self, socket ):
-        data = self.fp.read( 100 )
-        if not data:
-            log.debug( '%s: No data, stopping (pid %s)!' % \
-                       ( self.name, os.getpid() ) )
-            notifier.removeSocket( self.fp )
-            self.fp.close()
-            if self.logger:
-                self.logger.close()
-        else:
-            data  = data.replace('\r', '\n')
-            lines = data.split('\n')
-
-            # Only one partial line?
-            if len(lines) == 1:
-                self.saved += data
-            else:
-                # Combine saved data and first line, send to app
-                if self.logger:
-                    self.logger.write( self.saved + lines[ 0 ] + '\n' )
-                self.callback( self.saved + lines[ 0 ] )
-                self.saved = ''
-
-                # There's one or more lines + possibly a partial line
-                if lines[ -1 ] != '':
-                    # The last line is partial, save it for the next time
-                    self.saved = lines[ -1 ]
-
-                    # Send all lines except the last partial line to the app
-                    for line in lines[ 1 : -1 ]:
-                        if self.logger:
-                            self.logger.write( line + '\n' )
-                        self.callback( line )
-                else:
-                    # Send all lines to the app
-                    for line in lines[ 1 : ]:
-                        if self.logger:
-                            self.logger.write( line + '\n' )
-                        self.callback( line )
-        return True
-    
-
-class _Watcher:
-    def __init__( self ):
-        log.info('new process watcher instance')
-        self.__processes = {}
-
-
-    def add( self, proc, cb ):
-        self.__processes[ proc ] = cb
-
-    def remove( self, proc ):
-        if self.__processes.has_key():
-            del self.__processes[ proc ]
-    
-    def step( self ):
-        remove_proc = []
-        for p in copy.copy( self.__processes ):
-            try:
-                if isinstance( p, popen2.Popen3 ):
-                    pid, status = os.waitpid( p.pid, os.WNOHANG )
-                else:
-                    pid, status = os.waitpid( p.pid, os.WNOHANG )
-            except OSError:
-                remove_proc.append( p )
-                continue
-            if not pid: continue
-            log.info('DEAD CHILD: %s (%s)' % ( pid, status ))
-            if status == -1:
-                log.error('error retrieving process information from %d' % p)
-            elif os.WIFEXITED( status ) or os.WIFSIGNALED( status ) or \
-                     os.WCOREDUMP( status ):
-                self.__processes[ p ]( p )
-                remove_proc.append( p )
-
-        # remove dead processes
-        for p in remove_proc: del self.__processes[ p ]
-
-watcher = _Watcher()
---
+++ src/util/popen.py
@@ -0,0 +1,370 @@
+# -*- coding: iso-8859-1 -*-
+# -----------------------------------------------------------------------------
+# popen.py - process control using pyNotifier
+# -----------------------------------------------------------------------------
+# $Id$
+#
+#
+# -----------------------------------------------------------------------------
+# Freevo - A Home Theater PC framework
+# Copyright (C) 2002-2004 Krister Lagerstrom, Dirk Meyer, et al.
+#
+# First Edition: Dirk Meyer <dmeyer@tzi.de>
+# Maintainer:    Dirk Meyer <dmeyer@tzi.de>
+#
+# Please see the file freevo/Docs/CREDITS for a complete list of authors.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MER-
+# CHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+#
+# -----------------------------------------------------------------------------
+
+__all__ = [ 'Process', 'killall' ]
+
+# python imports
+import os
+import fcntl
+import copy
+import popen2
+import glob
+import re
+import logging
+
+# notifier
+import notifier
+
+log = logging.getLogger('popen')
+
+# the global watcher object
+watcher = None
+
+
+def killall():
+    """
+    killall running processes
+    """
+    watcher.killall()
+
+
+class Process:
+    """
+    Base class for started child processes
+    """
+    def __init__( self, app, debugname = None):
+        """
+        Init the child process 'app'. This can either be a string or a list
+        of arguments (similar to popen2). If debugname is given, the stdout
+        and stderr will also be written. To get the stdout and stderr, you
+        need to inherit from this class and override stdout_cb and stderr_cb.
+        """
+        if isinstance(app, str):
+            # app is a string to execute. It will be executed by 'sh -c '
+            # inside the popen code
+            self.binary = app.lstrip()
+            
+            start_str = app
+        else:
+            # app is a list
+            while '' in app:
+                app.remove( '' )
+
+            self.binary = str( ' ' ).join( app )
+            start_str = app
+            
+            log.info('running %s' % self.binary)
+        
+        self.__kill_timer = None
+        self.stopping = False
+        self.dead = False
+        self.child = popen2.Popen3( start_str, True, 100 )
+        # IO_Handler for stdout
+        self.stdout = IO_Handler( 'stdout', self.child.fromchild,
+                                    self.stdout_cb, debugname )
+        # IO_Handler for stderr
+        self.stderr = IO_Handler( 'stderr', self.child.childerr,
+                                    self.stderr_cb, debugname )
+        watcher.add( self, self.__child_died )
+        
+        
+
+    def write( self, line ):
+        """
+        Write a string to the app. 
+        """
+        try:
+            self.child.tochild.write(line)
+            self.child.tochild.flush()
+        except (IOError, ValueError):
+            pass
+        
+
+    def isAlive( self ):
+        """
+        Return True if the app is still running
+        """
+        return not self.dead
+        
+
+    def stop( self, cmd = '' ):
+        """
+        Stop the child. If 'cmd' is given, this stop command will send to
+        the app to stop itself. If this is not working, kill -15 and kill -9
+        will be used to kill the app.
+        """
+        if self.stopping:
+            return
+
+        self.stopping = True
+
+        if self.isAlive() and not self.__kill_timer:
+            if cmd:
+                log.info('sending exit command to app')
+                self.write(cmd)
+                cb = notifier.Callback( self._kill, 15 )
+                self.__kill_timer = notifier.addTimer( 3000, cb )
+            else:
+                cb = notifier.Callback( self._kill, 15 )
+                self.__kill_timer = notifier.addTimer( 0, cb )
+
+            while not self.dead:
+                notifier.step()
+
+
+    def _kill( self, signal ):
+        """
+        Internal kill helper function
+        """
+        if not self.isAlive():
+            self.dead = True
+            return False
+        # child needs some assistance with dying ...
+        try:
+            os.kill( self.child.pid, signal )
+        except OSError:
+            pass
+
+        if signal == 15:
+            cb = notifier.Callback( self._kill, 9 )
+        else:
+            cb = notifier.Callback( self._killall, 15 )
+            
+        self.__kill_timer = notifier.addTimer( 3000, cb )
+        
+        return False
+
+
+    def _killall( self, signal ):
+        """
+        Internal killall helper function
+        """
+        if not self.isAlive():
+            self.dead = True
+            return False
+        # child needs some assistance with dying ...
+        try:
+            # kill all applications with the string <appname> in their
+            # commandline. This implementation uses the /proc filesystem,
+            # it is Linux-dependent.
+            unify_name = re.compile('[^A-Za-z0-9]').sub
+            appname = unify_name('', self.binary)
+
+            cmdline_filenames = glob.glob('/proc/[0-9]*/cmdline')
+
+            for cmdline_filename in cmdline_filenames:
+                try:
+                    fd = vfs.open(cmdline_filename)
+                    cmdline = fd.read()
+                    fd.close()
+                except IOError:
+                    continue
+                if unify_name('', cmdline).find(appname) != -1:
+                    # Found one, kill it
+                    pid = int(cmdline_filename.split('/')[2])
+                    try:
+                        os.kill(pid, signal)
+                    except:
+                        pass
+        except OSError:
+            pass
+
+        log.info('kill -%d %s' % ( signal, self.binary ))
+        if signal == 15:
+            cb = notifier.Callback( self._killall, 9 )
+            self.__kill_timer = notifier.addTimer( 2000, cb )
+        else:
+            log.critical('PANIC %s' % self.binary)
+            
+        return False
+
+    def __child_died( self, proc ):
+        """
+        Callback from watcher when the child died.
+        """
+        self.dead = True
+        # cleanup IO handler and kill timer
+        self.stdout.cleanup()
+        self.stderr.cleanup()
+        if self.__kill_timer:
+            notifier.removeTimer( self.__kill_timer )
+        self.finished()
+
+        
+    def finished(self):
+        """
+        Override this method to handle cleanup / notifications after the child
+        is dead.
+        """
+        pass
+
+        
+    def stdout_cb( self, line ):
+        """
+        Override this method to receive stdout from the child app
+        The function receives complete lines
+        """
+        pass
+
+
+    def stderr_cb( self, line ):
+        """
+        Override this method to receive stderr from the child app
+        The function receives complete lines
+        """
+        pass
+
+
+class IO_Handler:
+    """
+    Reading data from socket (stdout or stderr)
+    """
+    def __init__( self, name, fp, callback, logger = None):
+        self.name = name
+        self.fp = fp
+        fcntl.fcntl( self.fp.fileno(), fcntl.F_SETFL, os.O_NONBLOCK )
+        self.callback = callback
+        self.logger = None
+        self.saved = ''
+        notifier.addSocket( fp, self._handle_input )
+        if logger:
+            logger = '%s-%s.log' % ( logger, name )
+            try:
+                try:
+                    os.unlink(logger)
+                except:
+                    pass
+                self.logger = open(logger, 'w')
+                log.info('logging child to "%s"' % logger)
+            except IOError:
+                log.warning('Error: Cannot open "%s" for logging' % logger)
+            
+
+    def close( self ):
+        notifier.removeSocket( self.fp )
+        self.fp.close()
+        if self.logger:
+            self.logger.close()
+        
+
+    def cleanup( self ):
+        notifier.removeSocket( self.fp )
+
+
+    def _handle_input( self, socket ):
+        data = self.fp.read( 10000 )
+        if not data:
+            log.debug( '%s: No data, stopping (pid %s)!' % \
+                       ( self.name, os.getpid() ) )
+            notifier.removeSocket( self.fp )
+            self.fp.close()
+            if self.logger:
+                self.logger.close()
+        else:
+            data  = data.replace('\r', '\n')
+            lines = data.split('\n')
+
+            # Only one partial line?
+            if len(lines) == 1:
+                self.saved += data
+            else:
+                # Combine saved data and first line, send to app
+                if self.logger:
+                    self.logger.write( self.saved + lines[ 0 ] + '\n' )
+                self.callback( self.saved + lines[ 0 ] )
+                self.saved = ''
+
+                # There's one or more lines + possibly a partial line
+                if lines[ -1 ] != '':
+                    # The last line is partial, save it for the next time
+                    self.saved = lines[ -1 ]
+
+                    # Send all lines except the last partial line to the app
+                    for line in lines[ 1 : -1 ]:
+                        if self.logger:
+                            self.logger.write( line + '\n' )
+                        self.callback( line )
+                else:
+                    # Send all lines to the app
+                    for line in lines[ 1 : ]:
+                        if self.logger:
+                            self.logger.write( line + '\n' )
+                        self.callback( line )
+        return True
+    
+
+class _Watcher:
+    def __init__( self ):
+        log.info('new process watcher instance')
+        self.__processes = {}
+
+
+    def add( self, proc, cb ):
+        self.__processes[ proc ] = cb
+
+    def remove( self, proc ):
+        if self.__processes.has_key():
+            del self.__processes[ proc ]
+    
+    def step( self ):
+        remove_proc = []
+        for p in copy.copy( self.__processes ):
+            try:
+                if isinstance( p.child, popen2.Popen3 ):
+                    pid, status = os.waitpid( p.child.pid, os.WNOHANG )
+                else:
+                    pid, status = os.waitpid( p.pid, os.WNOHANG )
+            except OSError:
+                remove_proc.append( p )
+                continue
+            if not pid: continue
+            log.info('DEAD CHILD: %s (%s)' % ( pid, status ))
+            if status == -1:
+                log.error('error retrieving process information from %d' % p)
+            elif os.WIFEXITED( status ) or os.WIFSIGNALED( status ) or \
+                     os.WCOREDUMP( status ):
+                self.__processes[ p ]( p )
+                remove_proc.append( p )
+
+        # remove dead processes
+        for p in remove_proc: del self.__processes[ p ]
+
+    def killall( self ):
+        for p in copy.copy(self.__processes):
+            p.stop()
+
+
+# init global watcher object
+watcher = _Watcher()
+
+# add checking for dead children to the notifier
+notifier.addDispatcher( watcher.step )
