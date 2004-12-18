c1f40454bd1403799ffdc76a8969de3d42a81b97
Dirk Meyer <dischi@freevo.org>
2004-12-18 19:07:12 +0000

    add doc page
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@6789 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

---
+++ src/www/doc.py
@@ -0,0 +1,61 @@
+# python imports
+import sys
+import os
+import re
+
+# freevo imports
+import config
+import util
+
+# www imports
+from base import HTMLResource, FreevoResource
+
+re_link = re.compile('(href *= *")([^:]*?.html)"')
+
+class DocResource(FreevoResource):
+    def replace_link(self, reg):
+        return '%sdoc?file=%s"' % reg.groups()
+
+
+    def _render(self, request):
+        if request.query.has_key('file'):
+            page = request.query['file']
+        else:
+            page = 'Index.html'
+
+        fv = HTMLResource()
+        fv.printHeader(_('Documentation'), None, selected=_('Doc'))
+        header = fv.res.split('</head>')
+        fv.res = header[0] + '''
+        <link rel="stylesheet" type="text/css" charset="utf-8"
+        media="all" href="modern/css/common.css">
+        <link rel="stylesheet" type="text/css" charset="utf-8" media="screen"
+        href="modern/css/screen.css">
+        <link rel="stylesheet" type="text/css" charset="utf-8" media="print"
+        href="modern/css/print.css">
+        <link rel="stylesheet" type="text/css" charset="utf-8"
+        href="modern/css/freevo.css">
+        ''' + header[1]
+
+        fv.res += '<p>&nbsp;</p>\n'
+
+        src = os.path.join(config.DOC_DIR, 'html/%s' % page)
+        src = open(src)
+        p = False
+        for line in src.readlines():
+            if line.find('<!-- start page -->') > 0:
+                p = True
+            if p:
+                line = re_link.sub(self.replace_link, line)
+                fv.res += line
+            if line.find('<!-- end page -->') > 0:
+                p = False
+        src.close()
+        fv.printLinks()
+        fv.printFooter()
+
+        return String( fv.res )
+
+
+# init the resource
+resource = DocResource()
