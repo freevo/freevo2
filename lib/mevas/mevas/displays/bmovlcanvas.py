6832f93599ad0174ce13f5120bd9e051c4cf04ba
Dirk Meyer <dischi@freevo.org>
2004-12-18 13:29:04 +0000

    major cleanup
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@6767 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- lib/mevas/mevas/displays/bmovlcanvas.py
+++ lib/mevas/mevas/displays/bmovlcanvas.py
@@ -3,22 +3,54 @@ import os
 from bitmapcanvas import *
 from mevas import imagelib
 
+fifo_counter = 0
+
 class BmovlCanvas(BitmapCanvas):
 
 	def __init__(self, size):
-		super(BmovlCanvas, self).__init__(size, preserve_alpha = True)
-		self._update_rect = None
-		self.fifo = os.open('/tmp/bmovl-%s' % os.getpid(), os.O_WRONLY)
+		self.open_fifo()
 		self.bmovl_visible = True
 		self.send('SHOW\n')
+		super(BmovlCanvas, self).__init__(size, preserve_alpha = True)
+		self._update_rect = None
 
 
+	def open_fifo(self):
+		if os.path.exists(self.get_fname()):
+			os.unlink(self.get_fname())
+		os.mkfifo(self.get_fname())
+		self.fifo = os.open(self.get_fname(), os.O_RDWR, os.O_NONBLOCK)
+
+		
+	def close_fifo(self):
+		if self.fifo:
+			try:
+				os.close(self.fifo)
+			except OSError:
+				self.fifo = None
+		if os.path.exists(self.get_fname()):
+			os.unlink(self.get_fname())
+
+		
+	def get_fname(self):
+		"""
+		return fifo filename
+		"""
+		try:
+			return self.__fname
+		except AttributeError:
+			pass
+		global fifo_counter
+		self.__fname = '/tmp/bmovl-%s-%s' % (os.getpid(), fifo_counter)
+		fifo_counter += 1
+		return self.__fname
+
+		
 	def send(self, msg):
 		try:
 			os.write(self.fifo, msg)
 			return True
 		except (IOError, OSError):
-			print 'IOError on bmovl.fifo'
 			return False
 		
 	def has_visible_child(self):
@@ -32,27 +64,29 @@ class BmovlCanvas(BitmapCanvas):
 		if not self._update_rect:
 			return
 		if not self.fifo:
-			print 'IOError: no bmovl fifo'
 			return
 
 		if self.bmovl_visible and not self.has_visible_child():
-			print 'bmovl hide'
 			self.send('HIDE\n')
 			self.send('CLEAR %s %s 0 0\n' % \
 				  (self.width, self.height))
 			self.bmovl_visible = False
 			return
-		
+
+		# get update rect
 		pos, size = self._update_rect
+		# increase update rect because mplayer sometimes draws outside
+		pos = (max(0, pos[0] - 2), max(pos[1] - 2, 0))
+		size = (min(size[0] + 4, self.width), 
+			min(size[1] + 4, self.height))
+
 		img = imagelib.crop(self._backing_store, pos, size)
-		print 'bmovl update', pos, size
 		self.send('RGBA32 %d %d %d %d %d %d\n' % \
 			  (size[0], size[1], pos[0], pos[1], 0, 0))
 		self.send(str(img.get_raw_data('RGBA')))
 		self._update_rect = None
 
 		if not self.bmovl_visible and self.has_visible_child():
-			print 'bmovl show'
 			self.send('SHOW\n')
 			self.bmovl_visible = True
 
