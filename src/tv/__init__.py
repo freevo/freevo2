2cec22ae2801fe87ab7f372bce9f0dbc30bbf3fd
Dirk Meyer <dischi@freevo.org>
2005-01-08 11:59:58 +0000

    respect coding style
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@6925 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- src/tv/__init__.py
+++ src/tv/__init__.py
@@ -1,43 +1,17 @@
 # -*- coding: iso-8859-1 -*-
-# -----------------------------------------------------------------------
-# tv.py - This is the Freevo TV plugin. 
-# -----------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+# __init__.py - Freevo tv plugin
+# -----------------------------------------------------------------------------
 # $Id$
 #
-# Notes:
-# Todo:        
 #
-# -----------------------------------------------------------------------
-# $Log$
-# Revision 1.13  2004/12/04 01:21:23  rshortt
-# Detect channels too.
-#
-# Revision 1.12  2004/11/21 10:12:47  dischi
-# improve system detect, use config.detect now
-#
-# Revision 1.11  2004/08/05 17:27:16  dischi
-# Major (unfinished) tv update:
-# o the epg is now taken from pyepg in lib
-# o all player should inherit from player.py
-# o VideoGroups are replaced by channels.py
-# o the recordserver plugins are in an extra dir
-#
-# Bugs:
-# o The listing area in the tv guide is blank right now, some code
-#   needs to be moved to gui but it's not done yet.
-# o The only player working right now is xine with dvb
-# o channels.py needs much work to support something else than dvb
-# o recording looks broken, too
-#
-# Revision 1.10  2004/07/10 12:33:41  dischi
-# header cleanup
-#
-# Revision 1.9  2003/12/06 16:52:11  dischi
-# only import stuff we need
-#
-# -----------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # Freevo - A Home Theater PC framework
-# Copyright (C) 2002 Krister Lagerstrom, et al. 
+# Copyright (C) 2002-2004 Krister Lagerstrom, Dirk Meyer, et al.
+#
+# Maintainer:    Dirk Meyer <dmeyer@tzi.de>
+#                Rob Shortt <rob@infointeractive.com>
+#
 # Please see the file freevo/Docs/CREDITS for a complete list of authors.
 #
 # This program is free software; you can redistribute it and/or modify
@@ -54,10 +28,11 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 #
-# ----------------------------------------------------------------------- */
+# -----------------------------------------------------------------------------
 
-
+# freevo imports
 import plugin
+
 
 class PluginInterface(plugin.MainMenuPlugin):
     """
@@ -67,11 +42,15 @@ class PluginInterface(plugin.MainMenuPlugin):
         """
         return the tv menu
         """
-        import config
-        import tvmenu
-        import menu
+        # import here to avoid importing all this when some helpers only
+        # want to import something from iside the tv directory
 
+        import config
+        from tvmenu import TVMenu
+        from mainmenu import MainMenuItem
+
+        # detect channels
         config.detect('tvcards', 'channels')
 
-        return [ menu.MenuItem('', action=tvmenu.TVMenu().main_menu,
-                               type='main', parent=parent, skin_type='tv') ]
+        return [ MainMenuItem('', action=TVMenu().main_menu, type='main',
+                              parent=parent, skin_type='tv') ]
