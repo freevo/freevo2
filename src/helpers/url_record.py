3b80cfe26fba4e626f6914cb8c087bf195f7802f
Rob Shortt <rob@tvcentric.com>
2005-01-16 20:42:13 +0000

    Use this helper to record a stream from a given URL.
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@6973 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

---
+++ src/helpers/url_record.py
@@ -0,0 +1,105 @@
+# -*- coding: iso-8859-1 -*-
+# -----------------------------------------------------------------------------
+# url_record.py - Freevo helper to record a stream from a URL to a file.
+# -----------------------------------------------------------------------------
+# $Id$
+#
+#
+# -----------------------------------------------------------------------------
+# Freevo - A Home Theater PC framework
+# Copyright (C) 2002-2004 Krister Lagerstrom, Dirk Meyer, et al.
+#
+# First Edition: Rob Shortt <rshortt@users.sourceforge.net>
+# Maintainer:    Rob Shortt <rshortt@users.sourceforge.net>
+#
+# Please see the file freevo/Docs/CREDITS for a complete list of authors.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MER-
+# CHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+#
+# -----------------------------------------------------------------------------
+
+import urllib
+import time
+import signal
+import sys
+
+import config
+
+DEBUG = config.DEBUG
+CHUNKSIZE = 1024 * 128
+
+data = None
+save_file = None
+URL = None
+
+
+def main():
+    global data
+    global save_file
+    global URL
+    record_forever = False
+
+    if len(sys.argv) < 3:
+        print 'Usage: url_record <URL> <save file> <seconds to record>'
+        print '       If <seconds to record> is ommitted it will record '
+        print '       until interrupted.'
+        sys.exit(0)
+
+    URL = sys.argv[1]
+    SAVE_FILE = sys.argv[2]
+    try:
+        LENGTH = int(sys.argv[3])
+        stop = time.time() + LENGTH
+    except:
+        record_forever = True
+        LENGTH = 'undetermined'
+
+    data = urllib.urlopen(URL)
+
+    save_file = open(SAVE_FILE, 'w')
+    #signal.signal(signal.SIGINT, finish)
+    #signal.signal(signal.SIGKILL, finish)
+    print 'Recording %s to %s for %s seconds.' % (URL, SAVE_FILE, LENGTH)
+
+
+    try:
+        while record_forever or time.time() < stop:
+            buf = data.read(CHUNKSIZE)
+            if DEBUG:  
+                sys.stdout.write('.')
+                sys.stdout.flush()
+            save_file.write(buf)
+            save_file.flush()
+    except:
+        print 'recording interrupted'
+
+    finish(0)
+
+
+def finish(signum=None, frame=None):
+    global data
+    global save_file
+    global URL
+
+    if DEBUG: print
+    print 'Finished recording %s with signal %s.' % (URL, signum)
+    data.close()
+    save_file.close()
+    sys.exit(0)
+
+
+if __name__ == '__main__':
+    main()
+
