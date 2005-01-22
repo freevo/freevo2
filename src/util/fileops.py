cc760ea0c2316d2608098a9df5c5c77384e4b9bf
Dirk Meyer <dischi@freevo.org>
2005-01-22 12:04:59 +0000

    ignore . directories
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@6987 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- src/util/fileops.py
+++ src/util/fileops.py
@@ -214,6 +214,10 @@ def _match_files_recursively_helper(result, dirname, names):
     """
     help function for match_files_recursively
     """
+    if dirname[dirname.rfind('/'):][1] == '.':
+        # ignore directories starting with a dot
+        # Note: subdirectories of that dir will still be searched
+        return result
     for name in names:
         if not name in ('CVS', '.xvpics', '.thumbnails', '.pics',
                         'folder.fxd', 'lost+found'):
