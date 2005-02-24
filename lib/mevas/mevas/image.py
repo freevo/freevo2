d8bf344d0d79738968be44df0387e7ef4866d2d0
Dirk Meyer <dischi@freevo.org>
2005-02-24 19:39:49 +0000

    repect freevo coding style
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@7097 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- lib/mevas/mevas/image.py
+++ lib/mevas/mevas/image.py
@@ -1,182 +1,235 @@
+# -*- coding: iso-8859-1 -*-
+# -----------------------------------------------------------------------------
+# image.py - image canvas
+# -----------------------------------------------------------------------------
+# $Id$
+#
+# -----------------------------------------------------------------------------
+# Mevas - MeBox Canvas System
+# Copyright (C) 2004-2005 Jason Tackaberry <tack@sault.org>
+#
+# First Edition: Jason Tackaberry <tack@sault.org>
+# Maintainer:    Dirk Meyer <dmeyer@tzi.de>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MER-
+# CHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+#
+# -----------------------------------------------------------------------------
+
 __all__ = [ 'CanvasImage' ]
 
-import types, time, math
-from util import *
+# python imports
+import types
+import time
+import math
 
+# mevas imports
+from util import *
 import imagelib
 from canvasobject import *
 
 class CanvasImage(CanvasObject):
 
-	def __init__(self, image_or_size = None):
-		CanvasObject.__init__(self)
+    def __init__(self, image_or_size = None):
+        CanvasObject.__init__(self)
 
-		self.image = None
-		if type(image_or_size) == types.TupleType:
-			if 0 in image_or_size:
-				raise ValueError, "Invalid dimension for CanvasImage %s" % repr(image_or_size)
-			self.new(image_or_size)
-		else:
-			self.set_image(image_or_size)
-			if self.image and not self.image.has_alpha:
-				self.set_alpha(256)
-			
-	def needs_blitting(self, blit = None):
-		"""
-		Sets whether or not the image needs blitting (i.e. the actual image
-		data has changed, not just its attributes like position, zindex,
-		etc.).  If True, the image will get blitted on the next paint.
-
-		With no arguments, it just returns the last value passed.
-		"""
-		if blit == None:
-			return self._needs_blitting
-		self._needs_blitting = blit
-
-	def _image_changed(self):
-		self.set_size(self.image.size)
-		self.needs_blitting(True)
-		self.queue_paint()
-
-	def scale(self, dst_size, src_pos = (0,0), src_size = (-1, -1)):
-		"""
-		Scale the image to the given 'size', where 'size' is a tuple holding the
-		new width and height.
-		"""
-		# Clamp dimensions to multiples of two
-		# FIXME: we only really need to do this for MPlayerCanvas since it
-		# stores images in YV12.
-		#w, h = dst_size
-		#if w % 2: w = int(math.ceil(w / 2.0)) * 2
-		#if h % 2: h = int(math.ceil(h / 2.0)) * 2
-		self.image.scale(dst_size, src_pos, src_size)
-		self._image_changed()
+        self.image = None
+        if type(image_or_size) == types.TupleType:
+            if 0 in image_or_size:
+                raise ValueError, "Invalid dimension for CanvasImage %s" % \
+                      repr(image_or_size)
+            self.new(image_or_size)
+        else:
+            self.set_image(image_or_size)
+            if self.image and not self.image.has_alpha:
+                self.set_alpha(256)
 
 
-	def crop(self, pos, size):
-		"""
-		Crop the image at position 'pos', a tuple holding the left and
-		top coordinates, to the size 'size', a tuple holding the new
-		width and height.
-		"""
-		self.image.crop(pos, size)
-		self._image_changed()
+    def needs_blitting(self, blit = None):
+        """
+        Sets whether or not the image needs blitting (i.e. the actual image
+        data has changed, not just its attributes like position, zindex,
+        etc.).  If True, the image will get blitted on the next paint.
 
-	def rotate(self, angle):
-		"""
-		Rotate the image by the given angle.
-		"""
-		self.image.rotate(angle)
-		self._image_changed()
-
-	def flip(self):
-		self.image.flip()
-		self._image_changed()
-
-	def mirror(self):
-		self.image.mirror()
-		self._image_changed()
-
-	def draw_mask(self, mask_image, pos = (0, 0)):
-		"""
-		Draw 'mask_image' over top of the existing image.  The luma channel of
-		the mask image is blended with the alpha channel of the existing 
-		image.
-		"""
-		if type(mask_image) in types.StringTypes:
-			mask_image = imagelib.open(mask_image)
-		elif isinstance(mask_image, CanvasImage):
-			mask_image = mask_image.image
-
-		self.image.draw_mask(mask_image, pos)
-		self._image_changed()
+        With no arguments, it just returns the last value passed.
+        """
+        if blit == None:
+            return self._needs_blitting
+        self._needs_blitting = blit
 
 
-	def draw_image(self, image, dst_pos = (0, 0), dst_size = (-1, -1), 
-	               src_pos = (0, 0), src_size = (-1, -1), alpha = 255):
-		"""
-		Draws (and blends) another image 'image' on top of the existing image,
-		at position 'pos', a tuple containing the left and top coordinates 
-		relative to the destination image with layer alpha 'alpha'
-		"""
-		if type(image) in types.StringTypes:
-			image = imagelib.open(image)
-		elif isinstance(image, CanvasImage):
-			image = image.image
-		if not self.image:
-			if dst_size != (-1, -1):
-				image.scale(dst_size)
-			else:
-				self.image = image
-		else:
-			self.image.blend(image, dst_pos, dst_size, src_pos, src_size, alpha)
-		self._image_changed()
+    def _image_changed(self):
+        self.set_size(self.image.size)
+        self.needs_blitting(True)
+        self.queue_paint()
 
 
-	def get_image(self, image):
-		return self.image
-
-	def set_image(self, image):
-		"""
-		Sets the image for this object to 'image'
-		"""
-		if type(image) in types.StringTypes:
-			self.filename = image
-			image = imagelib.open(image)
-		elif isinstance(image, CanvasImage):
-			image = image.image.copy()
-			self.filename = None
-		if self.image == image:
-			return
-		if image == None:
-			self.reset()
-
-		self.image = image
-		self._image_changed()
+    def scale(self, dst_size, src_pos = (0,0), src_size = (-1, -1)):
+        """
+        Scale the image to the given 'size', where 'size' is a tuple holding
+        the new width and height.
+        """
+        # Clamp dimensions to multiples of two
+        # FIXME: we only really need to do this for MPlayerCanvas since it
+        # stores images in YV12.
+        #w, h = dst_size
+        #if w % 2: w = int(math.ceil(w / 2.0)) * 2
+        #if h % 2: h = int(math.ceil(h / 2.0)) * 2
+        self.image.scale(dst_size, src_pos, src_size)
+        self._image_changed()
 
 
-	def draw_text(self, text, pos = (0, 0), font = "arial", size = 24, color = (255,255,255,255)):
-		"""
-		Draws the given text over the image at position 'pos', a tuple holding
-		the left and top coordinates, with the supplied font, size, and color.
-		color is a tuple holding the RGBA values for the text.
-		"""
-		if type(font) in types.StringTypes:
-			font = imagelib.load_font(font, size)
-		metrics = self.image.draw_text(pos, text, color, font)
-		self._image_changed()
-		return metrics
-
-	def draw_rectangle(self, pos, size, color = (255, 255, 255, 255), fill = True):
-		#print "Draw rect", pos, size, color, fill
-		self.image.draw_rectangle(pos, size, color, fill)
-		self._image_changed()
-
-	def draw_ellipse(self, center_pos, amplitude, color = (255, 255, 255, 255), fill = True):
-		self.image.draw_ellipse(center_pos, amplitude, color, fill)
-		self._image_changed()
+    def crop(self, pos, size):
+        """
+        Crop the image at position 'pos', a tuple holding the left and
+        top coordinates, to the size 'size', a tuple holding the new
+        width and height.
+        """
+        self.image.crop(pos, size)
+        self._image_changed()
 
 
-	def new(self, size):
-		"""
-		Initialize a blank image to the given size, a tuple containing the
-		new width and height.
-		"""
-		self.image = imagelib.new(size)
-		self.filename = None
-		self._image_changed()
-
-	def get_size(self):
-		if not self.image:
-			return 0, 0
-		return self.image.width, self.image.height
-
-	def reset(self):
-		CanvasObject.reset(self)
-		self.needs_blitting(True)
-
-	def save(self, filename):
-		return self.image.save(filename)
+    def rotate(self, angle):
+        """
+        Rotate the image by the given angle.
+        """
+        self.image.rotate(angle)
+        self._image_changed()
 
 
-# vim: ts=4
+    def flip(self):
+        """
+        Flip the image.
+        """
+        self.image.flip()
+        self._image_changed()
+
+
+    def mirror(self):
+        """
+        Mirror the image.
+        """
+        self.image.mirror()
+        self._image_changed()
+
+
+    def draw_mask(self, mask_image, pos = (0, 0)):
+        """
+        Draw 'mask_image' over top of the existing image.  The luma channel of
+        the mask image is blended with the alpha channel of the existing
+        image.
+        """
+        if type(mask_image) in types.StringTypes:
+            mask_image = imagelib.open(mask_image)
+        elif isinstance(mask_image, CanvasImage):
+            mask_image = mask_image.image
+
+        self.image.draw_mask(mask_image, pos)
+        self._image_changed()
+
+
+    def draw_image(self, image, dst_pos = (0, 0), dst_size = (-1, -1),
+               src_pos = (0, 0), src_size = (-1, -1), alpha = 255):
+        """
+        Draws (and blends) another image 'image' on top of the existing image,
+        at position 'pos', a tuple containing the left and top coordinates
+        relative to the destination image with layer alpha 'alpha'
+        """
+        if type(image) in types.StringTypes:
+            image = imagelib.open(image)
+        elif isinstance(image, CanvasImage):
+            image = image.image
+        if not self.image:
+            if dst_size != (-1, -1):
+                image.scale(dst_size)
+            else:
+                self.image = image
+        else:
+            self.image.blend(image, dst_pos, dst_size, src_pos, src_size,
+                             alpha)
+        self._image_changed()
+
+
+    def get_image(self, image):
+        return self.image
+
+
+    def set_image(self, image):
+        """
+        Sets the image for this object to 'image'
+        """
+        if type(image) in types.StringTypes:
+            self.filename = image
+            image = imagelib.open(image)
+        elif isinstance(image, CanvasImage):
+            image = image.image.copy()
+            self.filename = None
+        if self.image == image:
+            return
+        if image == None:
+            self.reset()
+
+        self.image = image
+        self._image_changed()
+
+
+    def draw_text(self, text, pos = (0, 0), font = "arial", size = 24,
+                  color = (255,255,255,255)):
+        """
+        Draws the given text over the image at position 'pos', a tuple holding
+        the left and top coordinates, with the supplied font, size, and color.
+        color is a tuple holding the RGBA values for the text.
+        """
+        if type(font) in types.StringTypes:
+            font = imagelib.load_font(font, size)
+        metrics = self.image.draw_text(pos, text, color, font)
+        self._image_changed()
+        return metrics
+
+
+    def draw_rectangle(self, pos, size, color = (255, 255, 255, 255),
+                       fill = True):
+        self.image.draw_rectangle(pos, size, color, fill)
+        self._image_changed()
+
+
+    def draw_ellipse(self, center_pos, amplitude, color = (255, 255, 255, 255),
+                     fill = True):
+        self.image.draw_ellipse(center_pos, amplitude, color, fill)
+        self._image_changed()
+
+
+    def new(self, size):
+        """
+        Initialize a blank image to the given size, a tuple containing the
+        new width and height.
+        """
+        self.image = imagelib.new(size)
+        self.filename = None
+        self._image_changed()
+
+
+    def get_size(self):
+        if not self.image:
+            return 0, 0
+        return self.image.width, self.image.height
+
+
+    def reset(self):
+        CanvasObject.reset(self)
+        self.needs_blitting(True)
+
+
+    def save(self, filename):
+        return self.image.save(filename)
--- lib/mevas/mevas/rect.py
+++ lib/mevas/mevas/rect.py
@@ -1,212 +1,228 @@
+# -*- coding: iso-8859-1 -*-
+# -----------------------------------------------------------------------------
+# rect.py - rectangle calculation functions
+# -----------------------------------------------------------------------------
+# $Id$
+#
+# -----------------------------------------------------------------------------
+# Mevas - MeBox Canvas System
+# Copyright (C) 2004-2005 Jason Tackaberry <tack@sault.org>
+#
+# First Edition: Jason Tackaberry <tack@sault.org>
+# Maintainer:    Dirk Meyer <dmeyer@tzi.de>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MER-
+# CHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+#
+# -----------------------------------------------------------------------------
+
 import math
+
 empty = (0, 0), (0, 0)
 
 def union(r1, r2):
-	r1x, r1y, r1w, r1h = r1[0] + r1[1]
-	r2x, r2y, r2w, r2h = r2[0] + r2[1]
+    r1x, r1y, r1w, r1h = r1[0] + r1[1]
+    r2x, r2y, r2w, r2h = r2[0] + r2[1]
 
-	Rx = min(r1x, r2x)
-	Ry = min(r1y, r2y)
-	Rw = max(r1x+r1w, r2x+r2w) - Rx
-	Rh = max(r1y+r1h, r2y+r2h) - Ry
-	
-	return (Rx, Ry), (Rw, Rh)
+    Rx = min(r1x, r2x)
+    Ry = min(r1y, r2y)
+    Rw = max(r1x+r1w, r2x+r2w) - Rx
+    Rh = max(r1y+r1h, r2y+r2h) - Ry
+
+    return (Rx, Ry), (Rw, Rh)
 
 
 def point_in_rect(pt, r):
-	rx, ry, rw, rh = r[0] + r[1]
-	px, py = pt
+    rx, ry, rw, rh = r[0] + r[1]
+    px, py = pt
 
-	if px < rx or px > rx + rw or \
-	   py < ry or py > ry + rh:
-		return False
+    if px < rx or px > rx + rw or \
+           py < ry or py > ry + rh:
+        return False
 
-	return True
+    return True
 
 
 def intersect(r1, r2):
-	r1x, r1y, r1w, r1h = r1[0] + r1[1]
-	r2x, r2y, r2w, r2h = r2[0] + r2[1]
+    r1x, r1y, r1w, r1h = r1[0] + r1[1]
+    r2x, r2y, r2w, r2h = r2[0] + r2[1]
 
-	# No intersection for these cases.
-	if r1x + r1w < r2x or \
- 	  r1x > r2x + r2w or \
-	  r1y + r1h < r2y or \
-	  r1y > r2y + r2h:
-		return empty
+    # No intersection for these cases.
+    if r1x + r1w < r2x or \
+           r1x > r2x + r2w or \
+           r1y + r1h < r2y or \
+           r1y > r2y + r2h:
+        return empty
 
-	Rx = max(r1x, r2x)
-	Ry = max(r1y, r2y)
-	Rw = min(r1x+r1w, r2x+r2w) - Rx
-	Rh = min(r1y+r1h, r2y+r2h) - Ry
+    Rx = max(r1x, r2x)
+    Ry = max(r1y, r2y)
+    Rw = min(r1x+r1w, r2x+r2w) - Rx
+    Rh = min(r1y+r1h, r2y+r2h) - Ry
 
-	if 0 in (Rw, Rh):
-		return empty
+    if 0 in (Rw, Rh):
+        return empty
 
-	return (Rx, Ry), (Rw, Rh)
+    return (Rx, Ry), (Rw, Rh)
 
 
 def contained_in(r1, r2):
-	"""
-	Returns True if r1 is fully contained in r2.
-	"""
-	r = intersect(r1, r2)
-	return r == r1
+    """
+    Returns True if r1 is fully contained in r2.
+    """
+    return intersect(r1, r2) == r1
 
 
 def contained_in_list(r, list):
-	for rect in list:
-		if contained_in(r, rect):
-			return True
+    for rect in list:
+        if intersect(r, rect) == r:
+            return True
+    return False
 
-	return False
 
 def offset(r, pos, size = (0, 0)):
-	(rx, ry), (rw, rh)= r
-	offset_x, offset_y = pos
-	offset_w, offset_h = size
+    (rx, ry), (rw, rh)= r
+    offset_x, offset_y = pos
+    offset_w, offset_h = size
 
-	return (rx + offset_x, ry + offset_y), (rw + offset_w, rh + offset_h)
+    return (rx + offset_x, ry + offset_y), (rw + offset_w, rh + offset_h)
 
 
 def offset_list(list, pos):
-	new_list = []
-	for r in list:
-		new_list.append( offset(r, pos) )
-	return new_list
+    new_list = []
+    for r in list:
+        new_list.append( offset(r, pos) )
+    return new_list
+
 
 def clip(r1, r2):
-	return intersect(r1, r2)
+    return intersect(r1, r2)
+
 
 def clip_list(list, rect):
-	new_list = []
-	for r in list:
-		i = intersect(r, rect)
-		if i != empty:
-			new_list.append(i)
-	return new_list
+    new_list = []
+    for r in list:
+        i = intersect(r, rect)
+        if i != empty:
+            new_list.append(i)
+    return new_list
 
-def translate((pos, size), offset = (0, 0), scale = (1.0, 1.0), scale_pos = False, pixel_aspect = 1):
-	pos = pos[0] + offset[0], pos[1] + offset[1]
-	size = int(math.ceil(size[0] * scale[0])), int(math.ceil(size[1] * scale[1]))
-	if scale_pos:
-		pos = int(pos[0] * scale[0]), int(pos[1] * scale[1])
-	return pos, size
 
-def translate_list(list, offset = (0, 0), scale = (1.0, 1.0), scale_pos = False, pixel_aspect = 1):
-	new_list = []
-	for r in list:
-		new_list.append( translate(r, offset, scale, scale_pos, pixel_aspect) )
-	return new_list
+def translate((pos, size), offset = (0, 0), scale = (1.0, 1.0),
+          scale_pos = False, pixel_aspect = 1):
+    pos = pos[0] + offset[0], pos[1] + offset[1]
+    size = int(math.ceil(size[0] * scale[0])), \
+           int(math.ceil(size[1] * scale[1]))
+    if scale_pos:
+        pos = int(pos[0] * scale[0]), int(pos[1] * scale[1])
+    return pos, size
+
+
+def translate_list(list, offset = (0, 0), scale = (1.0, 1.0),
+		   scale_pos = False, pixel_aspect = 1):
+    new_list = []
+    for r in list:
+        new_list.append( translate(r, offset, scale, scale_pos, pixel_aspect) )
+    return new_list
 
 
 def reduce(list):
-	"""
-	Removes redundant rectangles.
-	"""
-	if len(list) == 0:
-		return list
+    """
+    Removes redundant rectangles.
+    """
+    if len(list) == 0:
+        return list
 
-	list = list[:]
-	new_list = []
-	while len(list):
-		rect = list.pop()
-		if rect[1][0] == 0 or rect[1][1] == 0:
-			continue
-#		print "Check", rect, "new list", new_list, "old list", list
-		if not contained_in_list(rect, new_list) and not contained_in_list(rect, list):
-			new_list.append(rect)
-
-	return new_list
+    list = list[:]
+    new_list = []
+    while len(list):
+        rect = list.pop()
+        if rect[1][0] == 0 or rect[1][1] == 0:
+            continue
+        if not contained_in_list(rect, new_list) and \
+               not contained_in_list(rect, list):
+            new_list.append(rect)
+    return new_list
 
 
 def optimize_for_rendering(list):
-	list = reduce(list)
+    list = reduce(list)
 
-	orig_len = len(list)
-	new_list = []
+    orig_len = len(list)
+    new_list = []
 
-	while len(list):
-		r1 = list.pop()
-		merged = False
-		for r2 in list[:]:
-			area_r1 = r1[1][0] * r1[1][1]
-			area_r2 = r2[1][0] * r2[1][1]
+    while len(list):
+        r1 = list.pop()
+        merged = False
+        for r2 in list[:]:
+            area_r1 = r1[1][0] * r1[1][1]
+            area_r2 = r2[1][0] * r2[1][1]
 
-			# If these two rectangles overlap more than half the area of the
-			# smaller one, then take the union instead.
-			r3 = intersect(r1, r2)
-			if r3 != empty:
-				area_r3 = r3[1][0] * r3[1][1]
-				if area_r3 > min(area_r1, area_r2) / 2:
-					list.remove(r2)
-					new_list.append( union(r1, r2) )
-					merged = True
-			
-			# If the union of these two rectangles has an area at most 20%
-			# larger than the two rectangles separately, then take the
-			# union instead.
-			# TODO: test empirically if 50% is a good figure.
-			if not merged:
-				r3 = union(r1, r2)
-				area_r3 = r3[1][0] * r3[1][1]
-				if area_r3 < (area_r1 + area_r2) * 1.5:
-					list.remove(r2)
-					new_list.append(r3)
-					merged = True
-				
-		if not merged:
-			new_list.append(r1)
+            # If these two rectangles overlap more than half the area of the
+            # smaller one, then take the union instead.
+            r3 = intersect(r1, r2)
+            if r3 != empty:
+                area_r3 = r3[1][0] * r3[1][1]
+                if area_r3 > min(area_r1, area_r2) / 2:
+                    list.remove(r2)
+                    new_list.append( union(r1, r2) )
+                    merged = True
 
-	if len(new_list) != orig_len:
-		# Keep passing over until we stop making progress.
-		new_list = optimize_for_rendering(new_list)
+            # If the union of these two rectangles has an area at most 20%
+            # larger than the two rectangles separately, then take the
+            # union instead.
+            # TODO: test empirically if 50% is a good figure.
+            if not merged:
+                r3 = union(r1, r2)
+                area_r3 = r3[1][0] * r3[1][1]
+                if area_r3 < (area_r1 + area_r2) * 1.5:
+                    list.remove(r2)
+                    new_list.append(r3)
+                    merged = True
 
-	return new_list
+        if not merged:
+            new_list.append(r1)
+
+    if len(new_list) != orig_len:
+        # Keep passing over until we stop making progress.
+        return optimize_for_rendering(new_list)
+
+    return new_list
 
 
 def remove_intersections(list):
 
-	# FIXME: this function could be smarter.  Instead of creating a union
-	# of two rectangles that intersect, it could create a number of smaller
-	# rectangles that represent the same original area but don't intersect
-	# if that total area is much less than the union would have been.
+    # FIXME: this function could be smarter.  Instead of creating a union
+    # of two rectangles that intersect, it could create a number of smaller
+    # rectangles that represent the same original area but don't intersect
+    # if that total area is much less than the union would have been.
 
-	list = reduce(list)
+    list = reduce(list)
 
-	orig_len = len(list)
-	new_list = []
-	while len(list):
-		r1 = list.pop()
-		merged = False
-		for r2 in list[:]:
-			if intersect(r1, r2) != empty:
-				list.remove(r2)
-				new_list.append( union(r1, r2) )
-				merged = True
-		if not merged:
-			new_list.append(r1)
+    orig_len = len(list)
+    new_list = []
+    while len(list):
+        r1 = list.pop()
+        merged = False
+        for r2 in list[:]:
+            if intersect(r1, r2) != empty:
+                list.remove(r2)
+                new_list.append( union(r1, r2) )
+                merged = True
+        if not merged:
+            new_list.append(r1)
 
-	if len(new_list) != orig_len:
-		new_list = remove_intersections(new_list)
+    if len(new_list) != orig_len:
+        new_list = remove_intersections(new_list)
 
-	return new_list
-				
-
-if __name__ == "__main__":
-	r1 = (10, 20), (40, 70)
-	r2 = (40, 20), (40, 70)
-	list = [((118, 443), (56, 22)), ((189, 448), (12, 19)), ((189, 448), (19, 19)), ((33, 398), (226, 199)), ((220, 410), (50, 50))]
-	list = [((0, 0), (800, 600)), ((608, 478), (4, 7)), ((0, 398), (800, 202))]
-	print list
-	print translate_list(list, offset = (0, 0), scale = ((720/800.0), (480/600.0)), scale_pos = True)
-#	print intersect( ((188, -27), (19, 19)), ((0, 0), (800, 125)) )
-#	print intersect( ((189, 448), (12, 19)), ((189, 448), (19, 19)) )
-#	print contained_in( ((189, 448), (12, 19)), ((189, 448), (19, 19)) )
-#	print reduce(list)
-	#print remove_intersections(list)
-#	print union(r1, r2)
-#	print intersect(r1, r2)
-#	print point_in_rect( (10, 20), r1)
-#	print contained_in( ((45, 25), (30, 30)), r2)
-#	print contained_in_list( ((45, 25), (130, 30)), (r2, r1))
+    return new_list
