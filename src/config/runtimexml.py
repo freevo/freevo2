69c36bd525800852f9b6a23e751bbdb179934718
Dirk Meyer <dischi@freevo.org>
2005-05-09 14:31:29 +0000

    remove mcomm support in runtime config for now
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@7237 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- src/config/runtimexml.py
+++ src/config/runtimexml.py
@@ -39,8 +39,14 @@ from xml.dom import minidom
 from xml.dom.minidom import getDOMImplementation
 from xml.dom.ext import PrettyPrint
 
-# freevo imports
-import mcomm
+# FIXME: importing this would import config again.
+#
+# This is not only bad, it also causes two mbus entities for the
+# recordserver, both in one process but one without rpc registered.
+# Maybe the mcomm part here could move to a plugin or mcomm.py could
+# auto add it?
+#
+# import mcomm
 
 # get logging object
 log = logging.getLogger('config')
@@ -637,7 +643,8 @@ class RuntimeXML:
 
         cfgfilepath = [ '.', os.path.expanduser('~/.freevo') ]
 
-        mcomm.register_event('config.runtimexml.rescan', self.__rescan)
+        # FIXME: This would create a circular import
+        # mcomm.register_event('config.runtimexml.rescan', self.__rescan)
 
         # Make a copy of our configuration variables
         self.config_globals = conf_globs
