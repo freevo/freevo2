ec2e80df957348c5e7a4d6ec93411d5d5bf5348c
Dirk Meyer <dischi@freevo.org>
2005-06-04 17:56:59 +0000

    add doc
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@7266 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- src/gui/__init__.py
+++ src/gui/__init__.py
@@ -1,12 +1,62 @@
 # -*- coding: iso-8859-1 -*-
 # -----------------------------------------------------------------------------
-# gui - Interface to all gui functions and objects
+# gui - Freevo GUI subsystem
 # -----------------------------------------------------------------------------
 # $Id$
 #
-# This file is the interface to the complete gui code of Freevo. It includes
-# some basic get/set functions and also imports all parts from submodules that
-# should be allowed to use.
+# The only variables that can be access by gui.variable are width and height
+# of the current display and the display itself. The variables may change
+# when the current activate display changes
+#
+# Submodule:    import gui.font
+# Dependencies: mevas, freevo config
+# Content:      get
+# Description:  This module makes it possible to load a font from disc and
+#               provides some basic drawing functions. It should not be used
+#               directly, better use the theme module to get fonts by aliases
+#               defined by the current theme
+#
+# Submodule:    import gui.theme
+# Dependencies: freevo, gui.font
+# Content:      get, set, font, image, icon, set_base_fxd
+# Description:  This module reads theme fxd files and provides an interface to
+#               the objects defined in the theme
+#
+# Submodule:    import gui.imagelib
+# Dependencies: PIL, mevas, freevo config and util, gui.theme
+# Content:      rotate, scale, load, item_image
+# Description:  This module can read images based on filenames or theme
+#               definitions. In most cases this module is not needed and the
+#               Image widget can be used.
+#
+# Submodule:    import gui.widgets
+# Dependencies: mevas, imagelib
+# Content:      The different widgets
+# Description:  Basic widgets that can be drawn on the display. Use this module
+#               and not mevas directly.
+#
+# Submodule:    import gui.animation
+# Dependencies: notifier
+# Content:      The different animation modules
+# Description:  Animation classes in this module can be used to animate gui
+#               widgets
+#
+# Submodule:    import gui.displays
+# Dependencies: mevas displays, freevo config, util and plugin, gui.animation
+# Content:      get, set, remove, shutdown, active
+# Description:  Display engine control module. Based on the different display
+#               engines in the display subdirectory it is possible to change
+#               the display on runtime and get some basic display informations.
+#
+# Submodule:    import gui.windows
+# Dependencies: freevo config, event and eventhandler, gui.widgets
+# Content:      The different windows and popup boxes
+# Description:  This module includes different popup boxes and a basic window
+#
+# Submodule:    import gui.areas
+# Dependencies: freevo config, util and recoord, gui.widgets, gui.animation,
+#               gui.imagelib
+# Content:      Handler, Area (for defining areas outside the gui system)
 #
 # -----------------------------------------------------------------------------
 # Freevo - A Home Theater PC framework
@@ -32,59 +82,6 @@
 #
 # -----------------------------------------------------------------------------
 
-# Imagelib
-# import imagelib
-
-# The theme engine
-# import theme
-
-# Basic widgets
-# import widgets
-
-# Windows / PopupBoxes
-# import windows
-
-# The animation module
-# import animation
-
-# -----------------------------------------------------------------------------
-# Display engine control module. Based on the different display engines
-# in the display subdirectory it is possible to change the display on
-# runtime and get some basic display informations.
-# import gui.displays
-#
-# get()
-#      Returns the current active display object
-#
-# create()
-#      Create initial display
-#
-# set(name, size)
-#      Create a display based on the display type name with the given size.
-#      The new display will be returned. This will also create a new animation
-#      render class for this display.
-#
-# remove(name)
-#      Remove the display name and stop all animations for it. The function
-#      will return the new active display.
-# -----------------------------------------------------------------------------
-
-
-# set width, height and display to fake values
-# Call gui.displays.create() to setup the display
 width   = 0
 height  = 0
 display = None
-
-# -----------------------------------------------------------------------------
-# Area module.
-# import gui.areas
-#
-# provided classes:
-#
-# Handler(type, area_list)
-#      Create an area handler with areas
-#
-# Area(name)
-#      Template for an Area to add to the AreaHandler
-# -----------------------------------------------------------------------------
--- src/gui/font.py
+++ src/gui/font.py
@@ -1,14 +1,55 @@
+# -*- coding: iso-8859-1 -*-
+# -----------------------------------------------------------------------------
+# font.py - Freevo font module
+# -----------------------------------------------------------------------------
+# $Id$
+#
+# -----------------------------------------------------------------------------
+# Freevo - A Home Theater PC framework
+# Copyright (C) 2002-2004 Krister Lagerstrom, Dirk Meyer, et al.
+#
+# First Edition: Dirk Meyer <dmeyer@tzi.de>
+# Maintainer:    Dirk Meyer <dmeyer@tzi.de>
+#
+# Please see the file freevo/Docs/CREDITS for a complete list of authors.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MER-
+# CHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+#
+# -----------------------------------------------------------------------------
+
+__all__ = [ 'get' ]
+
+# python imports
+import logging
 import mevas
+
+# freevo imports
 import config
 
-import logging
+# get logging object
 log = logging.getLogger('gui')
 
+# list of fonts already known to be not found
 font_warning = []
 
 class Font:
+    """
+    Freevo GUI font object based on the mevas font.
+    """
     def __init__(self, name, ptsize):
-        self.font   = self.__getfont__(name, ptsize)
+        self.font   = self.__getfont(name, ptsize)
         self.height = self.font.get_text_size('')[3]
         self.chars  = {}
         self.name   = name
@@ -16,6 +57,9 @@ class Font:
 
 
     def charsize(self, c):
+        """
+        Return the width of the given char.
+        """
         try:
             return self.chars[c]
         except:
@@ -23,12 +67,19 @@ class Font:
             self.chars[c] = w
             return w
 
+
     def stringsize(self, s):
+        """
+        Return the width of the given string.
+        """
         s = self.font.get_text_size(s)
         return max(s[0], s[2])
-        
 
-    def __getfont__(self, name, ptsize):
+
+    def __getfont(self, name, ptsize):
+        """
+        Load font into this object.
+        """
         log.debug('Loading font "%s:%s"' % (name, ptsize))
         try:
             return mevas.imagelib.load_font(name, ptsize)
@@ -43,23 +94,23 @@ class Font:
                     return mevas.imagelib.load_font(alt_fname, ptsize)
                 except IOError:
                     # not good
-                    global font_warning
                     if not name in font_warning:
                         print 'WARNING: No alternate found in the alias list!'
-                        print 'Falling back to default font, this may look very ugly'
+                        print 'Falling back to default font, this looks ugly'
                         font_warning.append(name)
-                    return mevas.imagelib.load_font(config.GUI_FONT_DEFAULT_NAME, ptsize)
+                    name = config.GUI_FONT_DEFAULT_NAME
+                    return mevas.imagelib.load_font(name, ptsize)
 
-        
 
-
-# init
+# init mevas font (imlib2)
 mevas.imagelib.add_font_path(config.FONT_DIR)
+
+# the font cache object for 'get'
 font_info_cache = {}
 
 def get(font, ptsize):
     """
-    return cached font
+    Return the (cached) font
     """
     key = (font, ptsize)
     try:
