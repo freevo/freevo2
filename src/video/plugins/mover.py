949a073cc6cc36618dda4af02be4ff1e22eeee1b
Dirk Meyer <dischi@freevo.org>
2005-06-18 12:05:17 +0000

    adjust to new menu system
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@7382 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- src/video/plugins/mover.py
+++ src/video/plugins/mover.py
@@ -1,35 +1,19 @@
 # -*- coding: iso-8859-1 -*-
-# -----------------------------------------------------------------------
-# mover.py - Example item plugin
-# -----------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+# mover.py - Move files to a predefined directory
+# -----------------------------------------------------------------------------
 # $Id$
 #
-# Notes: This is an example how plugins work. This plugin add a move action
-#        to movie items in a given directory to move the item to a second
-#        given directory.
+# This is an example how plugins work. This plugin add a move action to movie
+# items in a given directory to move the item to a second given directory.
 #
-# Todo:        
-#
-# -----------------------------------------------------------------------
-# $Log$
-# Revision 1.12  2005/06/12 18:49:53  dischi
-# adjust to new menu code
-#
-# Revision 1.11  2005/05/01 17:45:32  dischi
-# small ending / bugfix
-#
-# Revision 1.10  2004/11/20 18:23:05  dischi
-# use python logger module for debug
-#
-# Revision 1.9  2004/07/10 12:33:43  dischi
-# header cleanup
-#
-# Revision 1.8  2004/01/12 19:11:48  dischi
-# fix typo to prevent crash
-#
-# -----------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # Freevo - A Home Theater PC framework
-# Copyright (C) 2002 Krister Lagerstrom, et al. 
+# Copyright (C) 2002-2004 Krister Lagerstrom, Dirk Meyer, et al.
+#
+# First Edition: Dirk Meyer <dmeyer@tzi.de>
+# Maintainer:    Dirk Meyer <dmeyer@tzi.de>
+#
 # Please see the file freevo/Docs/CREDITS for a complete list of authors.
 #
 # This program is free software; you can redistribute it and/or modify
@@ -46,84 +30,101 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 #
-# ----------------------------------------------------------------------- */
+# -----------------------------------------------------------------------------
 
-
+# Python imports
 import os
 import plugin
-
 import logging
+
+# Freevo imports
+from menu import ItemPlugin, Action
+
+# get logging object
 log = logging.getLogger('video')
 
-class PluginInterface(plugin.ItemPlugin):
+
+class PluginInterface(ItemPlugin):
     """
     Plugin to move video files
 
-    Activate: 
+    Activate:
       plugin.activate('video.mover', args=('from-dir', 'to-dir'))
-   
+
     from_dir: from which directory the files should be moved from
     to_dir:   to which directory the files should be moved to
-   
+
     Optional parameters:
     recursive (boolean): should the plugin work for every subdirectory of
                          the from-directory (defaults to False)
-   
-      This parameter can be used to make a "safe delete function":
+
+      This parameter can be used to make a 'safe delete function':
       map(lambda dir: plugin.activate('video.mover', args=(dir[1],
                                                            '/mnt/media/trash',
                                                            True)),
           VIDEO_ITEMS)
-   
-   
+
+
     makedirs (boolean): should freevo create the to-dir if it doesn't exist
                         (defaults to False)
-   
+
       This parameter can be used to move seen tv-episodes/movies
       into a subdirectory ('seen'):
-   
+
       map(lambda dir: plugin.activate('video.mover', args=(dir[1],
                                                            'seen',
                                                            True,
                                                            True)),
-          VIDEO_ITEMS) 
-   
+          VIDEO_ITEMS)
+
       When to-dir is not specified as an absolute path, the mover will try
       to locate it in the current dir. If makedirs==True and to-dir
       doesn't exist, it will be created in the current-dir.
 
     """
     def __init__(self, from_dir, to_dir, recursive=False, makedirs=False):
+        """
+        Init the plugin.
+        """
         plugin.ItemPlugin.__init__(self)
         self.from_dir = from_dir
-        self.to_dir   = to_dir
+        self.to_dir = to_dir
         self.recursive = recursive
-        self.makedirs =  makedirs
-        
+        self.makedirs = makedirs
+
+
     def actions(self, item):
-        self.item = item
-        if ( item.type == 'video' 
-             and item.mode == 'file' 
-             and item.parent.type == 'dir' 
-             # don't bother moving to current-dir
-             and self.to_dir != os.path.dirname(self.item.filename)
-             # recursive
-             and ( ( self.recursive 
-                     and os.path.commonprefix( ( item.parent.dir, self.from_dir)) == self.from_dir )
-                   # or absolute
-                   or ( item.parent.dir == self.from_dir)
-                   or ( item.parent.dir == self.from_dir + '/'))):
-            return [ (self.mover_to_series,
-                      _('Move to [%s]') % os.path.basename(self.to_dir)) ]
+        """
+        Return possible actions for the given item. This is either nothing
+        or the move action.
+        """
+        if item.type != 'video' or item.mode != 'file' or \
+           item.parent.type != 'dir':
+            # we don't move such items
+            return []
+
+        if self.to_dir == os.path.dirname(item.filename):
+            # don't bother moving to current-dir
+            return []
+
+        cp = os.path.commonprefix((item.parent.dir, self.from_dir))
+        if (self.recursive and cp == self.from_dir) \
+           or ( item.parent.dir == self.from_dir) \
+           or ( item.parent.dir == self.from_dir + '/'):
+               # Create Action
+               name = _('Move to [%s]') % os.path.basename(self.to_dir)
+               return [ Action(name, self.move_item) ]
+
+        # nothing to do
         return []
 
-    def mover_to_series(self, arg=None, menuw=None):
+
+    def move_item(self, item):
         # make to_dir an absolute path
         local_to_dir = self.to_dir
         if not os.path.isabs(self.to_dir):
-            local_to_dir = os.path.join(os.path.dirname(self.item.filename),
+            local_to_dir = os.path.join(os.path.dirname(item.filename),
                                         self.to_dir)
-        
 
         # make to_dir if it doesn't exist and makedirs==True
         if not os.path.exists(local_to_dir):
@@ -131,14 +132,16 @@ class PluginInterface(plugin.ItemPlugin):
                 if self.makedirs:
                     os.makedirs(local_to_dir)
                 else:
-                    log.info("Path doesn't exist, and makedirs=%s" % self.makedirs)
-                    menuw.delete_menu()
-                    return
+                    log.info("Path doesn't exist, and makedirs=%s" % \
+                             self.makedirs)
+
         elif not os.path.isdir(local_to_dir):
             log.info("%s is not a dir" % local_to_dir)
-            return
 
-        if self.item.files:
-            self.item.files.move(local_to_dir)
+        elif item.files:
+            item.files.move(local_to_dir)
 
-        menuw.delete_menu()
+        else:
+            log.info("no files to move")
+
+        item.get_menustack().back_to_menu(item)
