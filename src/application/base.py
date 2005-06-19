ad1ed8afb3da2d7b0a46f8ea9655e379ac645378
Dirk Meyer <dischi@freevo.org>
2005-06-19 16:29:04 +0000

    respect coding guidelines, internal renamings
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@7402 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- src/application/base.py
+++ src/application/base.py
@@ -1,32 +1,16 @@
 # -*- coding: iso-8859-1 -*-
-# -----------------------------------------------------------------------
-# base.py - Basic Application inside Freevo
-# -----------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+# base.py - Basic Application
+# -----------------------------------------------------------------------------
 # $Id$
 #
-# Notes: 
-#
-# -----------------------------------------------------------------------
-# $Log$
-# Revision 1.5  2005/06/18 12:08:19  dischi
-# cosmetic update
-#
-# Revision 1.4  2004/11/20 18:22:59  dischi
-# use python logger module for debug
-#
-# Revision 1.3  2004/08/27 14:22:59  dischi
-# make it possible to switch event context
-#
-# Revision 1.2  2004/08/26 15:32:35  dischi
-# some logic change
-#
-# Revision 1.1  2004/08/25 12:51:43  dischi
-# moved Application for eventhandler into extra dir for future templates
-#
-#
-# -----------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # Freevo - A Home Theater PC framework
-# Copyright (C) 2002 Krister Lagerstrom, et al. 
+# Copyright (C) 2002-2004 Krister Lagerstrom, Dirk Meyer, et al.
+#
+# First Edition: Dirk Meyer <dmeyer@tzi.de>
+# Maintainer:    Dirk Meyer <dmeyer@tzi.de>
+#
 # Please see the file freevo/Docs/CREDITS for a complete list of authors.
 #
 # This program is free software; you can redistribute it and/or modify
@@ -43,15 +27,19 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 #
-# ----------------------------------------------------------------------- */
+# -----------------------------------------------------------------------------
 
+__all__ = [ 'Application' ]
 
-import traceback
+# python imports
+import logging
 
+# freevo imports
 import eventhandler
 import gui
+import gui.animation 
 
-import logging
+# get logging object
 log = logging.getLogger()
 
 
@@ -59,20 +47,29 @@ class Application:
     """
     A basic application
     """
-    def __init__(self, name, event_context, fullscreen, animated=False):
-        self._evt_name       = name
-        self._evt_context    = event_context
-        self._evt_fullscreen = fullscreen
-        self._evt_handler    = eventhandler.get_singleton()
-        self._evt_stopped    = False
-        self._animated       = animated
-        self.post_event      = self._evt_handler.post
-        self.visible         = False
+    def __init__(self, name, eventmap, fullscreen, animated=False):
+        """
+        Init the Application object.
 
-        
+        @param name       : internal name of the application
+        @param eventmap   : context for the event handler mapping
+        @param fullscreen : if the application uses the whole screen
+        @param animated   : use fade in/out animation
+        """
+        self.__name     = name
+        self.__eventmap = eventmap
+        self.__handler  = eventhandler.get_singleton()
+
+        self.animated   = animated
+        self.post_event = self.__handler.post
+        self.visible    = False
+        self.stopped    = False
+        self.fullscreen = fullscreen
+
+
     def eventhandler(self, event, menuw=None):
         """
-        eventhandler for this application
+        Eventhandler for this application
         """
         print 'Error, no eventhandler defined for %s' % self.application
 
@@ -84,32 +81,40 @@ class Application:
         In case it is not overloaded, this function will also update the
         display.
         """
-        self._evt_stopped = False
+        self.stopped = False
         if self.visible:
+            # already visible
             return False
+        # Set visible and append to the eventhandler
         self.visible = True
-        self._evt_handler.append(self)
-        # check if the new app uses animation to show itself
-        if not self._animated:
-            log.info('no show animation for %s -- waiting' % self)
+        self.__handler.append(self)
+        # Check if the new app uses animation to show itself
+        if not self.animated:
+            # This application has no animation for showing. But the old one
+            # may have. So we need to wait until the renderer is done.
+            # FIXME: what happens if a 'normal' animation is running?
+            log.info('no show animation for %s -- waiting' % self.__name)
             gui.animation.render().wait()
-        s = traceback.extract_stack(limit = 2)[0][3]
-        if s != 'Application.show(self)':
+        if self.__class__.show == Application.show:
+            # show() is not changed in the inherting class. Update the display.
             gui.display.update()
         return True
 
-        
+
     def hide(self):
         """
         Hide this application. This can be either because a different
         application has started or that the application was stopped and is no
         longer needed.
         """
-        if self.visible:
-            # wait until all application change animations are
-            # done. There should be none, but just in case to
-            # avoid fading in and out the same object
-            gui.animation.render().wait()
+        if not self.visible:
+            # already hidden
+            return
+        # Wait until all application change animations are done. There
+        # should be none, but just in case to avoid fading in and out the
+        # same object.
+        # FIXME: what happens if a 'normal' animation is running?
+        gui.animation.render().wait()
         self.visible = False
 
 
@@ -118,15 +123,22 @@ class Application:
         Stop the application. If it is not shown again in the same cycle, the
         hide() function will be called.
         """
-        self._evt_stopped = True
+        self.stopped = True
 
 
-    def set_event_context(self, context):
+    def set_eventmap(self, eventmap):
         """
-        Set a new context for the event handler
+        Set a new eventmap for the event handler
         """
-        self._evt_context = context
-        if self._evt_handler.get() == self:
+        self.__eventmap = eventmap
+        if self.__handler.get() == self:
             # We are the current application with the focus,
-            # so set context of the eventhandler to the new context
-            self._evt_handler.context = context
+            # so set eventmap of the eventhandler to the new eventmap
+            self.__handler.context = eventmap
+
+
+    def get_eventmap(self):
+        """
+        Return the eventmap for the application.
+        """
+        return self.__eventmap
