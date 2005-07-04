c12b790fcf00ec2f129a7ebb69822f6de96a7757
Dirk Meyer <dischi@freevo.org>
2005-07-04 15:51:30 +0000

    make the plugin work again
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@7450 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- src/plugins/shoppingcart.py
+++ src/plugins/shoppingcart.py
@@ -1,38 +1,21 @@
 # -*- coding: iso-8859-1 -*-
-# -----------------------------------------------------------------------
-# shoppingcart.py - Example item plugin
-# -----------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+# shoppingcart.py - Add files to a card and do something
+# -----------------------------------------------------------------------------
 # $Id$
 #
-# Notes: This is a plugin to move and copy files
-#
-# Activate: 
-#   plugin.activate('shoppingcart')
-#
-# Todo:        
+# Todo:
 #   o handle fxd files
 #   o also add metafiles like covers to the cart
+#   o show status in the idlebar
+#   o add stuff like making the card a playlist
 #
-# -----------------------------------------------------------------------
-# $Log$
-# Revision 1.9  2005/06/04 17:18:13  dischi
-# adjust to gui changes
-#
-# Revision 1.8  2004/07/26 18:10:18  dischi
-# move global event handling to eventhandler.py
-#
-# Revision 1.7  2004/07/22 21:21:49  dischi
-# small fixes to fit the new gui code
-#
-# Revision 1.6  2004/07/10 12:33:40  dischi
-# header cleanup
-#
-# Revision 1.5  2004/01/31 13:15:14  dischi
-# only add the plugin if the parent is a dir
-#
-# -----------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # Freevo - A Home Theater PC framework
-# Copyright (C) 2002 Krister Lagerstrom, et al. 
+# Copyright (C) 2002-2004 Krister Lagerstrom, Dirk Meyer, et al.
+#
+# Maintainer:    Dirk Meyer <dmeyer@tzi.de>
+#
 # Please see the file freevo/Docs/CREDITS for a complete list of authors.
 #
 # This program is free software; you can redistribute it and/or modify
@@ -49,18 +32,15 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 #
-# ----------------------------------------------------------------------- */
+# -----------------------------------------------------------------------------
 
+# copy and move in a thread
+import kaa.notifier
 
-import os
+# freevo imports
 import plugin
-import config
-import shutil
-import util
-import eventhandler
-import event as em
-import menu
-from gui.windows import WaitBox
+from menu import Action
+
 
 class PluginInterface(plugin.ItemPlugin):
     """
@@ -69,72 +49,98 @@ class PluginInterface(plugin.ItemPlugin):
     and pick what you want to do.
 
     plugin.activate('shoppingcart')
-
     """
     def __init__(self):
         plugin.ItemPlugin.__init__(self)
-        self.item = None
         self.cart = []
 
-    def moveHere(self, arg=None, menuw=None):
-        popup = WaitBox(text=_('Moving files...'))
-        popup.show()
-        for cartfile in self.cart:
-            cartfile.files.move(self.item.dir)
-        popup.destroy()
+
+    def __move_or_copy_thread(self, cart, operation, dest):
+        """
+        Do the copy/move in a thread.
+        """
+        for item in cart:
+            getattr(item.files, operation)(dest)
+
+
+    def move_or_copy(self, item, operation):
+        """
+        Move/Copy items to the given directory.
+        """
+        kaa.notifier.Thread(self.__move_or_copy_thread, self.cart, operation,
+                            item.dir).start()
         self.cart = []
-        eventhandler.post(em.MENU_BACK_ONE_MENU)
+        item.get_menustack().delete_submenu()
 
 
-    def copyHere(self, arg=None, menuw=None):
-        popup = WaitBox(text=_('Copying files...'))
-        popup.show()
-        for cartfile in self.cart:
-            cartfile.files.copy(self.item.dir)
-        popup.destroy()
-        self.cart = []
-        eventhandler.post(em.MENU_BACK_ONE_MENU)
-
-
-    def addToCart(self, arg=None, menuw=None):
-        self.cart.append(self.item)
-        if isinstance(menuw.menustack[-1].selected, menu.MenuItem):
-            eventhandler.post(em.MENU_BACK_ONE_MENU)
+    def add_or_remove(self, item):
+        """
+        Add or remove an item from the cart.
+        """
+        if item in self.cart:
+            self.cart.remove(item)
+            txt = _('Removed to Cart')
         else:
-            eventhandler.post(em.Event(em.OSD_MESSAGE, arg=_('Added to Cart')))
-            
+            self.cart.append(item)
+            txt = _('Added to Cart')
+        item.get_menustack().delete_submenu(osd_message=txt)
 
-    def deleteCart(self, arg=None, menuw=None):
+
+    def clear(self, item):
+        """
+        Clear the cart.
+        """
         self.cart = []
-        eventhandler.post(em.MENU_BACK_ONE_MENU)
+        item.get_menustack().delete_submenu()
 
 
     def actions(self, item):
-        self.item = item
-        myactions = []
-
-        if self.item.parent and self.item.parent.type != 'dir':
-            # only activate this for directory items
-            return []
+        """
+        Return actions based on the item.
+        """
+        actions = []
 
         if item.type == 'dir':
+            # It is a dir, so it is possible to copy or move files
+            # to this location.
             if len(self.cart) > 0:
                 for c in self.cart:
                     if not c.files.move_possible():
+                        # moving not possible for this item
                         break
                 else:
-                    myactions.append((self.moveHere, _('Cart: Move Files Here')))
-                myactions.append((self.copyHere, _('Cart: Copy Files Here')))
+                    # add move action
+                    a = Action(_('Cart: Move Files Here'), self.move_or_copy)
+                    a.parameter('move')
+                    actions.append(a)
+                # add copy action
+                a = Action(_('Cart: Copy Files Here'), self.move_or_copy)
+                a.parameter('copy')
+                actions.append(a)
 
+        if item.parent and item.parent.type != 'dir':
+            # only activate the following for directory items
+            return actions
+
+        if item.type == 'dir':
             if not item in self.cart:
-                myactions.append((self.addToCart, _('Add Directory to Cart'), 'cart:add'))
+                txt = _('Add Directory to Cart')
+            else:
+                txt = _('Remove Directory to Cart')
+            a = Action(txt, self.add_or_remove, 'cart:add')
+            actions.append(a)
 
-        elif hasattr(item, 'files') and item.files and item.files.copy_possible() and \
-                 not item in self.cart:
-            myactions.append((self.addToCart, _('Add File to Cart'), 'cart:add'))
+        elif hasattr(item, 'files') and item.files and \
+                 item.files.copy_possible():
+            if not item in self.cart:
+                txt = _('Add Item to Cart')
+            else:
+                txt = _('Remove Item to Cart')
+            a = Action(txt, self.add_or_remove, 'cart:add')
+            actions.append(a)
 
         if self.cart:
-            myactions.append((self.deleteCart, _('Delete Cart')))
+            # add delete option
+            actions.append(Action(_('Delete Cart'), self.clear))
 
-        return myactions
-
+        return actions
