1125ad7ea6d7d369a0f291b11c34f9812d5fd065
Dirk Meyer <dischi@freevo.org>
2005-07-08 18:59:04 +0000

    split plugins from the loader
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@7468 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- src/plugin.py
+++ src/plugin.py
@@ -4,9 +4,8 @@
 # -----------------------------------------------------------------------------
 # $Id$
 #
-# This file is the basic plugin interface for Freevo. It defines simple plugin
-# base classes functions to add or remove a plugin. On init it will load all
-# plugins for the given application.
+# This file defines some special plugins known to Freevo. It also contains
+# all functions defined in the plugin_loader.
 #
 # -----------------------------------------------------------------------------
 # Freevo - A Home Theater PC framework
@@ -33,45 +32,35 @@
 #
 # -----------------------------------------------------------------------------
 
-# python imports
-import os
-import copy
-import logging
-
+# kaa imports
 import kaa.notifier
 
 # freevo imports
+import config
 from event import Event
 import eventhandler
 
-# get logging object
-log = logging.getLogger('config')
+# plugin loader
+from plugin_loader import *
+import plugin_loader
 
-# status of the plugin module
-initialized = False
 
 #
 # Some basic plugins known to Freevo.
 #
 
-class Plugin(object):
+class Plugin(plugin_loader.Plugin):
     """
-    Basic plugin class. All plugins should inherit from this
-    class
+    Basic plugin class.
     """
     def __init__(self):
-        self._type   = None
-        self._level  = 10
-        self._number = 0
-        self.plugin_name   = ''
-
-        # FIXME: bad coding style
-        import config
-        
+        """
+        Execute on activation of the plugin.
+        """
         for var, val, desc in self.config():
-            log.debug('FIXME: %s' % var)
             if not hasattr(config, var):
                 setattr(config, var, val)
+        plugin_loader.Plugin.__init__(self)
 
 
     def config(self):
@@ -90,6 +79,16 @@ class Plugin(object):
         pass
 
 
+    def plugin_activate(self):
+        """
+        Execute on activation of the plugin.
+        """
+        # register shutdown handler
+        if self.__class__.shutdown != Plugin.shutdown:
+            # plugin has a self defined shutdown function
+            kaa.notifier.addShutdown( self.shutdown )
+
+
 
 class MainMenuPlugin(Plugin):
     """
@@ -97,6 +96,8 @@ class MainMenuPlugin(Plugin):
     """
     def __init__(self):
         Plugin.__init__(self)
+        self.plugin_type = 'mainmenu'
+        self.plugin_special = True
 
 
     def items(self, parent):
@@ -119,6 +120,8 @@ class ItemPlugin(Plugin):
     """
     def __init__(self):
         Plugin.__init__(self)
+        self.plugin_type = 'item'
+        self.plugin_special = True
 
 
     def actions(self, item):
@@ -135,7 +138,7 @@ class ItemPlugin(Plugin):
         """
         return False
 
-    
+
 class DaemonPlugin(Plugin):
     """
     Plugin class for daemon objects who will be activate in the
@@ -156,7 +159,7 @@ class DaemonPlugin(Plugin):
         pass
 
 
-    def _poll(self):
+    def __poll(self):
         """
         wrapper for the poll function
         """
@@ -164,6 +167,28 @@ class DaemonPlugin(Plugin):
             return True
         self.poll()
         return True
+
+
+    def plugin_activate(self):
+        """
+        Execute on activation of the plugin.
+        """
+        Plugin.plugin_activate(self)
+        if self.__class__.poll != DaemonPlugin.poll:
+            # plugin has a self defined poll function, register it
+            kaa.notifier.addTimer( self.poll_interval, self.__poll )
+
+        if self.__class__.eventhandler != DaemonPlugin.eventhandler:
+            # plugin has a self defined eventhandler
+            if self.events:
+                for e in self.events:
+                    eventhandler.register(self, e)
+            else:
+                if self.event_listener:
+                    handler = eventhandler.EVENT_LISTENER
+                else:
+                    handler = eventhandler.GENERIC_HANDLER
+                eventhandler.register(self, handler)
 
 
     def eventhandler(self, event):
@@ -175,7 +200,7 @@ class DaemonPlugin(Plugin):
         """
         return False
 
-    
+
 class MimetypePlugin(Plugin):
     """
     Plugin class for mimetypes handled in a directory/playlist.
@@ -185,6 +210,7 @@ class MimetypePlugin(Plugin):
     def __init__(self):
         Plugin.__init__(self)
         self.display_type = []
+        self.plugin_type = 'mimetype'
 
 
     def suffix(self):
@@ -239,174 +265,6 @@ VIDEO_PLAYER   = 'VIDEO_PLAYER'
 TV             = 'TV'
 RECORD         = 'RECORD'
 
-#
-# Plugin functions
-#
-
-
-def activate(name, type=None, level=10, args=None):
-    """
-    activate a plugin
-    """
-    global _plugin_number
-    _plugin_number += 1
-
-    for p in _all_plugins:
-        if not isinstance(name, Plugin) and p[0] == name and \
-               p[1] == type and p[3] == args:
-            log.warning('duplicate plugin activation, ignoring:\n' + \
-                        '  <%s %s %s>' % (name, type, args))
-            return
-    if initialized:
-        _load_plugin(name, type, level, args, _plugin_number)
-        _sort_plugins()
-    else:
-        _all_plugins.append((name, type, level, args, _plugin_number))
-    return _plugin_number
-
-
-def remove(id):
-    """
-    remove a plugin from the list. This can only be done in local_config.py
-    and not while Freevo is running
-    """
-    if initialized:
-        return
-
-    # remove by plugin id
-    if isinstance(id, int):
-        for p in _all_plugins:
-            if p[4] == id:
-                _all_plugins.remove(p)
-                return
-
-    # remove by name
-    r = []
-    for p in copy.copy(_all_plugins):
-        if p[0] == id:
-            _all_plugins.remove(p)
-
-
-def is_active(name, arg=None):
-    """
-    search the list if the given plugin is active. If arg is set,
-    check arg, too.
-    """
-    for p in _all_plugins:
-        if p[0] == name:
-            if not arg:
-                return p
-            if isinstance(arg, list) or isinstance(arg, tuple):
-                try:
-                    for i in range(len(arg)):
-                        if arg[i] != p[3][i]:
-                            break
-                    else:
-                        return p
-                except:
-                    pass
-            if arg == p[3]:
-                return p
-    return False
-
-
-def get_number():
-    return len(_all_plugins)
-
-
-def init(callback = None, reject=['record', 'www'], exclusive=[]):
-    """
-    load and init all the plugins
-    """
-    global initialized
-    initialized = True
-
-    current = 0
-    for name, type, level, args, number in _all_plugins:
-        if kaa.notifier.step:
-            kaa.notifier.step(False, False)
-        current += 1
-        if callback:
-            callback()
-        if exclusive:
-            # load only plugins from exclusive list
-            for e in exclusive:
-                if not isinstance(name, str):
-                    _load_plugin(name, type, level, args, number)
-                    break
-                if name.startswith('%s.' % e):
-                    _load_plugin(name, type, level, args, number)
-                    break
-        else:
-            # load all plugin except rejected once
-            for r in reject:
-                if isinstance(name, str) and name.startswith('%s.' % r):
-                    break
-            else:
-                _load_plugin(name, type, level, args, number)
-
-
-    # sort plugins in extra function (exec doesn't like to be
-    # in the same function is 'lambda'
-    _sort_plugins()
-
-
-
-def get(type):
-    """
-    get the plugin list 'type'
-    """
-    if not _plugin_type_list.has_key(type):
-        _plugin_type_list[type] = []
-    return _plugin_type_list[type]
-
-
-def getall():
-    """
-    return a list of all plugins
-    """
-    ret = []
-    for t in _all_plugins:
-        ret.append(t[0])
-    return ret
-
-
-def mimetype(display_type=None):
-    """
-    return all MimetypePlugins for the given display_type. If display_type is
-    None, return all MimetypePlugins.
-    """
-    if not display_type:
-        return _plugin_type_list['mimetype']
-    ret = []
-    for p in _plugin_type_list['mimetype']:
-        if not p.display_type or display_type in p.display_type:
-            ret.append(p)
-    return ret
-
-
-def getbyname(name, multiple_choises=0):
-    """
-    get a plugin by it's name
-    """
-    if _named_plugins.has_key(name):
-        return _named_plugins[name]
-    if multiple_choises:
-        return []
-    return None
-
-
-def register(plugin, name, multiple_choises=0):
-    """
-    register an object as a named plugin
-    """
-    if multiple_choises:
-        if not _named_plugins.has_key(name):
-            _named_plugins[name] = []
-        _named_plugins[name].append(plugin)
-    else:
-        _named_plugins[name] = plugin
-
 
 def event(name, arg=None):
     """
@@ -426,182 +284,15 @@ def isevent(event):
         return None
 
 
-#
-# internal stuff
-#
-
-_all_plugins        = []
-_plugin_number      = 0
-_plugin_type_list   = {}
-_named_plugins      = {}
-
-
-def _add_to_ptl(type, object):
+def mimetype(display_type=None):
     """
-    small helper function to add a plugin to the PluginTypeList
+    return all MimetypePlugins for the given display_type. If display_type
+    is None, return all MimetypePlugins.
     """
-    if not _plugin_type_list.has_key(type):
-        _plugin_type_list[type] = []
-    _plugin_type_list[type].append(object)
-
-
-
-def _find_plugin_file(filename):
-    full_filename = os.path.join(os.environ['FREEVO_PYTHON'], filename)
-
-    if os.path.isfile(full_filename + '.py'):
-        return filename.replace('/', '.'), None
-
-    if os.path.isdir(full_filename):
-        return filename.replace('/', '.'), None
-
-    full_filename = os.path.join(os.environ['FREEVO_PYTHON'],
-                                 'plugins', filename)
-
-    if os.path.isfile(full_filename + '.py'):
-        return 'plugins.' + filename.replace('/', '.'), None
-
-    if os.path.isdir(full_filename):
-        return 'plugins.' + filename.replace('/', '.'), None
-
-    if filename.find('/') > 0:
-        special = filename[:filename.find('/')]
-        filename = os.path.join(special, 'plugins',
-                                filename[filename.find('/')+1:])
-        full_filename = os.path.join(os.environ['FREEVO_PYTHON'], filename)
-
-        if os.path.isfile(full_filename + '.py'):
-            return filename.replace('/', '.'), special
-
-        if os.path.isdir(full_filename):
-            return filename.replace('/', '.'), special
-
-    return None, None
-
-
-
-def _load_plugin(name, type, level, args, number):
-    """
-    load the plugin and add it to the lists
-    """
-    # fallback
-    module  = name
-    object  = '%s.PluginInterface' % module
-    special = None
-
-    # locate the plugin:
-    files = []
-
-    if not isinstance(name, Plugin):
-        module, special = _find_plugin_file(name.replace('.', '/'))
-        if module:
-            object = module + '.PluginInterface'
-        elif name.find('.') > 0:
-            pname = name[:name.rfind('.')].replace('.', '/')
-            module, special = _find_plugin_file(pname)
-            if module:
-                object = module + '.%s' % name[name.rfind('.')+1:]
-            else:
-                log.critical('can\'t locate plugin %s' % name)
-                return
-        else:
-            log.critical('can\'t locate plugin %s' % name)
-            return
-
-    try:
-        if not isinstance(name, Plugin):
-            log.debug('loading %s as plugin %s' % (module, object))
-
-            exec('import %s' % module)
-            if not args:
-                p = eval(object)()
-            elif isinstance(args, list) or isinstance(args, tuple):
-                paramlist = 'args[0]'
-                for i in range(1, len(args)):
-                    paramlist += ',args[%s]' % i
-                p = eval('%s(%s)' % (object, paramlist))
-            else:
-                p = eval(object)(args)
-
-            if not hasattr(p, '_type'):
-                if hasattr(p, 'reason'):
-                    reason = p.reason
-                else:
-                    reason = 'unknown\nThe plugin neither called __init__ '\
-                             'nor set a reason why\nPlease contact the plugin'\
-                             'author or the freevo list'
-                log.warning('plugin %s deactivated\n  reason: %s' % \
-                            (name, reason))
-                return
-        else:
-            p = name
-
-        p._number = number
-        p._level = level
-
-        if type:
-            special = type
-
-        if special == 'main':
-            special = ''
-        elif special:
-            special = '_%s' % special
-        else:
-            special = ''
-
-        if p._type:
-            # special plugin type (e.g. idlebar)
-            _add_to_ptl(p._type, p)
-
-        else:
-            if isinstance(p, DaemonPlugin):
-                # plugin is a DaemonPlugin
-                if p.__class__.poll != DaemonPlugin.poll:
-                    # plugin has a self defined poll function, register it
-                    kaa.notifier.addTimer( p.poll_interval, p._poll )
-
-                if p.__class__.eventhandler != DaemonPlugin.eventhandler:
-                    # plugin has a self defined eventhandler
-                    if p.events:
-                        for e in p.events:
-                            eventhandler.register(p, e)
-                    else:
-                        if p.event_listener:
-                            handler = eventhandler.EVENT_LISTENER
-                        else:
-                            handler = eventhandler.GENERIC_HANDLER
-                        eventhandler.register(p, handler)
-
-            if isinstance(p, MainMenuPlugin):
-                # plugin is a MainMenuPlugin
-                _add_to_ptl('mainmenu%s' % special, p)
-
-            if isinstance(p, ItemPlugin):
-                # plugin is an ItemPlugin
-                _add_to_ptl('item%s' % special, p)
-
-            if isinstance(p, MimetypePlugin):
-                # plugin is a MimetypePlugin
-                _add_to_ptl('mimetype', p)
-
-        # register shutdown handler
-        if p.__class__.shutdown != Plugin.shutdown:
-            # plugin has a self defined shutdown function
-            kaa.notifier.addShutdown( p.shutdown )
-
-        if p.plugin_name:
-            _named_plugins[p.plugin_name] = p
-
-
-    except:
-        log.exception('failed to load plugin %s' % name)
-
-
-def _sort_plugins():
-    """
-    sort all plugin lists based on the level
-    """
-    global _plugin_type_list
-    for key in _plugin_type_list:
-        _plugin_type_list[key].sort(lambda l, o: cmp(l._level, o._level))
-
+    if not display_type:
+        return plugin_loader.get('mimetype')
+    ret = []
+    for p in plugin_loader.get('mimetype'):
+        if not p.display_type or display_type in p.display_type:
+            ret.append(p)
+    return ret
---
+++ src/plugin_loader.py
@@ -0,0 +1,361 @@
+# -*- coding: iso-8859-1 -*-
+# -----------------------------------------------------------------------------
+# plugin_loader.py - Plugin interface
+# -----------------------------------------------------------------------------
+# $Id$
+#
+# This file is the basic plugin interface for Freevo. It defines simple plugin
+# base classes functions to add or remove a plugin.
+#
+# TODO: o make it possible to remove plugins later
+#       o more cleanup
+#
+# -----------------------------------------------------------------------------
+# Freevo - A Home Theater PC framework
+# Copyright (C) 2002-2004 Krister Lagerstrom, Dirk Meyer, et al.
+#
+# First Edition: Dirk Meyer <dmeyer@tzi.de>
+# Maintainer:    Dirk Meyer <dmeyer@tzi.de>
+#
+# Please see the file freevo/Docs/CREDITS for a complete list of authors.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MER-
+# CHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+#
+# -----------------------------------------------------------------------------
+
+# python imports
+import os
+import copy
+import logging
+
+import kaa.notifier
+
+# get logging object
+log = logging.getLogger('plugin')
+
+
+class Plugin(object):
+    """
+    Basic plugin class. All plugins should inherit from this class
+    """
+    def __init__(self):
+        self.plugin_type = None
+        self.plugin_level  = 10
+        self.plugin_name = ''
+        self.plugin_special = False
+
+
+    def plugin_activate(self):
+        """
+        Execute on activation of the plugin.
+        """
+        pass
+
+
+    def plugin_decativate(self):
+        """
+        Execute when the plugin is deactivated.
+        """
+        pass
+
+
+
+class PluginLoader(object):
+    """
+    Class for handling the different plugins.
+    """
+    def __init__(self):
+        """
+        Init the plugin loader.
+        """
+        # path where to search for plugins, will be set on init
+        self.path = None
+        # list of all plugins
+        self.plugins = []
+        # next id for a plugin
+        self.next_id = 0
+        # plugins sorted by type
+        self.types = {}
+        # plugins based on name
+        self.names = {}
+        # status of the plugin module
+        self.__initialized = False
+
+
+    def activate(self, name, type=None, level=10, args=None):
+        """
+        Activate a plugin.
+        """
+        self.next_id += 1
+
+        for p in self.plugins:
+            if not isinstance(name, Plugin) and p[0] == name and \
+                   p[1] == type and p[3] == args:
+                log.warning('duplicate plugin activation, ignoring:\n' + \
+                            '  <%s %s %s>' % (name, type, args))
+                return
+        if self.__initialized:
+            self.__load_plugin(name, type, level, args)
+            # sort plugins again
+            cmp_func = lambda l, o: cmp(l.plugin_level, o.plugin_level)
+            for key in self.types:
+                self.types[key].sort(cmp_func)
+        else:
+            self.plugins.append((name, type, level, args, self.next_id))
+        return self.next_id
+
+
+    def deactivate(self, id):
+        """
+        Remove a plugin from the list.
+        """
+        if self.__initialized:
+            return
+
+        if isinstance(id, int):
+            # remove by plugin id
+            for p in self.plugins:
+                if p[4] == id:
+                    self.plugins.remove(p)
+                    return
+        else:
+            # remove by name
+            for p in copy.copy(self.plugins):
+                if p[0] == id:
+                    self.plugins.remove(p)
+
+
+    def is_active(self, name, arg=None):
+        """
+        Search the list if the given plugin is active. If arg is set,
+        check arg, too.
+        """
+        for p in self.plugins:
+            if p[0] == name:
+                if not arg:
+                    return p
+                if isinstance(arg, list) or isinstance(arg, tuple):
+                    try:
+                        for i in range(len(arg)):
+                            if arg[i] != p[3][i]:
+                                break
+                        else:
+                            return p
+                    except:
+                        pass
+                if arg == p[3]:
+                    return p
+        return False
+
+
+    def init(self, plugin_path, callback = None, plugins=[]):
+        """
+        Load and init all the plugins. The function takes the path were the
+        plugins are searched in. Optional is a callback, called after a
+        plugin is loaded. If 'plugins' is given, only plugins with the given
+        prefix are loaded.
+        """
+        self.__initialized = True
+        self.path = plugin_path
+        
+        for name, type, level, args, number in self.plugins:
+            kaa.notifier.step(False, False)
+            if callback:
+                callback()
+            if isinstance(name, Plugin):
+                # plugin already an object
+                self.__load_plugin(name, type, level, args)
+                continue
+
+            if plugins:
+                # load only plugins from exclusive list
+                for p in plugins:
+                    if name.startswith('%s.' % p):
+                        self.__load_plugin(name, type, level, args)
+            else:
+                parent = name[:name.rfind('.')]
+                if name.find('.') > 0 and not self.is_active(parent):
+                    # Parent plugin is not active. So this plugin is
+                    # deactivated by default.
+                    log.info('skip plugin %s' % name)
+                    continue
+                self.__load_plugin(name, type, level, args)
+
+        # sort plugins
+        cmp_func = lambda l, o: cmp(l.plugin_level, o.plugin_level)
+        for key in self.types:
+            self.types[key].sort(cmp_func)
+
+
+    def get(self, type):
+        """
+        Get the plugin list 'type' or all if type is None
+        """
+        if not type:
+            return self.plugins
+        if not self.types.has_key(type):
+            self.types[type] = []
+        return self.types[type]
+
+
+    def getbyname(self, name, multiple_choises=0):
+        """
+        Get a plugin by it's name
+        """
+        if self.names.has_key(name):
+            return self.names[name]
+        if multiple_choises:
+            return []
+        return None
+
+
+    def register(self, plugin, name, multiple_choises=0):
+        """
+        Register an object as a named plugin
+        """
+        if multiple_choises:
+            if not self.names.has_key(name):
+                self.names[name] = []
+            self.names[name].append(plugin)
+        else:
+            self.names[name] = plugin
+
+
+    def __find_plugin_file(self, filename):
+        """
+        Find the filename for the plugin and the python import statement.
+        """
+        full_filename = os.path.join(self.path, filename)
+
+        if os.path.isfile(full_filename + '.py'):
+            return filename.replace('/', '.'), None
+
+        if os.path.isdir(full_filename):
+            return filename.replace('/', '.'), None
+
+        full_filename = os.path.join(self.path, 'plugins', filename)
+
+        if os.path.isfile(full_filename + '.py'):
+            return 'plugins.' + filename.replace('/', '.'), None
+
+        if os.path.isdir(full_filename):
+            return 'plugins.' + filename.replace('/', '.'), None
+
+        if filename.find('/') > 0:
+            special = filename[:filename.find('/')]
+            filename = os.path.join(special, 'plugins',
+                                    filename[filename.find('/')+1:])
+            full_filename = os.path.join(self.path, filename)
+
+            if os.path.isfile(full_filename + '.py'):
+                return filename.replace('/', '.'), special
+
+            if os.path.isdir(full_filename):
+                return filename.replace('/', '.'), special
+
+        return None, None
+
+
+    def __load_plugin(self, name, type, level, args):
+        """
+        Load the plugin and add it to the lists
+        """
+        # fallback
+        module  = name
+        object  = '%s.PluginInterface' % module
+        special = None
+
+        # locate the plugin:
+        files = []
+
+        if not isinstance(name, Plugin):
+            module, special = self.__find_plugin_file(name.replace('.', '/'))
+            if module:
+                object = module + '.PluginInterface'
+            elif name.find('.') > 0:
+                pname = name[:name.rfind('.')].replace('.', '/')
+                module, special = self.__find_plugin_file(pname)
+                if module:
+                    object = module + '.%s' % name[name.rfind('.')+1:]
+                else:
+                    log.critical('can\'t locate plugin %s' % name)
+                    return
+            else:
+                log.critical('can\'t locate plugin %s' % name)
+                return
+
+        try:
+            if not isinstance(name, Plugin):
+                log.debug('loading %s as plugin %s' % (module, object))
+
+                exec('import %s' % module)
+                if not args:
+                    p = eval(object)()
+                elif isinstance(args, list) or isinstance(args, tuple):
+                    paramlist = 'args[0]'
+                    for i in range(1, len(args)):
+                        paramlist += ',args[%s]' % i
+                    p = eval('%s(%s)' % (object, paramlist))
+                else:
+                    p = eval(object)(args)
+
+                if not hasattr(p, 'plugin_type'):
+                    if hasattr(p, 'reason'):
+                        reason = p.reason
+                    else:
+                        reason = '''unknown
+                        The plugin neither called __init__ nor set a reason why
+                        Please contact the plugin author'''
+                    log.warning('plugin %s deactivated\n  reason: %s' % \
+                                (name, reason))
+                    return
+            else:
+                p = name
+
+            p.plugin_activate()
+            p.plugin_level = level
+
+            if type:
+                special = type
+
+            if p.plugin_type:
+                if p.plugin_special and special:
+                    key = p.plugin_type + '_' + special, p
+                else:
+                    key = p.plugin_type
+                if not self.types.has_key(key):
+                    self.types[key] = []
+                self.types[key].append(p)
+
+            if p.plugin_name:
+                self.names[p.plugin_name] = p
+
+
+        except:
+            log.exception('failed to load plugin %s' % name)
+
+
+
+_loader = PluginLoader()
+
+# interface:
+activate = _loader.activate
+remove = _loader.deactivate
+deactivate = _loader.deactivate
+is_active = _loader.is_active
+init = _loader.init
+get = _loader.get
+getbyname = _loader.getbyname
+register = _loader.register
