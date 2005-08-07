657a2e3f0165d81ea186bd15497d1d1b00db950d
Dirk Meyer <dischi@freevo.org>
2005-08-07 10:13:26 +0000

    adjust to new menu interface
    git-svn-id: svn://svn.freevo.org/freevo/trunk/freevo@7599 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- src/plugins/file_ops.py
+++ src/plugins/file_ops.py
@@ -1,38 +1,15 @@
 # -*- coding: iso-8859-1 -*-
-# -----------------------------------------------------------------------
-# file_ops.py - Small file operations (currently only delete)
-# -----------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+# file_ops.py - Small file (item) operations
+# -----------------------------------------------------------------------------
 # $Id$
 #
-# Notes:
-# Todo:        
+# First edition: Dirk Meyer <dmeyer@tzi.de>
+# Maintainer:    Dirk Meyer <dmeyer@tzi.de>
 #
-# -----------------------------------------------------------------------
-# $Log$
-# Revision 1.25  2005/06/04 17:18:13  dischi
-# adjust to gui changes
-#
-# Revision 1.24  2005/01/31 20:00:30  dischi
-# delete files in the background
-#
-# Revision 1.23  2004/11/20 18:23:03  dischi
-# use python logger module for debug
-#
-# Revision 1.22  2004/11/01 20:15:40  dischi
-# fix debug
-#
-# Revision 1.21  2004/07/22 21:21:49  dischi
-# small fixes to fit the new gui code
-#
-# Revision 1.20  2004/07/10 12:33:40  dischi
-# header cleanup
-#
-# Revision 1.19  2004/02/18 21:54:34  dischi
-# use new gui ConfirmBox feature to show handler message
-#
-# -----------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # Freevo - A Home Theater PC framework
-# Copyright (C) 2002 Krister Lagerstrom, et al. 
+# Copyright (C) 2002-2004 Krister Lagerstrom, Dirk Meyer, et al.
 # Please see the file freevo/Docs/CREDITS for a complete list of authors.
 #
 # This program is free software; you can redistribute it and/or modify
@@ -49,22 +26,31 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 #
-# ----------------------------------------------------------------------- */
+# -----------------------------------------------------------------------------
 
+# python imports
+import logging
 
+# kaa imports
+from kaa.notifier import Callback
+
+# freevo imports
 import config
 import plugin
 import util
 
+from menu import Action
 from gui.windows import ConfirmBox
 
-import logging
+# get logging object
 log = logging.getLogger()
+
 
 class PluginInterface(plugin.ItemPlugin):
     """
-    small plugin to delete files
+    Small plugin to delete files
     """
+
     def config(self):
         return [ ('FILE_OPS_ALLOW_DELETE_IMAGE', True,
                   'Add delete image to the menu.'),
@@ -73,58 +59,63 @@ class PluginInterface(plugin.ItemPlugin):
 
     def actions(self, item):
         """
-        create list of possible actions
+        Create list of possible actions
         """
         if not item.parent or not item.parent.type == 'dir':
             # only activate this for directory listings
             return []
 
-        self.item = item
+        if not hasattr(item, 'files') or not item.files:
+            # no files to operate one
+            return []
 
-        items = []
-        
-        if hasattr(item, 'files') and item.files:
-            if item.files.delete_possible():
-                items.append((self.confirm_delete, _('Delete'), 'delete'))
-            if item.files.fxd_file and config.FILE_OPS_ALLOW_DELETE_INFO:
-                items.append((self.confirm_info_delete, _('Delete info'), 'delete_info'))
-            if item.files.image and config.FILE_OPS_ALLOW_DELETE_IMAGE:
-                items.append((self.confirm_image_delete, _('Delete image'), 'delete_image'))
-        return items
+        actions = []
+
+        if item.files.delete_possible():
+            a = Action(_('Delete'), self.delete, 'delete')
+            actions.append(a)
+        if item.files.fxd_file and config.FILE_OPS_ALLOW_DELETE_INFO:
+            a = Action(_('Delete info'), self.delete_info, 'delete_info')
+            actions.append(a)
+        if item.files.image and config.FILE_OPS_ALLOW_DELETE_IMAGE:
+            Action(_('Delete image'), self.delete_image, 'delete_image')
+            actions.append(a)
+
+        return actions
 
 
-    def confirm_delete(self, arg=None, menuw=None):
-        self.menuw = menuw
-        ConfirmBox(text=_('Do you wish to delete\n \'%s\'?') % self.item.name,
-                   handler=self.delete_file, default_choice=1,
-                   handler_message=_('Deleting...')).show()
-        
-    def confirm_info_delete(self, arg=None, menuw=None):
-        self.menuw = menuw
-        ConfirmBox(text=_('Delete info about\n \'%s\'?') % self.item.name,
-                   handler=self.delete_info, default_choice=1).show()
+    def delete(self, item):
+        ConfirmBox(text=_('Do you wish to delete\n \'%s\'?') % item.name,
+                   handler=Callback(self.__delete, item),
+                   default_choice=1, handler_message=_('Deleting...')).show()
 
-    def confirm_image_delete(self, arg=None, menuw=None):
-        self.menuw = menuw
-        ConfirmBox(text=_('Delete image about\n \'%s\'?') % self.item.name,
-                   handler=self.delete_image, default_choice=1).show()
 
-    def delete_file(self):
-        self.item.files.delete()
-        if self.menuw:
-            self.menuw.delete_submenu(True, True)
+    def delete_info(self, item):
+        ConfirmBox(text=_('Delete info about\n \'%s\'?') % item.name,
+                   handler=Callback(self.__delete_info, item),
+                   default_choice=1).show()
 
-    def delete_info(self):
-        util.unlink(self.item.files.image)
-        util.unlink(self.item.files.fxd_file)
-        if self.menuw:
-            self.menuw.delete_submenu(True, True)
 
-    def delete_image(self):
-        util.unlink(self.item.files.image)
-        if self.item.parent:
-            self.item.image = self.item.parent.image
-        else:
-            self.item.image = None
-        if self.menuw:
-            self.menuw.delete_submenu(True, True)
+    def delete_image(self, arg=None, menuw=None):
+        ConfirmBox(text=_('Delete image about\n \'%s\'?') % item.name,
+                   handler=Callback(self.__delete_image, item),
+                   default_choice=1).show()
+
+
+    def __delete(self, item):
+        item.files.delete()
+        item.get_menustack().delete_submenu(True, True)
+
+
+    def __delete_info(self, item):
+        util.unlink(item.files.image)
+        util.unlink(item.files.fxd_file)
+        item.get_menustack().delete_submenu(True, True)
+
+
+    def __delete_image(self, item):
+        util.unlink(item.files.image)
+        item.image = None
+        if item.parent:
+            item.image = item.parent.image
+        item.get_menustack().delete_submenu(True, True)
