eb364977486b551f0700c9470f9c76dcfc3aa22f
Dirk Meyer <dischi@freevo.org>
2006-04-13 17:02:01 +0000

    update doc
    git-svn-id: svn://svn.freevo.org/freevo/trunk/ui@8151 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- src/input/plugins/x11.py
+++ src/input/plugins/x11.py
@@ -1,44 +1,54 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-#
-# x11.py
-# 
-# Author:   <crunchy@tzi.de>
-# 
-# x11
-# 
+# -*- coding: iso-8859-1 -*-
+# -----------------------------------------------------------------------------
+# x11.py - X11 window key input plugin
+# -----------------------------------------------------------------------------
 # $Id$
-# 
-# Copyright (C) 2004 Andreas Büsching <crunchy@tzi.de>
-# 
+#
+# -----------------------------------------------------------------------------
+# Freevo - A Home Theater PC framework
+# Copyright (C) 2002-2005 Krister Lagerstrom, Dirk Meyer, et al.
+#
+# First Edition: Andreas Büsching <crunchy@tzi.de>
+# Maintainer:    Dirk Meyer <dischi@freevo.org>
+#
+# Please see the file AUTHORS for a complete list of authors.
+# Some of the work from Andreas Büsching is moved into kaa.display
+#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2, or (at your option)
-# any later version.
-# 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MER-
+# CHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+#
+# -----------------------------------------------------------------------------
 
+# python imports
+import logging
+
+# freevo imports
 import gui
 import config
 
+# input imports
 from input import linux_input
 from input.interface import InputPlugin
 
-import logging
+# get logging object
 log = logging.getLogger('input')
 
 SCREENSHOT = 0
 
 class PluginInterface(InputPlugin):
     """
-    Plugin for pygame input events
+    Plugin for x11 keys.
     """
     def __init__(self):
         InputPlugin.__init__(self)
@@ -47,7 +57,7 @@ class PluginInterface(InputPlugin):
 
     def handle( self, keycode ):
         """
-        Callback to handle the pygame events.
+        Callback to handle the x11 keys.
         """
         if keycode == 353:
             global SCREENSHOT
@@ -56,7 +66,7 @@ class PluginInterface(InputPlugin):
             gui.display._backing_store._image.save(filename)
             SCREENSHOT += 1
             return True
-        
+
         if isinstance(keycode, int):
             log.debug('Bad keycode %s' % keycode)
             return True
