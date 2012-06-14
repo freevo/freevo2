32f0d45d2fc63a97bc510242d35c22a3b19342e2
Dirk Meyer <dischi@freevo.org>
2012-06-14 21:25:31 +0200

    use kaa.candy and not kaa.popcorn for playback

--- src/plugins/audio/player.py
+++ src/plugins/audio/player.py
@@ -6,7 +6,7 @@
 #
 # -----------------------------------------------------------------------------
 # Freevo - A Home Theater PC framework
-# Copyright (C) 2007-2009 Dirk Meyer, et al.
+# Copyright (C) 2007-2012 Dirk Meyer, et al.
 #
 # First Edition: Dirk Meyer <dischi@freevo.org>
 # Maintainer:    Dirk Meyer <dischi@freevo.org>
@@ -41,6 +41,7 @@ import kaa.popcorn
 
 # Freevo imports
 from ... import core as freevo
+from ... import gui
 
 # get logging object
 log = logging.getLogger('audio')
@@ -51,16 +52,13 @@ class Player(freevo.Application):
     """
 
     name = 'audioplayer'
+    player = None
 
     def __init__(self):
         capabilities = (freevo.CAPABILITY_TOGGLE, freevo.CAPABILITY_PAUSE)
         super(Player, self).__init__('audio', capabilities)
-        self.player = kaa.popcorn.Player()
-        self.player.window = False
-        self.elapsed_timer = kaa.WeakTimer(self.elapsed)
         self.PLAY_START = freevo.Event(freevo.PLAY_START, handler=self.eventhandler)
         self.PLAY_END = freevo.Event(freevo.PLAY_END, handler=self.eventhandler)
-
 
     @kaa.coroutine()
     def play(self, item):
@@ -82,13 +80,11 @@ class Player(freevo.Application):
             # Freevo is in shutdown mode, do not start a new player, the old
             # only stopped because of the shutdown.
             yield False
-
         # Try to get AUDIO resource. The ressouce will be freed by the system
         # when the application switches to STATUS_STOPPED or STATUS_IDLE.
         if (yield self.get_resources('AUDIO', force=True)) == False:
             log.error("Can't get Audio resource.")
             yield False
-
         # Store item and playlist. We need to keep the playlist object
         # here to make sure it is not deleted when player is running in
         # the background.
@@ -96,30 +92,21 @@ class Player(freevo.Application):
         self.playlist = self.item.playlist
         if self.playlist:
             self.playlist.select(self.item)
-
         # Set the current item to the gui engine
         self.context.item = self.item.properties
         self.context.menu = self.playlist
         self.status = freevo.STATUS_RUNNING
-
         # Open media item and start playback
-        self.player.signals['finished'].connect_once(self.PLAY_END.post, self.item)
-        self.player.signals['stop'].connect_once(self.PLAY_END.post, self.item)
-        try:
-            yield self.player.open(self.item.url)
-            yield self.player.play()
-            self.PLAY_START.post(self.item)
-        except kaa.popcorn.PlayerError, e:
-            self.player.signals['finished'].disconnect(self.PLAY_END.post, self.item)
-            self.player.signals['stop'].disconnect(self.PLAY_END.post, self.item)
-            log.exception('video playback failed')
-            # We should handle it here with a messge or something like that. To
-            # make playlist work, we just send start and stop. It's ugly but it
-            # should work.
-            self.PLAY_START.post(self.item)
-            self.PLAY_END.post(self.item)
+        self.player = kaa.candy.Video(url=item.filename, size=gui.stage.size)
+        # FIXME: that should be done by the theme engine using an
+        # audio element. That element should also create self.player
+        # and should have a goom option. Choosing mplayer makes no
+        # sense, gstreamer can play all audio I know of.
+        gui.stage.layer[0].add(self.player)
+        self.player.signals['finished'].connect_weak_once(freevo.PLAY_END.post, self.item)
+        self.player.signals['progress'].connect_weak(self.set_elapsed)
+        self.player.play()
         yield True
-
 
     def stop(self):
         """
@@ -130,15 +117,13 @@ class Player(freevo.Application):
         self.player.stop()
         self.status = freevo.STATUS_STOPPING
 
-
-    def elapsed(self):
+    def set_elapsed(self, pos):
         """
         Callback for elapsed time changes.
         """
-        if self.item.elapsed != round(self.player.stream.position):
-            self.item.elapsed = round(self.player.stream.position)
+        if self.item.elapsed != round(pos):
+            self.item.elapsed = round(pos)
             self.context.sync()
-
 
     def eventhandler(self, event):
         """
@@ -150,81 +135,75 @@ class Player(freevo.Application):
             self.stop()
             self.item.eventhandler(event)
             return True
-
         if event == freevo.PLAY_START:
-            self.elapsed_timer.start(0.2)
             self.item.eventhandler(event)
             return True
-
         if event == freevo.PLAY_END:
             # Now the player has stopped (either we called self.stop() or the
             # player stopped by itself. So we need to set the application to
             # to stopped.
-            self.player.signals['finished'].disconnect(self.PLAY_END.post, self.item)
-            self.player.signals['stop'].disconnect(self.PLAY_END.post, self.item)
+            if self.player:
+                self.player.unparent()
             self.status = freevo.STATUS_STOPPED
-            self.elapsed_timer.stop()
             self.item.eventhandler(event)
             if self.status == freevo.STATUS_STOPPED:
                 self.status = freevo.STATUS_IDLE
             return True
-
-        if event in (freevo.PAUSE, freevo.PLAY):
-            if self.player.state == kaa.popcorn.STATE_PLAYING:
-                self.suspend()
+        if self.player:
+            # player control makes only sense if the player is still running
+            if event in (freevo.PAUSE, freevo.PLAY):
+                if self.player.state == kaa.candy.STATE_PLAYING:
+                    self.player.pause()
+                    return True
+                if self.player.state == kaa.candy.STATE_PAUSED:
+                    self.player.resume()
+                    return True
+                return False
+            if event == freevo.SEEK:
+                self.player.seek(int(event.arg), kaa.candy.SEEK_RELATIVE)
+                self.player.seek(int(event.arg), kaa.popcorn.SEEK_RELATIVE)
                 return True
-            if self.player.state == kaa.popcorn.STATE_PAUSED:
-                self.resume()
-                return True
-            return False
-
-        if event == freevo.SEEK:
-            self.player.seek(int(event.arg), kaa.popcorn.SEEK_RELATIVE)
-            return True
-
         return self.item.eventhandler(event)
-
 
     def can_suspend(self):
         """
         Return true since the application can be suspended.
         """
-        return True
+        return False
 
-    @kaa.coroutine()
-    def suspend(self):
-        """
-        Release the audio resource that others can use it.
-        """
-        # FIXME: make sure this function is not called twice
-        if not self.status == freevo.STATUS_RUNNING:
-            yield False
-        if self.player.state == kaa.popcorn.STATE_PAUSED:
-            yield None
-        # FIXME: what happens if we send pause() the same time the file
-        # is finished? This would create a race condition.
-        self.player.pause()
-        yield kaa.inprogress(self.player.signals['pause'])
-        self.free_resources()
+    # @kaa.coroutine()
+    # def suspend(self):
+    #     """
+    #     Release the audio resource that others can use it.
+    #     """
+    #     # FIXME: make sure this function is not called twice
+    #     if not self.status == freevo.STATUS_RUNNING:
+    #         yield False
+    #     if self.player.state == kaa.popcorn.STATE_PAUSED:
+    #         yield None
+    #     # FIXME: what happens if we send pause() the same time the file
+    #     # is finished? This would create a race condition.
+    #     self.player.pause()
+    #     yield kaa.inprogress(self.player.signals['pause'])
+    #     self.free_resources()
 
-
-    @kaa.coroutine()
-    def resume(self):
-        """
-        Resume playing the audio, at the position before.
-        """
-        # FIXME: make sure this function is not called twice
-        if not self.status == freevo.STATUS_RUNNING:
-            yield False
-        if self.player.state == kaa.popcorn.STATE_PLAYING:
-            yield False
-        blocked = yield self.get_resources('AUDIO')
-        if blocked == False:
-            # FIXME: what to do in this case?
-            log.error('unable to get AUDIO ressource')
-            yield False
-        self.player.resume()
-        yield kaa.inprogress(self.player.signals['play'])
+    # @kaa.coroutine()
+    # def resume(self):
+    #     """
+    #     Resume playing the audio, at the position before.
+    #     """
+    #     # FIXME: make sure this function is not called twice
+    #     if not self.status == freevo.STATUS_RUNNING:
+    #         yield False
+    #     if self.player.state == kaa.popcorn.STATE_PLAYING:
+    #         yield False
+    #     blocked = yield self.get_resources('AUDIO')
+    #     if blocked == False:
+    #         # FIXME: what to do in this case?
+    #         log.error('unable to get AUDIO ressource')
+    #         yield False
+    #     self.player.resume()
+    #     yield kaa.inprogress(self.player.signals['play'])
 
 
 # create singleton object
